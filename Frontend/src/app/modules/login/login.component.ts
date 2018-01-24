6757c7b5eabafbaf2dbfeb629ef9753eeaa826b6
Torsten Simon <simon@metaventis.com>
2018-01-24 10:35:09 +0100

    fix:login loop redirect on shibboleth

--- Frontend/src/app/modules/login/login.component.ts
+++ Frontend/src/app/modules/login/login.component.ts
@@ -76,14 +76,14 @@ export class LoginComponent  implements OnInit{
                 this.goToNext();
               }
             }
+            if(configService.instant("loginUrl") && data.statusCode!=RestConstants.STATUS_CODE_OK){
+              window.location.href=configService.instant("loginUrl");
+              return;
+            }
           });
           this.showUsername=this.scope!=RestConstants.SAFE_SCOPE;
           this.next=params['next'];
           this.mainnav=params['mainnav']=='false' ? false : true;
-          if(configService.instant("loginUrl") && data.statusCode!=RestConstants.STATUS_CODE_OK){
-            window.location.href=configService.instant("loginUrl");
-            return;
-          }
           if(this.scope==RestConstants.SAFE_SCOPE){
             this.connector.isLoggedIn().subscribe((data:LoginResult)=>{
               if(data.statusCode!=RestConstants.STATUS_CODE_OK){
