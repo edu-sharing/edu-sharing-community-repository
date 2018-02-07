ead9b2337fe7fb2139f861692d21c8d3a1a9f95c
Torsten Simon <simon@metaventis.com>
2018-02-07 18:31:14 +0100

    fix:mds widgets with different valuespaces per template use proper valuespace feature:new color helper features

--- Frontend/src/app/common/ui/color-helper.ts
+++ Frontend/src/app/common/ui/color-helper.ts
@@ -1,11 +1,35 @@
 export class ColorHelper{
   public static cssColorToRgb(color:string) : number[] {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+    color=color.trim();
+    if(color.startsWith("rgb")){
+      console.log(color);
+      let result = /rgb.?\(\s*([\d]*)\s*,\s*([\d]*)\s*,\s*([\d*]*)\s*/i.exec(color);
+      return result ? [
+        parseInt(result[1]),
+        parseInt(result[2]),
+        parseInt(result[3])
+      ] : null;
+    }
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
     return result ? [
       parseInt(result[1], 16),
       parseInt(result[2], 16),
       parseInt(result[3], 16)
     ] : null;
+  }
+
+  /**
+   * returns the estimated color brightness for a css color based on a pereceived brightness factor
+   * Value between [0,1] and -1 if the css string was not readable
+   * @param {string} color
+   * @returns {number}
+   */
+  public static getColorBrightness(color:string){
+    let rgb=ColorHelper.cssColorToRgb(color);
+    if(rgb){
+      return (rgb[0]*0.2126 + rgb[1]*0.7152 + rgb[2]*0.0722) / 255;
+    }
+    return -1;
   }
 
   public static rgbToHex(rgb:number[]) : string {
