8769920dbac3c0218946b23ec6c1642cc40f7873
simon <simon@edu-sharing.net>
2018-02-19 12:43:27 +0100

    fix:admin no full path breadcrumbs

--- Frontend/src/app/modules/collections/collections.component.ts
+++ Frontend/src/app/modules/collections/collections.component.ts
@@ -500,7 +500,7 @@ export class CollectionsMainComponent implements GwtEventListener {
 
   private renderBreadcrumbs() {
     this.path=[];
-    this.nodeService.getNodeParents(this.collectionContent.collection.ref.id).subscribe((data: EduData.NodeList) => {
+    this.nodeService.getNodeParents(this.collectionContent.collection.ref.id,false).subscribe((data: EduData.NodeList) => {
       console.log(data);
       this.path = data.nodes.reverse();
     });
