f9db44aabdae44a659d028f632b41b344ba606b6
Torsten Simon <simon@metaventis.com>
2018-03-08 19:04:26 +0100

    feature:new stats endpoint

--- Backend/src/org/edu_sharing/service/statistic/StatisticsGlobal.java
+++ Backend/src/org/edu_sharing/service/statistic/StatisticsGlobal.java
@@ -1,43 +1,32 @@
 package org.edu_sharing.service.statistic;
 
 import java.util.List;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class StatisticsGlobal {
 	public static class Materials{
-		@JsonProperty public int overall;
-		@JsonProperty public int image;
-		@JsonProperty public int video;
-		@JsonProperty public int text;
-		@JsonProperty public int spreadsheet;
-		@JsonProperty public int presentation;		
-		@JsonProperty public int link;		
+		@JsonProperty
+		public List<License> licenses;
 	}
-	public static class Licenses{
-		@JsonProperty public int CC_0;
-		@JsonProperty public int CC_BY;
-		@JsonProperty public int PDM;
-		@JsonProperty public int unknown;
-	}
-	public static class History{
-		@JsonProperty public String date;
-		@JsonProperty public int created;
-		@JsonProperty public int modified;
+	public static class License{
+		public static class Facette{
+			@JsonProperty public String name;
+			@JsonProperty public Map<String,Integer> count;
+		}
+		@JsonProperty public String name;
+		@JsonProperty public String type;
+		@JsonProperty public int count;
+		@JsonProperty public List<Facette> facettes;
 	}
 	public static class User{
-		@JsonProperty public int overall;
+		@JsonProperty public int count;
 	}
 	@JsonProperty
 	private Materials materials;
 	@JsonProperty
-	private Licenses licenses;
-	@JsonProperty
 	private User user;
-	@JsonProperty
-	private List<History> history_daily;
-	@JsonProperty
-	private List<History> history_monthly;
 	
 	public Materials getMaterials() {
 		return materials;
@@ -45,30 +34,14 @@ public class StatisticsGlobal {
 	public void setMaterials(Materials materials) {
 		this.materials = materials;
 	}
-	public Licenses getLicenses() {
-		return licenses;
-	}
-	public void setLicenses(Licenses licenses) {
-		this.licenses = licenses;
-	}
 	public User getUser() {
 		return user;
 	}
 	public void setUser(User user) {
 		this.user = user;
 	}
-	public List<History> getHistory_daily() {
-		return history_daily;
-	}
-	public void setHistory_daily(List<History> history_daily) {
-		this.history_daily = history_daily;
-	}
-	public List<History> getHistory_monthly() {
-		return history_monthly;
-	}
-	public void setHistory_monthly(List<History> history_monthly) {
-		this.history_monthly = history_monthly;
-	}
+	
+	
 	
 	
 }
