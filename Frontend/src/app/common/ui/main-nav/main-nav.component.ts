d9fb575d548ba8e9eb48120251e24a2449b31397
Torsten Simon <simon@metaventis.com>
2018-03-13 10:53:27 +0100

    fix:search all repositories bugfixes

--- Frontend/src/app/common/ui/main-nav/main-nav.component.ts
+++ Frontend/src/app/common/ui/main-nav/main-nav.component.ts
@@ -237,7 +237,6 @@ export class MainNavComponent {
     return null;
   }
   refreshBanner(){
-    console.log("refresh banner");
     this.handleScroll(null);
     setTimeout(()=>this.handleScroll(null),10);
   }
