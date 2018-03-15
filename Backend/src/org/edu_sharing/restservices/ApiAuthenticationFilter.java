55d5eaf54d9c729ff0984bac76bd2f11f79a1fb8
simon <simon@edu-sharing.net>
2018-03-15 11:42:47 +0100

    fix:revert bearer use session:fails for guest, always use bearer if given in headers, performance improved

--- Backend/src/org/edu_sharing/restservices/ApiAuthenticationFilter.java
+++ Backend/src/org/edu_sharing/restservices/ApiAuthenticationFilter.java
@@ -107,25 +107,22 @@ public class ApiAuthenticationFilter implements javax.servlet.Filter {
 					String accessToken = authHdr.substring(6).trim();
 					
 					try {
-						HashMap<String, String> currentAuth = authTool.validateAuthentication(session);
-						if(currentAuth==null) {
-							Token token = tokenService.getToken(accessToken);
+						Token token = tokenService.getToken(accessToken);
+						
+						if (token != null) {
+							logger.info("oAuthToken:"+ token.getAccessToken() +" alfresco ticket:"+ token.getTicket());
 							
-							if (token != null) {
-								logger.info("oAuthToken:"+ token.getAccessToken() +" alfresco ticket:"+ token.getTicket());
-								
-								//validate and set current user
-								authTool.storeAuthInfoInSession(
-										token.getUsername(), 
-										token.getTicket(),
-										CCConstants.AUTH_TYPE_OAUTH,
-										session);
-								
-								session.setAttribute(CCConstants.AUTH_ACCESS_TOKEN, token.getAccessToken());
-								
-								validatedAuth = authTool.validateAuthentication(session);							
-							}	
-						}
+							//validate and set current user
+							authTool.storeAuthInfoInSession(
+									token.getUsername(), 
+									token.getTicket(),
+									CCConstants.AUTH_TYPE_OAUTH,
+									session);
+							
+							session.setAttribute(CCConstants.AUTH_ACCESS_TOKEN, token.getAccessToken());
+							
+							validatedAuth = authTool.validateAuthentication(session);							
+						}	
 					} catch (Exception ex) {
 						
 						logger.error(ex.getMessage(), ex);
