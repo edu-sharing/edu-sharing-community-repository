50dd5e64cee44362a74582b4e5ef0d3de059c734
simon <simon@edu-sharing.net>
2018-03-27 14:02:56 +0200

    feature:resolve link as lti object

--- Frontend/src/app/modules/management-dialogs/file-upload-select/file-upload-select.component.ts
+++ Frontend/src/app/modules/management-dialogs/file-upload-select/file-upload-select.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, EventEmitter, Output, ViewChild, ElementRef} from '@angular/core';
 import {RestConstants} from "../../../common/rest/rest-constants";
-import {Node,NodeList} from "../../../common/rest/data-object";
+import {Node, NodeList, NodeWrapper} from '../../../common/rest/data-object';
 import {RestNodeService} from "../../../common/rest/services/rest-node.service";
 import {trigger} from "@angular/animations";
 import {UIAnimation} from "../../../common/ui/ui-animation";
+import {RestSearchService} from '../../../common/rest/services/rest-search.service';
 
 @Component({
   selector: 'workspace-file-upload-select',
@@ -22,6 +23,9 @@ export class WorkspaceFileUploadSelectComponent  {
   @Input() isFileOver=false;
   @Input() showPicker=false;
   private breadcrumbs: Node[];
+  private ltiAllowed:boolean;
+  private ltiActivated:boolean;
+  private ltiTool: Node;
   @Input() set parent(parent:string){
     if(parent==RestConstants.USERHOME){
       this.breadcrumbs=[];
@@ -48,8 +52,25 @@ export class WorkspaceFileUploadSelectComponent  {
   public setLink(){
     this.onLinkSelected.emit(this.link.nativeElement.value);
   }
-  public setState(event : any){
-    this.disabled=!this.link.nativeElement.value.trim();
+  public setState(event: any){
+    let link=this.link.nativeElement.value.trim();
+    this.disabled=!link;
+    this.ltiAllowed=false;
+    if(this.cleanupUrlForLti(link)) {
+        this.searchService.search([{
+            property: "url",
+            values: [this.cleanupUrlForLti(link)]
+        }], [], null, RestConstants.CONTENT_TYPE_ALL, RestConstants.HOME_REPOSITORY, RestConstants.DEFAULT, [], 'tool_instances')
+            .subscribe((result: NodeList) => {
+                this.ltiAllowed = result.nodes.length > 0;
+                if(result.nodes.length){
+                  this.nodeService.getNodeMetadata(result.nodes[0].parent.id,[],result.nodes[0].parent.repo).subscribe((data:NodeWrapper)=>{
+                    console.log(data.node);
+                    this.ltiTool=data.node;
+                  })
+                }
+            });
+    }
   }
   public parentChoosed(event:Node[]){
     this.parent=event[0].ref.id;
@@ -58,7 +79,19 @@ export class WorkspaceFileUploadSelectComponent  {
   }
   public constructor(
     private nodeService:RestNodeService,
+    private searchService:RestSearchService,
   ){
     this.parent=RestConstants.USERHOME;
   }
+
+    private cleanupUrlForLti(link: string) {
+        let start=link.indexOf("://");
+        if(start==-1)
+          return null;
+        start+=3;
+        let end=link.indexOf("/",start);
+        if(end==-1)
+          return null;
+        return link.substr(start,end-start);
+    }
 }
