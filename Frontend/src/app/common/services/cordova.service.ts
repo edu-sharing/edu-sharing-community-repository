f7664cab8a1f4afe213475a27db31ac7baecbc6b
Torsten Simon <simon@metaventis.com>
2018-04-17 15:02:42 +0200

    fix:force cordova false

--- Frontend/src/app/common/services/cordova.service.ts
+++ Frontend/src/app/common/services/cordova.service.ts
@@ -17,7 +17,7 @@ import {Router} from "@angular/router";
 export class CordovaService {
 
   // change this during development for testing true, but false is default
-  private forceCordovaMode: boolean = true;
+  private forceCordovaMode: boolean = false;
 
   private deviceIsReady: boolean = false;
 
