c2a0f880fd36f87c372c78ac4684a07b9e022811
Daniel Rudolph <rudolph@metaventis.com>
2018-07-02 10:55:35 +0200

    Feature: PersonLifecycle job

--- Backend/src/org/edu_sharing/repository/client/tools/CCConstants.java
+++ Backend/src/org/edu_sharing/repository/client/tools/CCConstants.java
@@ -293,6 +293,8 @@ public class CCConstants {
 	
 	public final static String CM_PROP_PERSON_ESORIGINALUID = "{http://www.alfresco.org/model/content/1.0}esoriginaluid";
 	
+	public final static String CM_PROP_PERSON_ESPERSONSTATUS = "{http://www.alfresco.org/model/content/1.0}espersonstatus";
+	
 	public final static String CM_PROP_PERSON_EDU_SCHOOL_PRIMARY_AFFILIATION = "{http://www.alfresco.org/model/content/1.0}eduSchoolPrimaryAffiliation";
 
 	public final static String CCM_PROP_PERSON_PREFERENCES = "{http://www.campuscontent.de/model/1.0}userPreferences";
