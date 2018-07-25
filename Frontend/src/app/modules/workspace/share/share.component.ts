14b92364f4ffff785f92d4af7c189ef57e8f77ce
simon <simon@edu-sharing.net>
2018-07-25 18:07:44 +0200

    fix:change detection error in share component

--- Frontend/src/app/modules/workspace/share/share.component.ts
+++ Frontend/src/app/modules/workspace/share/share.component.ts
@@ -32,7 +32,7 @@ import {UIHelper} from "../../../common/ui/ui-helper";
 })
 export class WorkspaceShareComponent implements AfterViewInit{
   ngAfterViewInit(): void {
-    UIHelper.setFocusOnCard();
+    setTimeout(()=>UIHelper.setFocusOnCard());
   }
   public ALL_PERMISSIONS=["All","Read","ReadPreview","ReadAll","Write","Delete",
     "DeleteChildren","DeleteNode","AddChildren","Consumer","ConsumerMetadata",
@@ -58,7 +58,7 @@ export class WorkspaceShareComponent implements AfterViewInit{
   private currentType=[RestConstants.ACCESS_CONSUMER,RestConstants.ACCESS_CC_PUBLISH];
   private inherited : boolean;
   private notifyUsers = true;
-  private notifyMessage = "";
+  private notifyMessage : string;
   private inherit : Permission[]=[];
   private permissions : Permission[]=[];
   public permissionsUser : Permission[];
