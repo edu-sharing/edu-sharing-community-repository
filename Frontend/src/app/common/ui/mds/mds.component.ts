f7d596919ad54b528eccce7ce6ab85239b54e85e
simon <simon@edu-sharing.net>
2018-08-01 17:04:03 +0200

    fix:license only if coordinator (can delete)

--- Frontend/src/app/common/ui/mds/mds.component.ts
+++ Frontend/src/app/common/ui/mds/mds.component.ts
@@ -1802,8 +1802,12 @@ export class MdsComponent{
     else {
         let html=`<div class="mdsLicense">`
         let isSafe=this.connector.getCurrentLogin() && this.connector.getCurrentLogin().currentScope!=null;
+        let canDelete=(this.currentNode && this.currentNode.access.indexOf(RestConstants.ACCESS_DELETE)!=-1);
         if(isSafe || !this.connector.hasToolPermissionInstant(RestConstants.TOOLPERMISSION_LICENSE)){
             html+=`<div class="mdsNoPermissions">`+this.translate.instant('MDS.LICENSE_NO_PERMISSIONS'+(isSafe ? '_SAFE' : ''))+`</div>`;
+        }
+        else if(!canDelete){
+            html+=`<div class="mdsNoPermissions">`+this.translate.instant('MDS.LICENSE_NO_PERMISSIONS_MATERIAL')+`</div>`;
         }
         else {
             html += `<a class="clickable licenseLink" onclick="window.mdsComponentRef.component.openLicenseDialog();">` +
