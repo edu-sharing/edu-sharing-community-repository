7eb359847d7f7f46c2489f36cf354afe073cf909
simon <simon@edu-sharing.net>
2018-08-03 16:19:54 +0200

    fix:lrmi tool more params + date mapping

--- Backend/src/org/edu_sharing/repository/server/tools/LRMITool.java
+++ Backend/src/org/edu_sharing/repository/server/tools/LRMITool.java
@@ -4,9 +4,12 @@ import com.google.gson.JsonObject;
 import com.google.gwt.json.client.JSONNull;
 import net.sf.vcard4j.java.VCard;
 import org.alfresco.service.cmr.repository.StoreRef;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.qpid.proton.amqp.messaging.Properties;
 import org.edu_sharing.repository.client.tools.CCConstants;
 import org.edu_sharing.repository.client.tools.metadata.ValueTool;
+import org.edu_sharing.service.license.LicenseService;
 import org.edu_sharing.service.nodeservice.NodeServiceFactory;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,9 +26,23 @@ public class LRMITool {
         HashMap<String, Object> props = NodeServiceFactory.getLocalService().getProperties(StoreRef.PROTOCOL_WORKSPACE, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE.getIdentifier(), nodeId);
         java.util.Properties lrmiProps = PropertiesHelper.getProperties(propFile, PropertiesHelper.TEXT);
         lrmi.put("@context","http://schema.org/");
-        lrmi.put("@type","WebPage");
-        Object creator=getFromVCard(CCConstants.CCM_PROP_IO_REPL_METADATACONTRIBUTER_CREATOR,props);
-        lrmi.put("creator",creator);
+        lrmi.put("@type",new String[]{"CreativeWork","MediaObject"});
+        Object contributor=getFromVCard(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_MITWIRKENDE,props);
+        lrmi.put("contributor",contributor);
+        Object author=getFromVCard(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_AUTHOR,props);
+        lrmi.put("creator",author);
+        Object publisher=getFromVCard(CCConstants.CCM_PROP_IO_REPL_LIFECYCLECONTRIBUTER_PUBLISHER,props);
+        lrmi.put("publisher",publisher);
+        lrmi.put("url",URLTool.getNgRenderNodeUrl(nodeId,null));
+        lrmi.put("thumbnailUrl",URLTool.getPreviewServletUrl(nodeId,StoreRef.STORE_REF_WORKSPACE_SPACESSTORE.getProtocol(),StoreRef.STORE_REF_WORKSPACE_SPACESSTORE.getIdentifier()));
+        lrmi.put("dateCreated",getDate(getProperty(props, Collections.singletonList(CCConstants.CM_PROP_C_CREATED+CCConstants.LONG_DATE_SUFFIX))));
+        lrmi.put("dateModified",getDate(getProperty(props, Collections.singletonList(CCConstants.CM_PROP_C_MODIFIED+CCConstants.LONG_DATE_SUFFIX))));
+        lrmi.put("datePublished",getDate(getProperty(props, Collections.singletonList(CCConstants.CCM_PROP_PUBLISHED_DATE+CCConstants.LONG_DATE_SUFFIX))));
+        lrmi.put("license",new LicenseService().getLicenseUrl(
+                (String)props.get(CCConstants.CCM_PROP_IO_COMMONLICENSE_KEY),
+                (String)props.get(CCConstants.CCM_PROP_IO_COMMONLICENSE_CC_LOCALE),
+                (String)props.get(CCConstants.CCM_PROP_IO_COMMONLICENSE_CC_VERSION)
+        ));
         for(String prop : lrmiProps.stringPropertyNames()){
             List<String> propsList = new ArrayList<>(
                     Arrays.asList(lrmiProps.getProperty(prop).split(",")));
@@ -42,6 +59,18 @@ public class LRMITool {
         lrmi.put("timeRequired",getProperty(props,CCConstants.CCM_PROP_IO_REPL_EDUCATIONAL_TYPICALLEARNINGTIME));
         */
         return lrmi;
+    }
+
+    private static String getDate(Object property) {
+        if(property==null)
+            return null;
+        try {
+            return DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.format(new Date(Long.parseLong((String) property)));
+
+        }
+        catch(Throwable t){
+            return property.toString();
+        }
     }
 
     private static Object getFromVCard(String key,HashMap<String,Object> props) throws JSONException {
@@ -79,6 +108,10 @@ public class LRMITool {
                 if(((String)props.get(key)).isEmpty()){
                     continue;
                 }
+                String[] data=StringUtils.splitByWholeSeparator((String) props.get(key),CCConstants.MULTIVALUE_SEPARATOR);
+                if(data.length>1)
+                    return data;
+                return data[0];
             }
                 return props.get(key);
         }
