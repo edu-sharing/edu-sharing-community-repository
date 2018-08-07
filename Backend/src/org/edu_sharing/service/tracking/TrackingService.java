9e48281696e06dc23ab178ac60474b05e1ca9f1d
simon <simon@edu-sharing.net>
2018-08-07 09:56:52 +0200

    build:fix unrelated imports

--- Backend/src/org/edu_sharing/service/tracking/TrackingService.java
+++ Backend/src/org/edu_sharing/service/tracking/TrackingService.java
@@ -1,12 +1,6 @@
 package org.edu_sharing.service.tracking;
 
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.edu_sharing.service.stream.model.ContentEntry;
-import org.edu_sharing.service.stream.model.ContentEntry.Audience.STATUS;
-import org.edu_sharing.service.stream.model.ScoreResult;
-import org.edu_sharing.service.stream.model.StreamSearchRequest;
-import org.edu_sharing.service.stream.model.StreamSearchResult;
-import org.json.JSONObject;
 
 import java.util.List;
 import java.util.Map;
