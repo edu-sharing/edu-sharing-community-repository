6857f391d31a7a8083a601be94dd714fd36aba39
Torsten Simon <simon@metaventis.com>
2018-08-31 12:01:59 +0200

    feat:option for secondary value on user information cards (badges, autocomplete, permissions, workflows)

--- Frontend/src/app/common/ui/permission-name.pipe.ts
+++ Frontend/src/app/common/ui/permission-name.pipe.ts
@@ -1,15 +1,34 @@
 import {PipeTransform, Pipe} from '@angular/core';
 import {NodeHelper} from "./node-helper";
 import {TranslateService} from "@ngx-translate/core";
+import {ConfigurationService} from "../services/configuration.service";
 
 @Pipe({name: 'permissionName'})
 export class PermissionNamePipe implements PipeTransform {
-  transform(permission : any,args:string[]): string {
-    if(permission.user && (permission.user.firstName || permission.user.lastName))
-      return permission.user.firstName+" "+permission.user.lastName;
-    if(permission.group && permission.group.displayName)
-      return permission.group.displayName;
-    return this.translate.instant(permission.authority.authorityName);
+  transform(permission : any,args:any): string {
+    if(args && args['field']){
+      let field=args['field'];
+      if(field=='secondary'){
+        field=this.config.instant('userSecondaryDisplayName','email')
+      }
+      console.log(field);
+      console.log(permission);
+
+      if(field=='email') {
+          if(permission.user){
+            return permission.user.email || permission.user.mailbox;
+          }
+          if(permission.profile){
+              return permission.profile.email || permission.profile.mailbox;
+          }
+      }
+      return "";
+    }
+      if(permission.user && (permission.user.firstName || permission.user.lastName))
+          return permission.user.firstName+" "+permission.user.lastName;
+      if(permission.group && permission.group.displayName)
+          return permission.group.displayName;
+      return this.translate.instant(permission.authority.authorityName);
   }
-  constructor(private translate : TranslateService){}
+  constructor(private translate : TranslateService,private config:ConfigurationService){}
 }
