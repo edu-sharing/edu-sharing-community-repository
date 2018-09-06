809fcc07e4c918a5bc9c41613d90a2259d33a17a
simon <simon@edu-sharing.net>
2018-09-06 14:56:14 +0200

    refactor:usage dao just one method feat:usage dao show app info data

--- Backend/src/org/edu_sharing/restservices/UsageDao.java
+++ Backend/src/org/edu_sharing/restservices/UsageDao.java
@@ -6,13 +6,15 @@ import java.util.List;
 
 import org.edu_sharing.repository.client.tools.CCConstants;
 import org.edu_sharing.repository.server.MCAlfrescoBaseClient;
+import org.edu_sharing.repository.server.tools.ApplicationInfoList;
 import org.edu_sharing.restservices.collection.v1.model.Collection;
+import org.edu_sharing.restservices.usage.v1.model.Usages;
 import org.edu_sharing.restservices.usage.v1.model.Usages.Usage;
 import org.edu_sharing.service.usage.Usage2Service;
 
 public class UsageDao {
 
-	
+
 	RepositoryDao repoDao;
 	
 	MCAlfrescoBaseClient baseClient;
@@ -29,26 +31,7 @@ public class UsageDao {
 			List<Usage> result = new ArrayList<Usage>();
 			
 			for(org.edu_sharing.service.usage.Usage usage : new Usage2Service().getUsages(appId)){
-				Usage usageResult = new Usage();
-				usageResult.setAppId(usage.getLmsId());
-
-				
-				//filter out usage info cause of security reasons
-				String currentUser = baseClient.getAuthenticationInfo().get(CCConstants.AUTH_USERNAME);
-				if(baseClient.isAdmin(currentUser) || currentUser.equals(usage.getAppUser())){
-					usageResult.setAppUser(usage.getAppUser());
-					usageResult.setAppUserMail(usage.getAppUserMail());
-				}
-				
-				usageResult.setCourseId(usage.getCourseId());
-				usageResult.setDistinctPersons(usage.getDistinctPersons());
-				usageResult.setGuid(usage.getGuid());
-				usageResult.setNodeId(usage.getNodeId());
-				usageResult.setParentNodeId(usage.getParentNodeId());
-				usageResult.setResourceId(usage.getResourceId());
-				usageResult.setUsageCounter(usage.getUsageCounter());
-				usageResult.setUsageVersion(usage.getUsageVersion());
-				usageResult.setUsageXmlParams(usage.getUsageXmlParams());
+				Usage usageResult = convertUsage(usage);
 				result.add(usageResult);
 			}
 			
@@ -57,30 +40,42 @@ public class UsageDao {
 			throw DAOException.mapping(e);
 		}
 	}
-	
+
+	private Usage convertUsage(org.edu_sharing.service.usage.Usage usage) throws Exception {
+		Usage usageResult = new Usage();
+		usageResult.setAppId(usage.getLmsId());
+
+		try {
+			usageResult.setAppType(ApplicationInfoList.getRepositoryInfoById(usage.getLmsId()).getType());
+			usageResult.setAppSubtype(ApplicationInfoList.getRepositoryInfoById(usage.getLmsId()).getSubtype());
+		}catch(Throwable t){
+
+		}
+
+		//filter out usage info cause of security reasons
+		String currentUser = baseClient.getAuthenticationInfo().get(CCConstants.AUTH_USERNAME);
+		if(baseClient.isAdmin(currentUser) || currentUser.equals(usage.getAppUser())){
+			usageResult.setAppUser(usage.getAppUser());
+			usageResult.setAppUserMail(usage.getAppUserMail());
+		}
+
+		usageResult.setCourseId(usage.getCourseId());
+		usageResult.setDistinctPersons(usage.getDistinctPersons());
+		usageResult.setGuid(usage.getGuid());
+		usageResult.setNodeId(usage.getNodeId());
+		usageResult.setParentNodeId(usage.getParentNodeId());
+		usageResult.setResourceId(usage.getResourceId());
+		usageResult.setUsageCounter(usage.getUsageCounter());
+		usageResult.setUsageVersion(usage.getUsageVersion());
+		usageResult.setUsageXmlParams(usage.getUsageXmlParams());
+		return usageResult;
+	}
+
 	public List<Usage> getUsagesByCourse(String appId, String courseId) throws DAOException{
 		try{
 			List<Usage> result = new ArrayList<Usage>();
 			for(org.edu_sharing.service.usage.Usage usage : new Usage2Service().getUsagesByCourse(appId, courseId)){
-				Usage usageResult = new Usage();
-				usageResult.setAppId(usage.getLmsId());
-				
-				//filter out usage info cause of security reasons
-				String currentUser = baseClient.getAuthenticationInfo().get(CCConstants.AUTH_USERNAME);
-				if(baseClient.isAdmin(currentUser) || currentUser.equals(usage.getAppUser())){
-					usageResult.setAppUser(usage.getAppUser());
-					usageResult.setAppUserMail(usage.getAppUserMail());
-				}
-				
-				usageResult.setCourseId(usage.getCourseId());
-				usageResult.setDistinctPersons(usage.getDistinctPersons());
-				usageResult.setGuid(usage.getGuid());
-				usageResult.setNodeId(usage.getNodeId());
-				usageResult.setParentNodeId(usage.getParentNodeId());
-				usageResult.setResourceId(usage.getResourceId());
-				usageResult.setUsageCounter(usage.getUsageCounter());
-				usageResult.setUsageVersion(usage.getUsageVersion());
-				usageResult.setUsageXmlParams(usage.getUsageXmlParams());
+				Usage usageResult = convertUsage(usage);
 				result.add(usageResult);
 			}
 		return result;
@@ -96,25 +91,7 @@ public class UsageDao {
 			
 			List<Usage> result = new ArrayList<Usage>();
 			for(org.edu_sharing.service.usage.Usage usage : new Usage2Service().getUsageByParentNodeId(null, null, nodeId)){
-				Usage usageResult = new Usage();
-				usageResult.setAppId(usage.getLmsId());
-				
-				//filter out usage info cause of security reasons
-				String currentUser = baseClient.getAuthenticationInfo().get(CCConstants.AUTH_USERNAME);
-				if(baseClient.isAdmin(currentUser) || currentUser.equals(usage.getAppUser())){
-					usageResult.setAppUser(usage.getAppUser());
-					usageResult.setAppUserMail(usage.getAppUserMail());
-				}
-				
-				usageResult.setCourseId(usage.getCourseId());
-				usageResult.setDistinctPersons(usage.getDistinctPersons());
-				usageResult.setGuid(usage.getGuid());
-				usageResult.setNodeId(usage.getNodeId());
-				usageResult.setParentNodeId(usage.getParentNodeId());
-				usageResult.setResourceId(usage.getResourceId());
-				usageResult.setUsageCounter(usage.getUsageCounter());
-				usageResult.setUsageVersion(usage.getUsageVersion());
-				usageResult.setUsageXmlParams(usage.getUsageXmlParams());
+				Usage usageResult = convertUsage(usage);
 				result.add(usageResult);
 			}
 			return result;
--- Backend/src/org/edu_sharing/restservices/usage/v1/model/Usages.java
+++ Backend/src/org/edu_sharing/restservices/usage/v1/model/Usages.java
@@ -1,5 +1,6 @@
 package org.edu_sharing.restservices.usage.v1.model;
 
+import java.util.Calendar;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -28,159 +29,161 @@ public class Usages {
 	}
 	
 	public static class Usage{
-		private java.lang.String appUser;
+		private String appUser;
 
-	    private java.lang.String appUserMail;
+	    private String appUserMail;
 
-	    private java.lang.String courseId;
+	    private String courseId;
 
-	    private java.lang.Integer distinctPersons;
+	    private Integer distinctPersons;
 
-	    private java.util.Calendar fromUsed;
+	    private Calendar fromUsed;
 
-	    private java.lang.String appId;
+	    private String appId;
 
-	    private java.lang.String nodeId;
+	    private String nodeId;
 
-	    private java.util.Calendar toUsed;
+	    private Calendar toUsed;
 
-	    private java.lang.Integer usageCounter;
+	    private Integer usageCounter;
 
-	    private java.lang.String parentNodeId;
+	    private String parentNodeId;
 
-	    private java.lang.String usageVersion;
+	    private String usageVersion;
 
-	    private java.lang.String usageXmlParams;
+	    private String usageXmlParams;
 
-	    private java.lang.String resourceId;
+	    private String resourceId;
 	    
 	    private String guid;
+		private String appSubtype;
+		private String appType;
 
-	    
-	    @ApiModelProperty(required = true, value = "")
+
+		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("appUser")
-		public java.lang.String getAppUser() {
+		public String getAppUser() {
 			return appUser;
 		}
 
-		public void setAppUser(java.lang.String appUser) {
+		public void setAppUser(String appUser) {
 			this.appUser = appUser;
 		}
 
 		
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("appUserMail")
-		public java.lang.String getAppUserMail() {
+		public String getAppUserMail() {
 			return appUserMail;
 		}
 
-		public void setAppUserMail(java.lang.String appUserMail) {
+		public void setAppUserMail(String appUserMail) {
 			this.appUserMail = appUserMail;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("courseId")
-		public java.lang.String getCourseId() {
+		public String getCourseId() {
 			return courseId;
 		}
 
-		public void setCourseId(java.lang.String courseId) {
+		public void setCourseId(String courseId) {
 			this.courseId = courseId;
 		}
 
 		@ApiModelProperty(required = false, value = "")
 		@JsonProperty("distinctPersons")
-		public java.lang.Integer getDistinctPersons() {
+		public Integer getDistinctPersons() {
 			return distinctPersons;
 		}
 
-		public void setDistinctPersons(java.lang.Integer distinctPersons) {
+		public void setDistinctPersons(Integer distinctPersons) {
 			this.distinctPersons = distinctPersons;
 		}
 
 		
-		public java.util.Calendar getFromUsed() {
+		public Calendar getFromUsed() {
 			return fromUsed;
 		}
 
-		public void setFromUsed(java.util.Calendar fromUsed) {
+		public void setFromUsed(Calendar fromUsed) {
 			this.fromUsed = fromUsed;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("appId")
-		public java.lang.String getAppId() {
+		public String getAppId() {
 			return appId;
 		}
 		
-		public void setAppId(java.lang.String appId) {
+		public void setAppId(String appId) {
 			this.appId = appId;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("nodeId")
-		public java.lang.String getNodeId() {
+		public String getNodeId() {
 			return nodeId;
 		}
 
-		public void setNodeId(java.lang.String nodeId) {
+		public void setNodeId(String nodeId) {
 			this.nodeId = nodeId;
 		}
 
-		public java.util.Calendar getToUsed() {
+		public Calendar getToUsed() {
 			return toUsed;
 		}
 
-		public void setToUsed(java.util.Calendar toUsed) {
+		public void setToUsed(Calendar toUsed) {
 			this.toUsed = toUsed;
 		}
 
 		
-		public java.lang.Integer getUsageCounter() {
+		public Integer getUsageCounter() {
 			return usageCounter;
 		}
 
-		public void setUsageCounter(java.lang.Integer usageCounter) {
+		public void setUsageCounter(Integer usageCounter) {
 			this.usageCounter = usageCounter;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("parentNodeId")
-		public java.lang.String getParentNodeId() {
+		public String getParentNodeId() {
 			return parentNodeId;
 		}
 
-		public void setParentNodeId(java.lang.String parentNodeId) {
+		public void setParentNodeId(String parentNodeId) {
 			this.parentNodeId = parentNodeId;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("usageVersion")
-		public java.lang.String getUsageVersion() {
+		public String getUsageVersion() {
 			return usageVersion;
 		}
 
-		public void setUsageVersion(java.lang.String usageVersion) {
+		public void setUsageVersion(String usageVersion) {
 			this.usageVersion = usageVersion;
 		}
 
 		@ApiModelProperty(required = false, value = "")
 		@JsonProperty("usageXmlParams")
-		public java.lang.String getUsageXmlParams() {
+		public String getUsageXmlParams() {
 			return usageXmlParams;
 		}
 
-		public void setUsageXmlParams(java.lang.String usageXmlParams) {
+		public void setUsageXmlParams(String usageXmlParams) {
 			this.usageXmlParams = usageXmlParams;
 		}
 
 		@ApiModelProperty(required = true, value = "")
 		@JsonProperty("resourceId")
-		public java.lang.String getResourceId() {
+		public String getResourceId() {
 			return resourceId;
 		}
 
-		public void setResourceId(java.lang.String resourceId) {
+		public void setResourceId(String resourceId) {
 			this.resourceId = resourceId;
 		}
 	    
@@ -192,6 +195,22 @@ public class Usages {
 
 		public void setGuid(String guid) {
 			this.guid = guid;
+		}
+
+        public void setAppSubtype(String appSubtype) {
+            this.appSubtype = appSubtype;
+        }
+
+        public String getAppSubtype() {
+            return appSubtype;
+        }
+
+		public void setAppType(String appType) {
+			this.appType = appType;
+		}
+
+		public String getAppType() {
+			return appType;
 		}
 	}
 }
