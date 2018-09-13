7bf6b991a2c5a7f5a4ca125561913d336ab37060
Stepan Boldt <boldt@edu-sharing.com>
2018-09-13 11:58:11 +0200

    feat: new component registrierung 5_0 ESPUB-223

--- Frontend/src/app/modules/register/declarations.ts
+++ Frontend/src/app/modules/register/declarations.ts
@@ -1,9 +1,13 @@
 import {RegisterComponent} from './register.component';
 import {RegisterDoneComponent} from "./register-done/register-done.component";
 import {RegisterFormComponent} from "./register-form/register-form.component";
+import {RegisterRequireComponent} from "./register-require/register-require.component";
+import {RegisterResetPasswordComponent} from "./register-reset-password/register-reset-password.component";
 
 export const DECLARATIONS_REGISTER = [
     RegisterComponent,
     RegisterFormComponent,
-    RegisterDoneComponent
+    RegisterDoneComponent,
+    RegisterRequireComponent,
+    RegisterResetPasswordComponent
 ];
