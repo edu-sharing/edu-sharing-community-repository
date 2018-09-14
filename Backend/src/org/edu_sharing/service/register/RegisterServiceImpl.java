3b16f3bdee9f070b068aa9ccdbe95e19354b4454
simon <simon@edu-sharing.net>
2018-09-14 16:21:12 +0200

    feat:new template for register, new template variables, auto-login after entering key, fixed permission issues for registration ESPUB-223

--- Backend/src/org/edu_sharing/service/register/RegisterServiceImpl.java
+++ Backend/src/org/edu_sharing/service/register/RegisterServiceImpl.java
@@ -4,14 +4,17 @@ import com.sun.star.lang.IllegalArgumentException;
 import io.swagger.models.auth.In;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.cache.SimpleCache;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.security.MutableAuthenticationService;
 import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.namespace.QName;
 import org.apache.commons.lang.RandomStringUtils;
+import org.edu_sharing.alfresco.authentication.subsystems.SubsystemChainingAuthenticationService;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
 import org.edu_sharing.repository.client.tools.CCConstants;
+import org.edu_sharing.repository.server.AuthenticationTool;
 import org.edu_sharing.repository.server.AuthenticationToolAPI;
 import org.edu_sharing.repository.server.authentication.Context;
 import org.edu_sharing.repository.server.tools.Mail;
@@ -29,7 +32,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterServiceImpl implements RegisterService {
-    private static int KEY_lENGTH = 16;
+    private static int KEY_LENGTH = 16;
     public final static SimpleCache<String,RegisterInformation> registerUserCache = (SimpleCache)  AlfAppContextGate.getApplicationContext().getBean("eduSharingRegisterUserCache");
 
     private final PersonService personService;
@@ -51,6 +54,10 @@ public class RegisterServiceImpl implements RegisterService {
         Map<String,String> replace=new HashMap<>();
         replace.put("link",URLTool.getNgComponentsUrl()+"register/done/"+URLEncoder.encode(key)+"/"+ URLEncoder.encode(info.getEmail()));
         replace.put("key",key);
+        replace.put("firstName",info.getFirstName());
+        replace.put("lastName",info.getLastName());
+        replace.put("email",info.getEmail());
+        replace.put("organization",info.getOrganization());
         Mail mail=new Mail();
         ServletContext context = Context.getCurrentInstance().getRequest().getSession().getServletContext();
         mail.sendMailHtml(
@@ -67,24 +74,39 @@ public class RegisterServiceImpl implements RegisterService {
         RegisterInformation info = registerUserCache.get(key);
         if(info==null)
             throw new InvalidKeyException();
-        return storeUser(info);
+        String result = storeUser(info);
+        registerUserCache.remove(key);
+        // authenticate the newly activated user automatically
+        authenticate(info);
+        return result;
+
     }
+
+    private void authenticate(RegisterInformation info) throws Exception {
+        AuthenticationTool authTool=new AuthenticationToolAPI();
+        HashMap<String, String> validatedAuth = authTool.createNewSession(info.getEmail(), info.getPassword());
+        authTool.storeAuthInfoInSession(info.getEmail(), validatedAuth.get(CCConstants.AUTH_TICKET),CCConstants.AUTH_TYPE_DEFAULT, Context.getCurrentInstance().getRequest().getSession(true));
+        authService.authenticate(info.getEmail(),info.getPassword().toCharArray());
+    }
+
     protected String storeUser(RegisterInformation info){
-        Map<QName, Serializable> map = new HashMap<>();
-        String authority = info.getEmail();
-        map.put(ContentModel.PROP_USERNAME,authority);
-        map.put(ContentModel.PROP_FIRSTNAME,info.getFirstName());
-        map.put(ContentModel.PROP_LASTNAME,info.getLastName());
-        map.put(ContentModel.PROP_ORGANIZATION,info.getOrganization());
-        map.put(ContentModel.PROP_EMAIL,info.getEmail());
-        try {
-            authService.createAuthentication(authority, info.getPassword().toCharArray());
-            NodeRef result = personService.createPerson(map);
-            return result.getId();
-        }catch(Throwable t){
-            // check for duplicates
-            throw t;
-        }
+        return AuthenticationUtil.runAsSystem(()-> {
+            Map<QName, Serializable> map = new HashMap<>();
+            String authority = info.getEmail();
+            map.put(ContentModel.PROP_USERNAME, authority);
+            map.put(ContentModel.PROP_FIRSTNAME, info.getFirstName());
+            map.put(ContentModel.PROP_LASTNAME, info.getLastName());
+            map.put(ContentModel.PROP_ORGANIZATION, info.getOrganization());
+            map.put(ContentModel.PROP_EMAIL, info.getEmail());
+            try {
+                authService.createAuthentication(authority, info.getPassword().toCharArray());
+                NodeRef result = personService.createPerson(map);
+                return result.getId();
+            } catch (Throwable t) {
+                // check for duplicates
+                throw t;
+            }
+        });
     }
     @Override
     public void register(RegisterInformation info) throws DuplicateAuthorityException, Throwable{
@@ -97,7 +119,7 @@ public class RegisterServiceImpl implements RegisterService {
 
     private String addToCache(RegisterInformation info) {
         while(true){
-            String id=RandomStringUtils.random(KEY_lENGTH,true,true);
+            String id=RandomStringUtils.random(KEY_LENGTH,true,true);
             if(registerUserCache.contains(id))
                 continue;
             registerUserCache.put(id,info);
