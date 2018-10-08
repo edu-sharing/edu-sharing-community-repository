c877efe03da0996f0d234f3879021b92726f9681
Torsten Simon <simon@metaventis.com>
2018-10-08 12:26:02 +0200

    fix:actionbar hide variant + collection in guest mode

--- Frontend/src/app/common/ui/actionbar/actionbar.component.ts
+++ Frontend/src/app/common/ui/actionbar/actionbar.component.ts
@@ -67,7 +67,7 @@ export class ActionbarComponent{
       this.optionsAlways=UIHelper.filterToggleOptions(options,false).slice(0,this.getNumberOptions());
     }
     this.optionsMenu=this.hideActionOptions(UIHelper.filterToggleOptions(options,false),this.optionsAlways);
-    if(this.optionsMenu.length<2){
+    if(this.optionsMenu.length<1){
       this.optionsAlways=this.optionsAlways.concat(this.optionsMenu);
       this.optionsMenu=[];
     }
