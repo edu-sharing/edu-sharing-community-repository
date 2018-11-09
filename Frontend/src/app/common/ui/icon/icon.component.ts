e4ce3e87222fa16f135ce268bb8ac7624ce5db04
simon <simon@edu-sharing.net>
2018-11-09 11:17:59 +0100

    fix:icon component failed on reload

--- Frontend/src/app/common/ui/icon/icon.component.ts
+++ Frontend/src/app/common/ui/icon/icon.component.ts
@@ -2,23 +2,26 @@
  * Created by Torsten on 13.01.2017.
  */
 
-import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ContentChildren, ElementRef, Input, Type, ViewChild} from '@angular/core';
 import {ConfigurationService} from "../../services/configuration.service";
 
 @Component({
   selector: 'icon',
   templateUrl: 'icon.component.html',
 })
-export class IconComponent implements AfterViewInit{
-    @ViewChild('icon') icon: ElementRef;
-    class = 'material-icons';
-    constructor() {
-    }
-    ngAfterViewInit() {
-        const id=this.icon.nativeElement.textContent;
+export class IconComponent{
+    _class = '';
+    _id= '';
+    @Input() set id(id:string){
         if(id.startsWith("edu-")){
-            this.class="edu-icons";
-            this.icon.nativeElement.innerText=id.substr(4);
+            this._class="edu-icons";
+            id=id.substr(4);
         }
+        else{
+            this._class="material-icons";
+        }
+        this._id=id;
+    }
+    constructor() {
     }
 }
