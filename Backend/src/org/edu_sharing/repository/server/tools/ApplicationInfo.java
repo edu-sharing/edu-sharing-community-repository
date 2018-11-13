5bebec4ad8daa43d4eeb3f603308438b92512e4c
simon <simon@edu-sharing.net>
2018-11-13 10:32:01 +0100

    refractor:remove old blowfish key in homeApp

--- Backend/src/org/edu_sharing/repository/server/tools/ApplicationInfo.java
+++ Backend/src/org/edu_sharing/repository/server/tools/ApplicationInfo.java
@@ -208,11 +208,7 @@ public class ApplicationInfo implements Comparable<ApplicationInfo>{
 	private String subtype = null;
 	
 	private String publicfolderid = null;
-	
-	private String blowfishkey = null;
-	
-	private String blowfishiv = null;
-	
+
 	private String searchclass = null;
 	
 	private String searchService = null;
@@ -388,11 +384,7 @@ public class ApplicationInfo implements Comparable<ApplicationInfo>{
 		subtype  = PropertiesHelper.getProperty("subtype", appFile, PropertiesHelper.XML);
 	
 		publicfolderid = PropertiesHelper.getProperty("publicfolderid", appFile, PropertiesHelper.XML);
-		
-		blowfishkey = PropertiesHelper.getProperty("blowfishkey", appFile, PropertiesHelper.XML);
-		
-		blowfishiv = PropertiesHelper.getProperty("blowfishiv", appFile, PropertiesHelper.XML);	
-		
+
 		searchclass = PropertiesHelper.getProperty(KEY_SEARCHCLASS, appFile, PropertiesHelper.XML);	
 		
 		searchService = PropertiesHelper.getProperty("searchService", appFile, PropertiesHelper.XML);	
@@ -611,16 +603,6 @@ public class ApplicationInfo implements Comparable<ApplicationInfo>{
 
 	public String getPublicfolderid() {
 		return publicfolderid;
-	}
-	
-	public String getBlowfishkey() {
-		
-		return blowfishkey;
-	}
-
-	public String getBlowfishiv() {
-		
-		return blowfishiv;
 	}
 
 	/**
