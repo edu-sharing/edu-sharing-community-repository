8e2c1cda4a1f2bdb1365d71688f6415ca05b8031
Daniel Rudolph <rudolph@metaventis.com>
2018-11-13 14:18:55 +0100

    Merge branch 'feature/4_2_minor' into feature/alfresco52
    fix fingerprint method

--- Frontend/src/app/common/rest/services/rest-search.service.ts
+++ Frontend/src/app/common/rest/services/rest-search.service.ts
@@ -66,11 +66,11 @@ export class RestSearchService extends AbstractRestService{
       return this.connector.post<NodeList>(q,body,this.connector.getRequestOptions());
     }
 
-    searchFingerprint(nodeid:string,request: any=null,repository = RestConstants.HOME_REPOSITORY) : Observable<NodeList> {
+    searchFingerprint(nodeid:string,request: any=null,repository = RestConstants.HOME_REPOSITORY) {
         let q=this.connector.createUrlNoEscape('search/:version/queries/:repository/fingerprint/:nodeid/?:request',repository,[
             [":nodeid",encodeURIComponent(nodeid)],
             [":request",this.connector.createRequestString(request)]
         ]);
-        return this.connector.post(q,null,this.connector.getRequestOptions()).map((response: Response) => response.json());
+        return this.connector.post<NodeList>(q,null,this.connector.getRequestOptions());
     }
 }
