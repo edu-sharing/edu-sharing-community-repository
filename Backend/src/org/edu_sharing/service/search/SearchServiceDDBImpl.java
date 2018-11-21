0df93dbeaa6afc620596d57066b17d915cb779a6
Daniel Rudolph <rudolph@metaventis.com>
2018-11-21 19:23:47 +0100

    fix ddb license mapping

--- Backend/src/org/edu_sharing/service/search/SearchServiceDDBImpl.java
+++ Backend/src/org/edu_sharing/service/search/SearchServiceDDBImpl.java
@@ -280,6 +280,35 @@ public class SearchServiceDDBImpl extends SearchServiceAdapter{
 			}catch(Throwable t) {}
 			
 			
+			try {
+				JSONObject webresource = allJson.getJSONObject("edm").getJSONObject("RDF").getJSONObject("WebResource");
+				String licenseUrl = webresource.getJSONObject("rights").getString("@resource");
+				System.out.println("licenseUrl:" + licenseUrl);
+				
+				for(Map.Entry<String, String> entry : CCConstants.getLicenseMap().entrySet()) {
+					String matching = entry.getKey().replace("/1.0/deed.", "");
+					matching = matching.replaceFirst("https://", "");
+					matching = matching.replaceFirst("http://", "");
+					System.out.println("matchingUrl:" + matching);
+					
+					if(licenseUrl != null) {
+						String tempLicenseurl = licenseUrl.replaceFirst("https://", "");
+						tempLicenseurl = tempLicenseurl.replaceFirst("http://", "");
+						if(tempLicenseurl.startsWith(matching)) {
+							System.out.println("matched");
+							properties.put(CCConstants.CCM_PROP_IO_COMMONLICENSE_KEY,entry.getValue());
+						}
+					}
+					
+				}
+				
+				
+				
+			}catch(Throwable e) {
+				
+			}
+			
+			
 			String previewUrl;
 			try {
 				previewUrl=DDB_API+ "/binary/" + allJson.getJSONObject("preview").getJSONObject("thumbnail").getString("@href")+"?oauth_consumer_key=" + URLEncoder.encode(this.APIKey, "UTF-8");
