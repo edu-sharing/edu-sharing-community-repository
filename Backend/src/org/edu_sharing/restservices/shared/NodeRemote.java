5d2b4f12b4a1f34736ddbbb2c768dffd51d2b56a
Daniel Rudolph <rudolph@metaventis.com>
2018-12-04 12:20:33 +0100

    fix swagger prepareUsage return type

--- Backend/src/org/edu_sharing/restservices/shared/NodeRemote.java
+++ Backend/src/org/edu_sharing/restservices/shared/NodeRemote.java
@@ -1,13 +1,23 @@
 package org.edu_sharing.restservices.shared;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description = "")
 public class NodeRemote {
 	Node node;
 	Node remote;
 	
+	@ApiModelProperty(required = true, value = "")
+	@JsonProperty("node")
 	public Node getNode() {
 		return node;
 	}
 	
+	@ApiModelProperty(required = true, value = "")
+	@JsonProperty("remote")
 	public Node getRemote() {
 		return remote;
 	}
