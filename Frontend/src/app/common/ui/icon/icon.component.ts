65c863a74ee47408e156d41ca28dfb5e6225e331
Torsten Simon <simon@metaventis.com>
2018-12-17 10:58:34 +0100

    feat:prepared icon component to support aria labels

--- Frontend/src/app/common/ui/icon/icon.component.ts
+++ Frontend/src/app/common/ui/icon/icon.component.ts
@@ -4,14 +4,23 @@
 
 import {AfterViewInit, Component, ContentChildren, Directive, ElementRef, Input, Type, ViewChild} from '@angular/core';
 import {ConfigurationService} from "../../services/configuration.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Directive({
   selector: '[icon]',
 })
 export class IconComponent{
     private element: ElementRef;
+    private _id: string;
+    private _aria: boolean;
+    @Input() set aria(aria:boolean){
+        this._aria=aria;
+        this.updateAria();
+    }
     @Input() set iconId(id: string){
+        this._id=id;
         let css:string;
+        this.updateAria();
         if(id.startsWith("edu-")){
             css="edu-icons";
             id=id.substr(4);
@@ -25,8 +34,21 @@ export class IconComponent{
     @Input() set id(id:string){
 
     }
-    constructor(element:ElementRef) {
+    constructor(element:ElementRef,private translate:TranslateService) {
         console.log(element);
         this.element=element;
+    }
+
+    private updateAria() {
+        this.element.nativeElement.removeAttribute('aria-label');
+        this.element.nativeElement.removeAttribute('aria-hidden');
+        if(this._aria){
+            this.translate.get('ICON_LABELS.'+this._id).subscribe((lang)=> {
+                this.element.nativeElement.setAttribute('aria-label',lang);
+            });
+        }
+        else{
+            this.element.nativeElement.setAttribute('aria-hidden',true);
+        }
     }
 }
