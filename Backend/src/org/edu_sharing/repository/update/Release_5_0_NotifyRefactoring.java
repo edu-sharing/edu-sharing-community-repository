682d744da9bf5fb767ef6953c24f43cee1ab0351
Daniel Rudolph <rudolph@metaventis.com>
2019-01-14 15:05:22 +0100

    refactoring: notifys: restore history on version revert, remove history on copyrefactoring: notifys: updater

--- Backend/src/org/edu_sharing/repository/update/Release_5_0_NotifyRefactoring.java
+++ Backend/src/org/edu_sharing/repository/update/Release_5_0_NotifyRefactoring.java
@@ -1,7 +1,17 @@
 package org.edu_sharing.repository.update;
 
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -10,8 +20,17 @@ import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.service.namespace.RegexQNamePattern;
+import org.apache.log4j.Logger;
+import org.edu_sharing.repository.client.rpc.ACE;
+import org.edu_sharing.repository.client.rpc.ACL;
+import org.edu_sharing.repository.client.rpc.Notify;
+import org.edu_sharing.repository.client.rpc.User;
 import org.edu_sharing.repository.client.tools.CCConstants;
+import org.edu_sharing.repository.server.MCAlfrescoAPIClient;
 import org.edu_sharing.service.Constants;
+
+import com.google.gson.Gson;
+
 
 public class Release_5_0_NotifyRefactoring extends UpdateAbstract{
 
@@ -24,6 +43,13 @@ public class Release_5_0_NotifyRefactoring extends UpdateAbstract{
 	SearchService searchService = serviceRegistry.getSearchService();
 	NodeService nodeService = serviceRegistry.getNodeService();
 	
+	MCAlfrescoAPIClient apiClient;
+	
+	Logger logger = Logger.getLogger(Release_5_0_NotifyRefactoring.class);
+	
+	public Release_5_0_NotifyRefactoring(PrintWriter out) {
+		this.out = out;
+	}
 	
 	@Override
 	public void execute() {
@@ -46,7 +72,7 @@ public class Release_5_0_NotifyRefactoring extends UpdateAbstract{
 	public void run() {
 		
 		
-		
+		apiClient = new MCAlfrescoAPIClient();
 		
 		SearchParameters sp = new SearchParameters();
 		sp.addStore(Constants.storeRef);
@@ -65,8 +91,96 @@ public class Release_5_0_NotifyRefactoring extends UpdateAbstract{
 			sp.setSkipCount(rs.getStart() + maxItems);
 		}
 		rs = searchService.query(sp);
+		
 		for(NodeRef nodeRef : rs.getNodeRefs()) {
+			
+			logger.info("migrating " + nodeService.getProperty(nodeRef, ContentModel.PROP_NAME));
+			
 			List<ChildAssociationRef> notifyParentAssocs = nodeService.getParentAssocs(nodeRef, QName.createQName(CCConstants.CCM_TYPE_NOTIFY), RegexQNamePattern.MATCH_ALL);
+			
+			Map<NodeRef, Map<QName,Serializable>> notifyProps = new TreeMap<NodeRef,Map<QName,Serializable>>();
+			
+		
+			for(ChildAssociationRef childRef : notifyParentAssocs) {
+				NodeRef notifyRef = childRef.getParentRef();
+				Map<QName,Serializable> properties = nodeService.getProperties(notifyRef);
+				notifyProps.put(notifyRef, properties);
+			}
+			
+			List<Map.Entry<NodeRef, Map<QName,Serializable>>> toSort = new ArrayList<Map.Entry<NodeRef, Map<QName,Serializable>>>();
+			
+			for(Map.Entry<NodeRef, Map<QName,Serializable>> entry : notifyProps.entrySet()) {
+				toSort.add(entry);
+			}
+			
+			Collections.sort(toSort, new Comparator<Map.Entry<NodeRef, Map<QName,Serializable>>>(){
+				@Override
+				public int compare(Entry<NodeRef, Map<QName, Serializable>> o1,
+						Entry<NodeRef, Map<QName, Serializable>> o2) {
+					Date o1Created = (Date)o1.getValue().get(ContentModel.PROP_CREATED);
+					Date o2Created = (Date)o2.getValue().get(ContentModel.PROP_CREATED);
+					return o1Created.compareTo(o2Created);
+				}
+			});
+			
+			/**
+			 * transform notify history
+			 */
+			int i = 0;
+			for(Map.Entry<NodeRef, Map<QName,Serializable>> entry : toSort) {
+				logger.info(" transforming notify from: " + entry.getValue().get(ContentModel.PROP_CREATED));
+				Gson gson = new Gson();
+				Notify n = new Notify();
+				try {
+					//get acl from notify cause its the same as io
+					ACL acl = apiClient.getPermissions(entry.getKey().getId());
+					List<ACE> directlySetAces = new ArrayList<ACE>();
+					for(ACE ace : acl.getAces()) {
+						if(!ace.isInherited()) {
+							directlySetAces.add(ace);
+						}
+					}
+					Date created = (Date)nodeService.getProperty(entry.getKey(), ContentModel.PROP_CREATED);
+					String action = (String)nodeService.getProperty(entry.getKey(), QName.createQName(CCConstants.CCM_PROP_NOTIFY_ACTION));
+					String user = (String)nodeService.getProperty(entry.getKey(), QName.createQName(CCConstants.CCM_PROP_NOTIFY_USER));
+					acl.setAces(directlySetAces.toArray(new ACE[]{}));
+					n.setAcl(acl);
+					n.setCreated(created);
+					n.setNotifyAction(action);
+					n.setNotifyUser(user);
+					User u = new User();
+					u.setAuthorityName(user);
+					u.setUsername(user);
+					n.setUser(u);
+					
+					if(!nodeService.hasAspect(nodeRef, QName.createQName(CCConstants.CCM_ASPECT_PERMISSION_HISTORY))) {
+						nodeService.addAspect(nodeRef, QName.createQName(CCConstants.CCM_ASPECT_PERMISSION_HISTORY), null);
+					}
+					String jsonStringACL = gson.toJson(n);
+					List<String> history = (List<String>)nodeService.getProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_PH_HISTORY));
+					history = (history == null)? new ArrayList<String>() : history;
+					history.add(jsonStringACL);
+					nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_PH_HISTORY), new ArrayList(history));
+				
+					/**
+					 * last one will be current
+					 */
+					if(i == (toSort.size() - 1)) {
+						nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_PH_ACTION), action);
+						nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_PH_USER), user);
+						nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_PH_MODIFIED), created);
+					}
+				
+				} catch (Exception e1) {
+					logger.error(e1.getMessage(), e1);
+				}
+				i++;
+				
+				
+			}
+			
+			toSort.get(toSort.size() - 1);
+			
 		}
 	}
 	
