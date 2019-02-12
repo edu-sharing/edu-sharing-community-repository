d15c63b7b7170999e5583bd682be95faa7a67701
Daniel Rudolph <rudolph@metaventis.com>
2019-02-12 12:27:02 +0100

    cherry pick from release_4_0 preview job fixes

--- Backend/src/org/edu_sharing/alfresco/jobs/PreviewJob.java
+++ Backend/src/org/edu_sharing/alfresco/jobs/PreviewJob.java
@@ -1,9 +1,16 @@
 package org.edu_sharing.alfresco.jobs;
 
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.lock.mem.LockState;
@@ -14,7 +21,10 @@ import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ActionService;
 import org.alfresco.service.cmr.action.ActionStatus;
 import org.alfresco.service.cmr.repository.ContentReader;
+import org.alfresco.service.cmr.repository.ContentService;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
 import org.edu_sharing.repository.client.tools.CCConstants;
@@ -24,20 +34,101 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
 
+import com.coremedia.iso.IsoFile;
+import com.coremedia.iso.boxes.Box;
+import com.coremedia.iso.boxes.MovieBox;
+import com.coremedia.iso.boxes.TrackBox;
+import com.coremedia.iso.boxes.TrackHeaderBox;
+
 public class PreviewJob implements Job {
 
 	ServiceRegistry serviceRegistry = (ServiceRegistry) AlfAppContextGate.getApplicationContext()
 			.getBean(ServiceRegistry.SERVICE_REGISTRY);
 	ActionService actionService = serviceRegistry.getActionService();
+	ContentService contentService = serviceRegistry.getContentService();
+	NodeService nodeService = serviceRegistry.getNodeService();
 
 	int maxRunning = 5;
 
 	Logger logger = Logger.getLogger(PreviewJob.class);
-
+	
 	/**
 	 * 5 seconds latency before starting
 	 */
 	long latency = 5000;
+	
+	
+	private void extractVideoImageMetadata(NodeRef nodeRef, String runAs) {
+		RunAsWork<Void> videoImageMetadataExtractor = new RunAsWork<Void>() {
+			@Override
+			public Void doWork() throws Exception {
+				ContentReader reader = contentService.getReader(nodeRef, ContentModel.PROP_CONTENT);
+				if(reader.getMimetype().contains("video")){
+					
+					ReadableByteChannel rbc = null;
+					try{
+						
+						rbc = Channels.newChannel(reader.getContentInputStream());
+						IsoFile isoFile = new IsoFile(rbc);
+						MovieBox moov = isoFile.getMovieBox();
+						if(moov != null && moov.getBoxes() != null){
+							for(Box b : moov.getBoxes()) {
+							   
+							    
+							    if(b instanceof TrackBox){
+							    	TrackHeaderBox thb = ((TrackBox)b).getTrackHeaderBox();
+							    	
+							    	if(thb.getWidth() > 0 && thb.getHeight() > 0){
+							    		nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_IO_WIDTH), thb.getWidth());
+							    		nodeService.setProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_IO_HEIGHT), thb.getHeight());
+							    	}
+							 
+							    }
+							    
+							}
+						}
+						
+					}catch(Exception e){
+						logger.error(e.getMessage(), e);
+					}finally{
+						
+						if(rbc != null){
+							try{
+							
+								rbc.close();
+							}catch(IOException e){
+								logger.error(e.getMessage(), e);
+							}
+						}
+					}
+				}
+				// alfresco does not read image size for all images, so we try to fix it
+				// trying to load not the whole image but just the bounding rect, see also:
+				// http://stackoverflow.com/questions/1559253/java-imageio-getting-image-dimensions-without-reading-the-entire-file
+				if(reader.getMimetype().contains("image")){
+					try{
+						try(ImageInputStream in = ImageIO.createImageInputStream(reader.getContentInputStream())){
+						    final Iterator<ImageReader> readers = ImageIO.getImageReaders(in);
+						    if (readers.hasNext()) {
+						        ImageReader imageReader = readers.next();
+						        try {
+						        	imageReader.setInput(in);
+						        	nodeService.setProperty(nodeRef, QName.createQName(CCConstants.EXIF_PROP_PIXELXDIMENSION), imageReader.getWidth(0));
+									nodeService.setProperty(nodeRef, QName.createQName(CCConstants.EXIF_PROP_PIXELYDIMENSION), imageReader.getHeight(0));
+						        } finally {
+						        	imageReader.dispose();
+						        }
+						    }
+						} 
+					}catch(Throwable t){}
+				}
+				return null;
+			}
+		};
+		
+		AuthenticationUtil.runAs(videoImageMetadataExtractor, runAs);
+	}
+	
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -67,14 +158,14 @@ public class PreviewJob implements Job {
 				logger.info("starting, nodes with actions:" + ActionObserver.getInstance().getNodeActionsMap().size());
 				ActionObserver.getInstance().removeInactiveActions();
 
-
+				
 				Map<NodeRef, List<Action>> m = ActionObserver.getInstance().getNodeActionsMap();
-
+				
 				synchronized (m) {
 					runJob(m);
 				}
-
-
+				
+				
 				logger.info("returning");
 				return null;
 			}
@@ -85,12 +176,6 @@ public class PreviewJob implements Job {
 	
 
 	
-
-	/**
-	 * 15 seconds latency before starting
-	 */
-	long latency = 15000;
-
 	public void runJob(Map<NodeRef, List<Action>> m) {
 		int countRunning = 0;
 		int countPending = 0;
@@ -104,7 +189,7 @@ public class PreviewJob implements Job {
 							|| action.getExecutionStatus() == ActionStatus.Pending) {
 						countRunning++;
 					}
-
+					
 					if (action.getExecutionStatus() == ActionStatus.Pending) {
 						countPending++;
 					}
@@ -160,6 +245,8 @@ public class PreviewJob implements Job {
 											.getParameterValue(ActionObserver.ACTION_OBSERVER_ADD_DATE);
 
 									if (System.currentTimeMillis() > (date.getTime() + latency)) {
+										
+										extractVideoImageMetadata(entry.getKey(),creator);
 										AuthenticationUtil.runAs(executeActionRunAs, creator);
 										logger.debug("finished action syncronously. nodeRef:" + entry.getKey()
 												+ " action status:" + action.getExecutionStatus()
@@ -185,7 +272,7 @@ public class PreviewJob implements Job {
 					}
 				}
 
-
+			
 		}
 	}
 }
