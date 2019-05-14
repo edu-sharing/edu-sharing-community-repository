84a2df60afccddb8ba22b0ebdb6f4e0353f2d2b0
Daniel Rudolph <rudolph@metaventis.com>
2019-05-14 17:54:53 +0200

    fix: Exception "An authority was not found for"

--- Backend/src/org/edu_sharing/repository/server/MCAlfrescoAPIClient.java
+++ Backend/src/org/edu_sharing/repository/server/MCAlfrescoAPIClient.java
@@ -2991,7 +2991,7 @@ public class MCAlfrescoAPIClient extends MCAlfrescoBaseClient {
                 		
                 		for (String containedAuthority : authorityService.getContainedAuthorities(null, key, true)) {
                 			
-                			authorityService.removeAuthority(groupName, containedAuthority);
+                			authorityService.removeAuthority(key, containedAuthority);
                 		}
 
                 		return null;
