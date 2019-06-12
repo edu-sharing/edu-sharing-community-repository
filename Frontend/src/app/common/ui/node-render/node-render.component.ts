c6ad0929903ad34e96126bf1c5f78dc3129c187d
Torsten Simon <simon@metaventis.com>
2019-06-12 10:03:02 +0200

    fix:reduce preload delay collection previews ESPUB-543

--- Frontend/src/app/common/ui/node-render/node-render.component.ts
+++ Frontend/src/app/common/ui/node-render/node-render.component.ts
@@ -374,7 +374,7 @@ export class NodeRenderComponent implements EventListener{
                 },
                 viewType:ListTableComponent.VIEW_TYPE_GRID_SMALL,
             };
-            UIHelper.injectAngularComponent(this.componentFactoryResolver,this.viewContainerRef,ListTableComponent,document.getElementsByTagName("collections")[0],data,1000);
+            UIHelper.injectAngularComponent(this.componentFactoryResolver,this.viewContainerRef,ListTableComponent,document.getElementsByTagName("collections")[0],data,250);
         },(error)=>{
 
         });
