40d2e5b14f5cf56c2e902808e37908c6b32b6da8
Torsten Simon <simon@metaventis.com>
2019-07-01 11:19:22 +0200

    fix:run deleted imports as system to avoid authentication errors

--- Backend/src/org/edu_sharing/repository/server/jobs/quartz/RemoveDeletedImportsJob.java
+++ Backend/src/org/edu_sharing/repository/server/jobs/quartz/RemoveDeletedImportsJob.java
@@ -33,6 +33,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.apache.log4j.Logger;
 import org.edu_sharing.repository.server.importer.ImportCleaner;
@@ -49,42 +50,42 @@ public class RemoveDeletedImportsJob extends AbstractJob {
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		super.execute(context);
 		logger.info("start");
-		try {
-			
-			Map jobDataMap = context.getJobDetail().getJobDataMap();
-			
-			String oaiBaseUrl = (String)jobDataMap.get(OAIConst.PARAM_OAI_BASE_URL);
-			if(oaiBaseUrl == null || oaiBaseUrl.trim().equals("")){
-				logger.warn("no oaiBaseUrl configured. will do nothing. end");
-				return;
+		AuthenticationUtil.runAsSystem(()-> {
+			try {
+				Map jobDataMap = context.getJobDetail().getJobDataMap();
+
+				String oaiBaseUrl = (String) jobDataMap.get(OAIConst.PARAM_OAI_BASE_URL);
+				if (oaiBaseUrl == null || oaiBaseUrl.trim().equals("")) {
+					logger.warn("no oaiBaseUrl configured. will do nothing. end");
+					return null;
+				}
+
+				Object catalogsParamObj = jobDataMap.get(OAIConst.PARAM_OAI_CATALOG_IDS);
+				if (catalogsParamObj == null) {
+					logger.warn("no allowed catalogs configured. will do nothing. end");
+					return null;
+				}
+
+				String metadataPrefix = (String) jobDataMap.get(OAIConst.PARAM_OAI_METADATA_PREFIX);
+
+				//String setsParam = (String) jobDataMap.get("sets");
+				List<String> catalogsList = null;
+				if (catalogsParamObj instanceof String) {
+					catalogsList = new ArrayList(Arrays.asList(((String) catalogsParamObj).split(",")));
+				} else {
+					catalogsList = (List) catalogsParamObj;
+				}
+
+
+				List<NodeRef> allNodes = new PersistentHandlerEdusharing(this, null, true).getAllNodesInImportfolder();
+				new ImportCleaner(oaiBaseUrl, catalogsList, metadataPrefix).removeDeletedImportedObjects(allNodes);
+
+			} catch (Throwable e) {
+				logger.error(e.getMessage(), e);
 			}
-			
-			Object catalogsParamObj = jobDataMap.get(OAIConst.PARAM_OAI_CATALOG_IDS);
-			if(catalogsParamObj == null){
-				logger.warn("no allowed catalogs configured. will do nothing. end");
-				return;
-			}
-			
-			String metadataPrefix = (String)jobDataMap.get(OAIConst.PARAM_OAI_METADATA_PREFIX);
-			
-			//String setsParam = (String) jobDataMap.get("sets");
-			List<String> catalogsList = null;
-			if(catalogsParamObj instanceof String){
-				catalogsList = new ArrayList(Arrays.asList(((String)catalogsParamObj).split(",")));
-			}else{
-				catalogsList = (List)catalogsParamObj;
-			}
-			
-			
-			
-			List<NodeRef> allNodes = new PersistentHandlerEdusharing(this,null,true).getAllNodesInImportfolder();
-			new ImportCleaner(oaiBaseUrl, catalogsList, metadataPrefix).removeDeletedImportedObjects(allNodes);
-			
-		} catch (Throwable e) {
-			logger.error(e.getMessage(), e);
-			e.printStackTrace();
-		}
-		logger.info("end");
+			logger.info("end");
+			return null;
+		});
 	}
 	
 	@Override
