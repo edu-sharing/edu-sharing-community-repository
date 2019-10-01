07421d900092a1bbcfdab1fed2634c4b6e3cc16b
Daniel Rudolph <rudolph@metaventis.com>
2019-10-01 12:04:07 +0200

    fix: Performance issue when many organizations, runas user = system should not switch logic to admin

--- Backend/src/org/edu_sharing/service/search/SearchServiceImpl.java
+++ Backend/src/org/edu_sharing/service/search/SearchServiceImpl.java
@@ -204,7 +204,6 @@ public class SearchServiceImpl implements SearchService {
 			
 			
 			Set<String> memberships = serviceRegistry.getAuthorityService().getAuthorities();
-			boolean isSystemUser = AuthenticationUtil.isRunAsUserTheSystemUser();
 			boolean isAdmin = ((memberships != null && memberships.contains(CCConstants.AUTHORITY_GROUP_ALFRESCO_ADMINISTRATORS)) 
 					|| "admin".equals(AuthenticationUtil.getFullAuthentication().getName())) ? true : false;
 			
@@ -229,7 +228,7 @@ public class SearchServiceImpl implements SearchService {
 						//only search organisations the curren user is in,except: its adminuser and onlyMemberShips == true
 						StringBuilder qbMemberShips=null;
 						if(onlyMemberShips == true || 
-								(onlyMemberShips == false && (!isSystemUser && !isAdmin))) {
+								(onlyMemberShips == false && !isAdmin)) {
 							
 						
 							
@@ -293,7 +292,6 @@ public class SearchServiceImpl implements SearchService {
 								boolean add = false;
 								for (String group : memberships) {
 									if (group.equals(CCConstants.AUTHORITY_GROUP_ALFRESCO_ADMINISTRATORS)
-											|| isSystemUser
 											|| group.equals(eduGroup.getGroupname())) {
 										add = true;
 										break;
