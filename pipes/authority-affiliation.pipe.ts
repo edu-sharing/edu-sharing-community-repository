c30728650a42bfdf0144ab50b40cc62ef0175f29
Torsten Simon <simon@edu-sharing.net>
2019-10-24 11:14:30 +0200

    Merge branch 'develop' into feature/5_1
    # Conflicts:
    # Backend/build.properties
    # Backend/src/org/edu_sharing/repository/server/rendering/RenderingProxy.java
    # Backend/src/org/edu_sharing/repository/server/tools/XApiTool.java
    # Backend/src/org/edu_sharing/webservices/render/RenderInfoSoapBindingImpl.java
    # Frontend/package-lock.json
    # Frontend/src/app/common/rest/data-object.ts
    # Frontend/src/app/common/rest/request-object.ts
    # Frontend/src/app/common/rest/services/rest-connector.service.ts
    # Frontend/src/app/common/rest/services/rest-organization.service.ts
    # Frontend/src/app/common/rest/services/rest-search.service.ts
    # Frontend/src/app/common/ui/DateHelper.ts
    # Frontend/src/app/common/ui/authority-color.pipe.ts
    # Frontend/src/app/common/ui/authority-search-input/authority-search-input.component.html
    # Frontend/src/app/common/ui/authority-search-input/authority-search-input.component.scss
    # Frontend/src/app/common/ui/authority-search-input/authority-search-input.component.ts
    # Frontend/src/app/common/ui/main-nav/main-nav.component.html
    # Frontend/src/app/common/ui/main-nav/main-nav.component.ts
    # Frontend/src/app/common/ui/modal-dialog-toast/modal-dialog-toast.component.html
    # Frontend/src/app/common/ui/node-helper.ts
    # Frontend/src/app/common/ui/toast.ts
    # Frontend/src/app/common/ui/user-avatar/user-avatar.component.scss
    # Frontend/src/app/common/ui/user-avatar/user-avatar.component.ts
    # Frontend/src/app/declarations.ts
    # Frontend/src/app/modules/admin/admin.component.ts
    # Frontend/src/app/modules/login/login.component.html
    # Frontend/src/app/modules/management-dialogs/add-stream/add-stream.component.html
    # Frontend/src/app/modules/permissions/authorities/authorities.component.html
    # Frontend/src/app/modules/search/declarations.ts
    # Frontend/src/app/modules/search/search.component.ts
    # Frontend/src/app/modules/stream/stream.component.ts
    # Frontend/src/app/modules/workspace/workspace.component.ts
    # Frontend/src/styles/variables.scss

---
+++ pipes/authority-affiliation.pipe.ts
@@ -0,0 +1,22 @@
+import {Pipe, PipeTransform} from "@angular/core";
+import {TranslateService} from "@ngx-translate/core";
+import {User} from "../../core-module/rest/data-object";
+@Pipe({name: 'authorityAffiliation'})
+export class AuthorityAffiliationPipe implements PipeTransform {
+  constructor(
+      private translate : TranslateService
+  ){}
+  transform(authority : any|User,args:string[]): string {
+    if(!authority)
+      return "invalid";
+    if(authority.profile && authority.profile.primaryAffiliation) {
+      let key='USER.PRIMARY_AFFILIATION.'+authority.profile.primaryAffiliation;
+      let result=this.translate.instant(key);
+      if(key!=result)
+        return result;
+      // in case of no translation, fetch raw value
+      return authority.profile.primaryAffiliation;
+    }
+    return '';
+  }
+}
