01ab802a0985eb60094e5b55c07d9fd52b219856
Torsten Simon <simon@edu-sharing.net>
2019-11-04 12:07:10 +0100

    fix:serialize exception client.config.xml

--- Backend/src/org/edu_sharing/service/config/model/Admin.java
+++ Backend/src/org/edu_sharing/service/config/model/Admin.java
@@ -1,8 +1,9 @@
 package org.edu_sharing.service.config.model;
 
 import javax.xml.bind.annotation.XmlElement;
+import java.io.Serializable;
 
-public class Admin {
+public class Admin implements Serializable {
     @XmlElement public Statistics statistics;
 
 }
--- Backend/src/org/edu_sharing/service/config/model/Statistics.java
+++ Backend/src/org/edu_sharing/service/config/model/Statistics.java
@@ -1,7 +1,8 @@
 package org.edu_sharing.service.config.model;
 
 import javax.xml.bind.annotation.XmlElement;
+import java.io.Serializable;
 
-public class Statistics {
+public class Statistics implements Serializable {
     @XmlElement public String[] groups;
 }
