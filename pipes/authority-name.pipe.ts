8baeed608872895fbea6d80c3df565cab3f04e83
Torsten Simon <simon@edu-sharing.net>
2019-12-02 14:19:00 +0100

    feat:handle deleted users on comments, feat for keeping deleted users contribution data

--- pipes/authority-name.pipe.ts
+++ pipes/authority-name.pipe.ts
@@ -1,23 +1,27 @@
-import {Pipe, PipeTransform} from "@angular/core";
+import {Pipe, PipeTransform} from '@angular/core';
 import {RestConstants} from '../../core-module/core.module';
 import {TranslateService} from '@ngx-translate/core';
 @Pipe({name: 'authorityName'})
 export class AuthorityNamePipe implements PipeTransform {
-  constructor(private translate:TranslateService){}
-  transform(authority : any,args:string[]=null): string {
-    if(!authority)
-      return "invalid";
-    if(authority.profile && authority.profile.displayName)
+  constructor(private translate: TranslateService){}
+  transform(authority: any, args: string[]= null): string {
+    if (!authority)
+      return 'invalid';
+    if (authority.profile && authority.profile.displayName)
       return authority.profile.displayName;
-    if(authority.profile)
-      return authority.profile.firstName+" "+authority.profile.lastName;
-    if(authority.authorityType==RestConstants.AUTHORITY_TYPE_EVERYONE){
+    if (authority.profile)
+      return authority.profile.firstName + ' ' + authority.profile.lastName;
+    if (authority.authorityType === RestConstants.AUTHORITY_TYPE_EVERYONE) {
       return this.translate.instant('GROUP_EVERYONE');
     }
-    if(authority.authorityName)
+    if (authority.authorityName) {
+      if (authority.authorityName === RestConstants.AUTHORITY_DELETED_USER) {
+        return this.translate.instant('DELETED_USER');
+      }
       return authority.authorityName;
-    if(authority.firstName || authority.lastName)
-      return authority.firstName+" "+authority.lastName;
-    return "invalid";
+    }
+    if (authority.firstName || authority.lastName)
+      return authority.firstName + ' ' + authority.lastName;
+    return 'invalid';
   }
 }
