e07a99acf165314d9ebd6065001dd69494d79911
Torsten Simon <simon@edu-sharing.net>
2019-12-06 10:08:55 +0100

    fix:gitignore remove the test folder

---
+++ Frontend/src/app/common/test/inputs/inputs-test.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+import {Collection, RestCollectionService, RestConnectorService} from "../../../core-module/core.module";
+
+@Component({
+  selector: 'app-inputs-test',
+  styleUrls: ['./inputs-test.component.scss'],
+  templateUrl: './inputs-test.component.html',
+})
+export class InputsTestComponent {
+}
