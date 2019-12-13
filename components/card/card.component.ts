de4fcabc5b0902e8828fc100d6f4383a99af3202
Torsten Simon <simon@edu-sharing.net>
2019-12-13 12:04:12 +0100

    fix:card animation trigger

--- components/card/card.component.ts
+++ components/card/card.component.ts
@@ -14,6 +14,7 @@ import {UIHelper} from '../../ui-helper';
   templateUrl: 'card.component.html',
   styleUrls: ['card.component.scss'],
   animations: [
+      trigger('cardAnimation', UIAnimation.cardAnimation())
   ]
 })
 /**
