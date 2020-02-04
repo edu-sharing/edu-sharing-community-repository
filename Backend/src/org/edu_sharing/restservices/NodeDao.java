5aa8a4f70e69f1fe403dd515121a22047334164d
Torsten Simon <simon@edu-sharing.net>
2020-02-04 15:22:19 +0100

    fix:copy node api return copied node, not original

--- Backend/src/org/edu_sharing/restservices/NodeDao.java
+++ Backend/src/org/edu_sharing/restservices/NodeDao.java
@@ -593,7 +593,7 @@ public class NodeDao {
 		try {
 			org.alfresco.service.cmr.repository.NodeRef newNode = nodeService.copyNode(sourceId, nodeId, withChildren);
 			permissionService.createNotifyObject(newNode.getId(), new AuthenticationToolAPI().getCurrentUser(), CCConstants.CCM_VALUE_NOTIFY_ACTION_PERMISSION_ADD);
-			return new NodeDao(repoDao, sourceId, Filter.createShowAllFilter());
+			return new NodeDao(repoDao, newNode.getId(), Filter.createShowAllFilter());
 			
 		} catch (Throwable t) {
 			
