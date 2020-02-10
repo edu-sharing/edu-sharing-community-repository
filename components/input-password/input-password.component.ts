87cd8991a3d2878d249263405daeea8077ab3251
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-02-10 16:23:11 +0100

    feat: accessibility for show-password button

--- components/input-password/input-password.component.ts
+++ components/input-password/input-password.component.ts
@@ -1,44 +1,63 @@
-import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from "@angular/core";
-import {UIHelper} from "../../ui-helper"
-@Component({
-  selector: 'input-password',
-  templateUrl: 'input-password.component.html',
-  styleUrls: ['input-password.component.scss'],
-})
+import {
+    Component,
+    ElementRef,
+    EventEmitter,
+    Input,
+    Output,
+    ViewChild,
+} from '@angular/core';
+import { UIHelper } from '../../ui-helper';
 
-export class InputPasswordComponent{
-    public input_type = "password";
-    @Input() id="passwordInput";
-    @Input() required=false;
-    @Input() autocomplete:string;
-    @Input() inputClass="";
-    @Input() hint=false;
-    @Input() label="";
-    @Input() floatLabel="auto";
-    @Input() displayStrength=false;
+@Component({
+    selector: 'input-password',
+    templateUrl: 'input-password.component.html',
+    styleUrls: ['input-password.component.scss'],
+})
+export class InputPasswordComponent {
+    @Input() id = 'passwordInput';
+    @Input() required = false;
+    @Input() autocomplete: string;
+    @Input() inputClass = '';
+    @Input() hint = false;
+    @Input() label = '';
+    @Input() floatLabel = 'auto';
+    @Input() displayStrength = false;
+    @Input() placeholder = '';
+    @Input() set value(_value: string) {
+        this._value = _value;
+        this.valueChange.emit(_value);
+        this.passwordStrength = UIHelper.getPasswordStrengthString(_value);
+    }
+    get value() {
+        return this._value;
+    }
+
     @Output() valueChange = new EventEmitter();
-    @Input() placeholder="";
     @Output() change = new EventEmitter();
     @Output() keydown = new EventEmitter();
     @Output() keyup = new EventEmitter();
     @Output() ngModelChange = new EventEmitter();
-    @ViewChild('input', {static: false}) nativeInput : ElementRef;
+
+    @ViewChild('input', { static: false }) nativeInput: ElementRef;
+
     passwordStrength: string;
-    _value:string;
-    @Input() public set value(_value:string){
-      this._value=_value;
-      this.valueChange.emit(_value);
-      this.passwordStrength = UIHelper.getPasswordStrengthString(_value);
-    }
-    public get value(){
-      return this._value;
-    }
-    public showPassword(){
-        if (this.input_type === "password") {
-            this.input_type = "text";
+    showPassword = false;
+    
+    private _value: string;
+
+    getInputType(): string {
+        if (this.showPassword) {
+            return 'text';
         } else {
-            this.input_type = "password";
+            return 'password';
         }
     }
 
+    getShowPasswordLabel(): string {
+        if (this.showPassword) {
+            return 'LOGIN.HIDE_PASSWORD';
+        } else {
+            return 'LOGIN.SHOW_PASSWORD';
+        }
+    }
 }
