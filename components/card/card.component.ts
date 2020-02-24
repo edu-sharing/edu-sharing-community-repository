c1cd84e6d8740f16fde47dabe94fe493e6f88c9b
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-02-24 12:56:07 +0100

    style: format card component

--- components/card/card.component.ts
+++ components/card/card.component.ts
@@ -1,29 +1,38 @@
+import { trigger } from '@angular/animations';
 import {
-    Component, Input, Output, EventEmitter, OnInit, HostListener, ViewChild, ElementRef,
-    QueryList, Inject, OnDestroy
+    Component,
+    ElementRef,
+    EventEmitter,
+    HostListener,
+    Input,
+    OnDestroy,
+    Output,
+    ViewChild,
 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { trigger } from '@angular/animations';
-import { UIAnimation } from '../../../core-module/ui/ui-animation';
-import { DialogButton, RestHelper, UIService, Node, UIConstants } from '../../../core-module/core.module';
+import {
+    DialogButton,
+    Node,
+    RestHelper,
+    UIService,
+} from '../../../core-module/core.module';
 import { Helper } from '../../../core-module/rest/helper';
+import { UIAnimation } from '../../../core-module/ui/ui-animation';
 import { UIHelper } from '../../ui-helper';
 
+/**
+ * A common edu-sharing modal card
+ */
 @Component({
     selector: 'card',
     templateUrl: 'card.component.html',
     styleUrls: ['card.component.scss'],
-    animations: [
-        trigger('cardAnimation', UIAnimation.cardAnimation())
-    ]
+    animations: [trigger('cardAnimation', UIAnimation.cardAnimation())],
 })
-/**
- * A common edu-sharing modal card
- */
 export class CardComponent implements OnDestroy {
-    @ViewChild('cardContainer', {static: false}) cardContainer: ElementRef;
-    @ViewChild('jumpmarksRef', {static: false}) jumpmarksRef: ElementRef;
-    private static modalCards: CardComponent[] = [];
+    @ViewChild('cardContainer', { static: false }) cardContainer: ElementRef;
+    @ViewChild('jumpmarksRef', { static: false }) jumpmarksRef: ElementRef;
+
     /**
      * the title of the card. Should be pre-translated
      */
@@ -66,7 +75,6 @@ export class CardComponent implements OnDestroy {
      */
     @Input() jumpmarks: CardJumpmark[];
     @Input() priority = 0;
-    jumpmarkActive: CardJumpmark;
 
     /**
      * Optional, bind a Node or Node-Array to this element
@@ -76,34 +84,121 @@ export class CardComponent implements OnDestroy {
         if (!node) {
             return;
         }
-        let nodes: Node[] = (node as any);
+        let nodes: Node[] = node as any;
         if (!Array.isArray(nodes)) {
-            nodes = [(node as any)];
+            nodes = [node as any];
         }
         if (nodes && nodes.length) {
             if (nodes.length === 1 && nodes[0]) {
                 this.avatar = nodes[0].iconURL;
                 this.subtitle = RestHelper.getTitle(nodes[0]);
-            }
-            else {
+            } else {
                 this.avatar = null;
-                this.subtitle = this.translate.instant('CARD_SUBTITLE_MULTIPLE', { count: nodes.length });
+                this.subtitle = this.translate.instant(
+                    'CARD_SUBTITLE_MULTIPLE',
+                    { count: nodes.length },
+                );
             }
         }
     }
-    @Output() onCancel = new EventEmitter();
-    @Output() onScrolled = new EventEmitter();
-    /** A list of buttons, see @DialogButton
-     * Also use the DialogButton.getYesNo() and others if applicable!
-     */
-    public _buttons: DialogButton[];
     @Input() set buttons(buttons: DialogButton[]) {
         this._buttons = buttons;
     }
+
+    @Output() onCancel = new EventEmitter();
+    @Output() onScrolled = new EventEmitter();
+
+    /** A list of buttons, see @DialogButton
+     * Also use the DialogButton.getYesNo() and others if applicable!
+     */
+    _buttons: DialogButton[];
+    jumpmarkActive: CardJumpmark;
+
+    private static modalCards: CardComponent[] = [];
+
+    static getNumberOfOpenCards() {
+        return CardComponent.modalCards.length;
+    }
+
+    constructor(
+        private uiService: UIService,
+        private translate: TranslateService,
+    ) {
+        CardComponent.modalCards.splice(0, 0, this);
+        document.body.style.overflow = 'hidden';
+        UIHelper.waitForComponent(this, 'jumpmarksRef').subscribe(() => {
+            console.log('jumpmarks ready');
+            setInterval(() => {
+                try {
+                    const jump = this.jumpmarksRef;
+                    const height =
+                        this.cardContainer.nativeElement.getBoundingClientRect()
+                            .bottom -
+                        this.cardContainer.nativeElement.getBoundingClientRect()
+                            .top;
+                    const pos =
+                        this.cardContainer.nativeElement.scrollTop -
+                        height -
+                        200;
+                    let closest = 999999;
+                    for (const jumpmark of this.jumpmarks) {
+                        const element = document.getElementById(jumpmark.id);
+                        const top = element.getBoundingClientRect().top;
+                        if (Math.abs(top - pos) < closest) {
+                            closest = Math.abs(top - pos);
+                            this.jumpmarkActive = this.jumpmarks[
+                                Helper.indexOfObjectArray(
+                                    this.jumpmarks,
+                                    'id',
+                                    element.id,
+                                )
+                            ];
+                        }
+                    }
+                } catch (e) {}
+            }, 1000 / 20); // 20 FPS
+        });
+        // handle the autofocus event as soon as the card is displayed
+        UIHelper.waitForComponent(this, 'cardContainer').subscribe(
+            cardContainer => {
+                const inputs = cardContainer.nativeElement.getElementsByTagName(
+                    'input',
+                );
+                for (let i = 0; i < inputs.length; i++) {
+                    if (inputs.item(i).autofocus) {
+                        console.log(
+                            'card: setting autofocus on element',
+                            inputs.item(i),
+                        );
+                        inputs.item(i).focus();
+                        return;
+                    }
+                }
+            },
+        );
+    }
+
+    ngOnDestroy() {
+        CardComponent.modalCards.splice(
+            CardComponent.modalCards.indexOf(this),
+            1,
+        );
+        if (CardComponent.modalCards.length === 0) {
+            document.body.style.overflow = null;
+        }
+    }
+
     @HostListener('window:resize')
     onResize() {
-        if (document.activeElement && this.cardContainer && this.cardContainer.nativeElement) {
-            UIHelper.scrollSmoothElementToChild(document.activeElement, this.cardContainer.nativeElement);
+        if (
+            document.activeElement &&
+            this.cardContainer &&
+            this.cardContainer.nativeElement
+        ) {
+            UIHelper.scrollSmoothElementToChild(
+                document.activeElement,
+                this.cardContainer.nativeElement,
+            );
         }
     }
     @HostListener('document:keydown', ['$event'])
@@ -114,53 +209,7 @@ export class CardComponent implements OnDestroy {
             }
         }
     }
-    private scrollSmooth(jumpmark: CardJumpmark) {
-        const pos = document.getElementById(jumpmark.id).offsetTop;
-        UIHelper.scrollSmoothElement(pos, this.cardContainer.nativeElement, 2);
-    }
-    constructor(private uiService: UIService, private translate: TranslateService) {
-        CardComponent.modalCards.splice(0, 0, this);
-        document.body.style.overflow = 'hidden';
-        UIHelper.waitForComponent(this, 'jumpmarksRef').subscribe(() => {
-            console.log('jumpmarks ready');
-            setInterval(() => {
-                try {
-                    const jump = this.jumpmarksRef;
-                    const height = this.cardContainer.nativeElement.getBoundingClientRect().bottom
-                        - this.cardContainer.nativeElement.getBoundingClientRect().top;
-                    const pos = this.cardContainer.nativeElement.scrollTop - height - 200;
-                    let closest = 999999;
-                    for (const jumpmark of this.jumpmarks) {
-                        const element = document.getElementById(jumpmark.id);
-                        const top = element.getBoundingClientRect().top;
-                        if (Math.abs(top - pos) < closest) {
-                            closest = Math.abs(top - pos);
-                            this.jumpmarkActive = this.jumpmarks[Helper.indexOfObjectArray(this.jumpmarks, 'id', element.id)];
-                        }
-                    }
-                } catch (e) {
 
-                }
-            }, 1000 / 20); // 20 FPS
-        });
-        // handle the autofocus event as soon as the card is displayed
-        UIHelper.waitForComponent(this, 'cardContainer').subscribe((cardContainer) => {
-            const inputs = cardContainer.nativeElement.getElementsByTagName('input');
-            for (let i = 0; i < inputs.length; i++) {
-                if (inputs.item(i).autofocus) {
-                    console.log('card: setting autofocus on element', inputs.item(i));
-                    inputs.item(i).focus();
-                    break;
-                }
-            }
-        });
-    }
-    ngOnDestroy() {
-        CardComponent.modalCards.splice(CardComponent.modalCards.indexOf(this), 1);
-        if (CardComponent.modalCards.length === 0) {
-            document.body.style.overflow = null;
-        }
-    }
     handleEvent(event: any) {
         if (event.key === 'Escape') {
             event.stopPropagation();
@@ -171,17 +220,21 @@ export class CardComponent implements OnDestroy {
         return false;
     }
 
-    public click(btn: DialogButton) {
+    click(btn: DialogButton) {
         btn.callback();
     }
-    public cancel() {
+
+    cancel() {
         this.onCancel.emit();
     }
+
     scrolled() {
         this.onScrolled.emit();
     }
-    static getNumberOfOpenCards(){
-        return CardComponent.modalCards.length;
+
+    private scrollSmooth(jumpmark: CardJumpmark) {
+        const pos = document.getElementById(jumpmark.id).offsetTop;
+        UIHelper.scrollSmoothElement(pos, this.cardContainer.nativeElement, 2);
     }
 }
 export class CardJumpmark {
@@ -191,5 +244,5 @@ export class CardJumpmark {
      * @param label the pre-translated label
      * @param icon the icon
      */
-    constructor(public id: string, public label: string, public icon: string) { }
+    constructor(public id: string, public label: string, public icon: string) {}
 }
