cd1df77d3ed9beabb95fff710211ec66cb25805c
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-03-03 14:39:09 +0100

    style: format search service

--- Frontend/src/app/modules/search/search.service.ts
+++ Frontend/src/app/modules/search/search.service.ts
@@ -1,45 +1,46 @@
 import { Injectable } from '@angular/core';
-import { Node } from "../../core-module/core.module";
-import {ListItem} from "../../core-module/core.module";
+import { Node } from '../../core-module/core.module';
+import { ListItem } from '../../core-module/core.module';
 
 @Injectable()
 export class SearchService {
+    searchTerm: string = '';
+    searchResult: Node[] = [];
+    searchResultRepositories: Node[][] = [];
+    searchResultCollections: Node[] = [];
+    columns: ListItem[] = [];
+    collectionsColumns: ListItem[] = [];
+    ignored: Array<string> = [];
+    reurl: string;
+    facettes: Array<any> = [];
+    autocompleteData: any = [];
+    skipcount: number[] = [];
+    numberofresults: number = 0;
+    offset: number = 0;
+    complete: boolean = false;
+    showchosenfilters: boolean = false;
+    reinit = true;
+    resultCount: any = {};
+    sidenavSet = false;
+    sidenavOpened = false;
+    showspinner: boolean;
+    viewType = -1;
+    sort: any = {};
 
-  searchTerm: string = '';
-  searchResult: Node[] = [];
-  searchResultRepositories: Node[][] = [];
-  searchResultCollections: Node[] = [];
-  columns : ListItem[]=[];
-  collectionsColumns : ListItem[]=[];
-  ignored: Array<string> = [];
-  reurl: string;
-  facettes: Array<any> = [];
-  autocompleteData:any = [];
-  skipcount: number[] = [];
-  numberofresults:number = 0;
-  offset: number = 0;
-  complete: boolean = false;
-  showchosenfilters: boolean = false;
-  reinit = true;
-  resultCount:any={};
-  sidenavSet = false;
-  sidenavOpened = false;
-  showspinner: boolean;
-  viewType = -1;
-  sort:any = {}
-  constructor() {}
-  clear(){
-    this.searchTerm="";
-  }
-   init() {
-    if(!this.reinit)
-      return;
-     this.skipcount = [];
-     this.offset = 0;
-     this.searchResult = [];
-     this.searchResultCollections = [];
-     this.searchResultRepositories = [];
-     this.complete = false;
-     this.facettes = [];
-   }
+    constructor() {}
+
+    clear() {
+        this.searchTerm = '';
+    }
+
+    init() {
+        if (!this.reinit) return;
+        this.skipcount = [];
+        this.offset = 0;
+        this.searchResult = [];
+        this.searchResultCollections = [];
+        this.searchResultRepositories = [];
+        this.complete = false;
+        this.facettes = [];
+    }
 }
