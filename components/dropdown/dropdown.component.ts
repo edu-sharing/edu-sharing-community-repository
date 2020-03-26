524c7f0d46de88ce8ccc827e93e1852ecb3c9038
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-03-26 10:34:00 +0100

    style: format dropdown component

--- components/dropdown/dropdown.component.ts
+++ components/dropdown/dropdown.component.ts
@@ -1,54 +1,58 @@
-import {Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef, HostListener} from '@angular/core';
-import {UIAnimation} from '../../../core-module/ui/ui-animation';
-import {trigger} from '@angular/animations';
-import {UIHelper} from '../../ui-helper'
-import {Helper} from '../../../core-module/rest/helper';
-import {OptionItem} from '../../option-item';
-import {UIService} from '../../../core-module/core.module';
-import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatMenu } from '@angular/material/menu';
+import { UIService } from '../../../core-module/core.module';
+import { Helper } from '../../../core-module/rest/helper';
+import { OptionItem } from '../../option-item';
+import { UIHelper } from '../../ui-helper';
 
-@Component({
-  selector: 'dropdown',
-  templateUrl: 'dropdown.component.html',
-  styleUrls: ['dropdown.component.scss']
-})
 /**
- * The dropdown is one base component of the action bar (showing more actions), but can also be used standalone
+ * The dropdown is one base component of the action bar (showing more actions),
+ * but can also be used standalone.
  */
+@Component({
+    selector: 'dropdown',
+    templateUrl: 'dropdown.component.html',
+    styleUrls: ['dropdown.component.scss'],
+})
 export class DropdownComponent {
-  @ViewChild('dropdown', {static: true}) menu : MatMenu;
-  _options: OptionItem[];
-  @Input() position : 'left' | 'right' = 'left';
-  @Input() set options(options:OptionItem[]) {
-    this._options = UIHelper.filterValidOptions(this.ui,Helper.deepCopyArray(options));
-  }
+    @ViewChild('dropdown', { static: true }) menu: MatMenu;
 
-  /**
-   * the object that should be returned via the option's callback
-   * Can be null
-   */
-  @Input() callbackObject:any;
+    @Input() position: 'left' | 'right' = 'left';
+
+    @Input() set options(options: OptionItem[]) {
+        this._options = UIHelper.filterValidOptions(
+            this.ui,
+            Helper.deepCopyArray(options),
+        );
+    }
+
+    /**
+     * The object that should be returned via the option's callback.
+     *
+     * Can be null
+     */
+    @Input() callbackObject: any;
 
     /**
      * Should disabled ("greyed out") options be shown or hidden?
-     * @type {boolean}
      */
     @Input() showDisabled = true;
 
+    _options: OptionItem[];
 
-  click(option : OptionItem) {
-      if(!option.isEnabled)
-          return;
-      setTimeout(() => option.callback(this.callbackObject));
-  }
-  constructor(private ui : UIService) {
+    constructor(private ui: UIService) {}
 
-  }
-
-  isNewGroup(i: number) {
-    if (i>0) {
-      return this._options[i].group !== this._options[i-1].group;
+    click(option: OptionItem) {
+        if (!option.isEnabled) {
+            return;
+        }
+        setTimeout(() => option.callback(this.callbackObject));
     }
-    return false;
-  }
+
+    isNewGroup(i: number) {
+        if (i > 0) {
+            return this._options[i].group !== this._options[i - 1].group;
+        }
+        return false;
+    }
 }
