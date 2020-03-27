231821c58ce6f46489f563c28a8a2de2cdd191fb
Daniel Rudolph <rudolph@metaventis.com>
2020-03-27 12:13:03 +0100

    Revert "fix: prevent delete of edu_homedir for non admins even if they got coordinator permissions"
    This reverts commit 26eed671c7ee28f97e0888ba84829bafa0d950ff.

--- Backend/src/org/edu_sharing/alfresco/policy/BeforeEduGroupDeletePolicy.java
+++ Backend/src/org/edu_sharing/alfresco/policy/BeforeEduGroupDeletePolicy.java
@@ -5,11 +5,8 @@ import org.alfresco.repo.node.NodeServicePolicies;
 import org.alfresco.repo.node.NodeServicePolicies.BeforeDeleteNodePolicy;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
-import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.security.AuthenticationService;
-import org.alfresco.service.cmr.security.AuthorityService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 import org.edu_sharing.repository.client.tools.CCConstants;
@@ -20,8 +17,6 @@ public class BeforeEduGroupDeletePolicy implements NodeServicePolicies.BeforeDel
 	
 	NodeService nodeService;
 	PolicyComponent policyComponent;
-	AuthenticationService authenticationService;
-	AuthorityService authorityService;
 	
 	Logger logger = Logger.getLogger(BeforeEduGroupDeletePolicy.class);
 	
@@ -32,10 +27,7 @@ public class BeforeEduGroupDeletePolicy implements NodeServicePolicies.BeforeDel
 	@Override
 	public void beforeDeleteNode(NodeRef nodeRef) {
 		if(nodeService.hasAspect(nodeRef, QName.createQName(CCConstants.CCM_ASPECT_EDUGROUP ))){
-			if( !new Helper(authorityService).isAdmin(authenticationService.getCurrentUserName()) 
-					&& !AuthenticationUtil.isRunAsUserTheSystemUser() ){
-				throw new SystemFolderDeleteDeniedException("you are not allowed to remove this folder!");
-			}
+			
 			EduGroupCache.remove(nodeRef);
 		}
 		
@@ -47,13 +39,5 @@ public class BeforeEduGroupDeletePolicy implements NodeServicePolicies.BeforeDel
 	
 	public void setPolicyComponent(PolicyComponent policyComponent) {
 		this.policyComponent = policyComponent;
-	}
-	
-	public void setAuthenticationService(AuthenticationService authenticationService) {
-		this.authenticationService = authenticationService;
-	}
-	
-	public void setAuthorityService(AuthorityService authorityService) {
-		this.authorityService = authorityService;
 	}
 }
