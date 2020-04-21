b91bbb186e0493c6e6e472a516ffe984444ffde6
Torsten Simon <simon@edu-sharing.net>
2020-04-21 11:31:45 +0200

    fix:nlp sharing component

--- Frontend/src/app/modules/sharing/sharing.component.ts
+++ Frontend/src/app/modules/sharing/sharing.component.ts
@@ -2,26 +2,26 @@
 import {Component, ViewChild, HostListener, ElementRef} from '@angular/core';
 import 'rxjs/add/operator/map';
 import {Router, ActivatedRoute, Params} from '@angular/router';
-import {TranslateService} from "@ngx-translate/core";
-import {Translation} from "../../core-ui-module/translation";
+import {TranslateService} from '@ngx-translate/core';
+import {Translation} from '../../core-ui-module/translation';
 import {ListItem, RestSearchService} from '../../core-module/core.module';
 import {RestNodeService} from '../../core-module/core.module';
 import {RestConstants} from '../../core-module/core.module';
-import {RestConnectorService} from "../../core-module/core.module";
-import {Node, NodeList, LoginResult, SharingInfo, Person} from "../../core-module/core.module";
-import {CustomOptions, OptionItem} from "../../core-ui-module/option-item";
-import {TemporaryStorageService} from "../../core-module/core.module";
-import {UIHelper} from "../../core-ui-module/ui-helper";
-import {Title} from "@angular/platform-browser";
-import {ConfigurationService} from "../../core-module/core.module";
-import {SessionStorageService} from "../../core-module/core.module";
-import {UIConstants} from "../../core-module/ui/ui-constants";
-import {RestMdsService} from "../../core-module/core.module";
-import {RestHelper} from "../../core-module/core.module";
-import {RestSharingService} from "../../core-module/core.module";
-import {Toast} from "../../core-ui-module/toast";
-import {ConfigurationHelper} from "../../core-module/core.module";
-import {Helper} from "../../core-module/rest/helper";
+import {RestConnectorService} from '../../core-module/core.module';
+import {Node, NodeList, LoginResult, SharingInfo, Person} from '../../core-module/core.module';
+import {CustomOptions, OptionItem} from '../../core-ui-module/option-item';
+import {TemporaryStorageService} from '../../core-module/core.module';
+import {UIHelper} from '../../core-ui-module/ui-helper';
+import {Title} from '@angular/platform-browser';
+import {ConfigurationService} from '../../core-module/core.module';
+import {SessionStorageService} from '../../core-module/core.module';
+import {UIConstants} from '../../core-module/ui/ui-constants';
+import {RestMdsService} from '../../core-module/core.module';
+import {RestHelper} from '../../core-module/core.module';
+import {RestSharingService} from '../../core-module/core.module';
+import {Toast} from '../../core-ui-module/toast';
+import {ConfigurationHelper} from '../../core-module/core.module';
+import {Helper} from '../../core-module/rest/helper';
 
 
 
@@ -46,7 +46,8 @@ export class SharingComponent {
       sortAscending: true
   };
   options:CustomOptions={
-      useDefaultOptions: false
+      useDefaultOptions: false,
+      addOptions: []
   };
   constructor(
     private router : Router,
@@ -65,17 +66,17 @@ export class SharingComponent {
       this.options.addOptions.push(new OptionItem('SHARING.DOWNLOAD','cloud_download',(node:Node)=>this.download(node)));
       Translation.initialize(translate,this.config,this.session,this.route).subscribe(()=> {
           UIHelper.setTitle('SHARING.TITLE', title, translate, config);
-          this.route.queryParams.subscribe((params)=>{
+          this.route.queryParams.subscribe((params)=> {
              this.params=params;
-             this.sharingService.getInfo(params['nodeId'],params['token']).subscribe((result)=>{
+             this.sharingService.getInfo(params.nodeId,params.token).subscribe((result)=> {
                  this.loading=false;
                  this.sharingInfo=result;
-                 if(result.expired){
+                 if(result.expired) {
                      this.router.navigate([UIConstants.ROUTER_PREFIX,'messages','share_expired']);
                      return;
                  }
                  this.loadChildren();
-             },(error)=>{
+             },(error)=> {
                  console.warn(error);
                  this.router.navigate([UIConstants.ROUTER_PREFIX,'messages','share_expired']);
                  this.loading=false;
@@ -84,31 +85,31 @@ export class SharingComponent {
       });
    }
     validatePassword() {
-        this.sharingService.getInfo(this.params['nodeId'], this.params['token'], this.passwordInput).subscribe((result) => {
-            if(!result.passwordMatches){
+        this.sharingService.getInfo(this.params.nodeId, this.params.token, this.passwordInput).subscribe((result) => {
+            if(!result.passwordMatches) {
                 this.toast.error(null,'SHARING.ERROR_INVALID_PASSWORD');
             }
             this.sharingInfo=result;
             this.loadChildren();
         });
     }
-    download(child : Node = null){
-      let node=this.params['nodeId'];
-      let token=this.params['token'];
-      let url = this.connector.getAbsoluteEndpointUrl() + "../share?mode=download&token=" + encodeURIComponent(token) + "&password=" + encodeURIComponent(this.passwordInput) + "&nodeId=" + encodeURIComponent(node);
-      if(child==null && this.sharingInfo.node.isDirectory){
-          let ids = RestHelper.getNodeIds(this.childs).join(",");
-          url+= "&childIds=" + encodeURIComponent(ids);
+    download(child : Node = null) {
+      const node=this.params.nodeId;
+      const token=this.params.token;
+      let url = this.connector.getAbsoluteEndpointUrl() + '../share?mode=download&token=' + encodeURIComponent(token) + '&password=' + encodeURIComponent(this.passwordInput) + '&nodeId=' + encodeURIComponent(node);
+      if(child==null && this.sharingInfo.node.isDirectory) {
+          const ids = RestHelper.getNodeIds(this.childs).join(',');
+          url+= '&childIds=' + encodeURIComponent(ids);
       }
       else {
           if (child != null) {
-              url += "&childIds=" + encodeURIComponent(child.ref.id);
+              url += '&childIds=' + encodeURIComponent(child.ref.id);
           }
       }
         window.open(url);
 
     }
-    private changeSort(sort:any){
+    private changeSort(sort:any) {
         this.sort=sort;
         this.loadChildren();
     }
@@ -117,16 +118,16 @@ export class SharingComponent {
             return;
         this.loadingChildren=true;
         this.childs=[];
-        let request={count:RestConstants.COUNT_UNLIMITED,sortBy:[this.sort.sortBy],sortAscending:[this.sort.sortAscending]};
-        this.sharingService.getChildren(this.params['nodeId'], this.params['token'], this.passwordInput,request).subscribe((nodes)=>{
+        const request= {count:RestConstants.COUNT_UNLIMITED,sortBy:[this.sort.sortBy],sortAscending:[this.sort.sortAscending]};
+        this.sharingService.getChildren(this.params.nodeId, this.params.token, this.passwordInput,request).subscribe((nodes)=> {
             this.childs=nodes.nodes;
             this.loadingChildren=false;
         });
     }
-    inviterIsAuthor(){
+    inviterIsAuthor() {
       return Helper.objectEquals(this.sharingInfo.invitedBy,this.sharingInfo.node.createdBy);
     }
-    getPersonName(person:Person){
+    getPersonName(person:Person) {
         return ConfigurationHelper.getPersonWithConfigDisplayName(person,this.config);
     }
 }
