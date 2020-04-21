bddd1bbc618d7ba6fbcd7228e5827560f25aabe3
Daniel Rudolph <rudolph@metaventis.com>
2020-04-21 13:06:48 +0200

    fix: Job for removing permissions for all authorities: second job uses database instead of solr
    (cherry picked from commit 6c3be5699ddf36a985300651e34fbefbf0446fae)

--- Backend/src/org/edu_sharing/repository/server/jobs/quartz/TakeBackGlobalShareNodeService.java
+++ Backend/src/org/edu_sharing/repository/server/jobs/quartz/TakeBackGlobalShareNodeService.java
@@ -1,17 +1,14 @@
 package org.edu_sharing.repository.server.jobs.quartz;
 
-import java.util.Set;
+import java.util.List;
 
 import org.alfresco.model.ContentModel;
-import org.alfresco.repo.search.impl.solr.ESSearchParameters;
+import org.alfresco.repo.model.Repository;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.repository.StoreRef;
-import org.alfresco.service.cmr.search.ResultSet;
-import org.alfresco.service.cmr.search.SearchService;
-import org.alfresco.service.cmr.security.AccessPermission;
 import org.alfresco.service.cmr.security.PermissionService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
@@ -21,62 +18,61 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.context.ApplicationContext;
 
-public class TakeBackGlobalShare extends AbstractJob {
-	
+public class TakeBackGlobalShareNodeService extends AbstractJob {
+
 	ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
 	ServiceRegistry serviceRegistry = (ServiceRegistry) applicationContext.getBean(ServiceRegistry.SERVICE_REGISTRY);
 	private NodeService nodeService = serviceRegistry.getNodeService();
-	private SearchService searchService = serviceRegistry.getSearchService();
 	private PermissionService permissionService = serviceRegistry.getPermissionService();
+	Repository repositoryHelper = (Repository) applicationContext.getBean("repositoryHelper");
+
 	
 	Logger logger = Logger.getLogger(TakeBackGlobalShare.class);
 	
 	public static String PARAM_EXECUTE = "EXECUTE";
-	
-	int PAGE_SIZE = 100;
 	
 	TakeBackGlobalShareWorker worker;
 	
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		boolean execute = new Boolean((String)context.getJobDetail().getJobDataMap().get(PARAM_EXECUTE));
-		
 		worker = new TakeBackGlobalShareWorker(nodeService, permissionService, execute);
-		
 		AuthenticationUtil.RunAsWork<Void> runAs = new AuthenticationUtil.RunAsWork<Void>() {
 			@Override
 			public Void doWork() throws Exception {
-				run(execute, 0);
+				run(execute);
 				return null;
 			}
 		};
 		AuthenticationUtil.runAsSystem(runAs);
 	}
 	
-	private void run(boolean execute, int page) {
-		ESSearchParameters sp = new ESSearchParameters();
-		sp.setLanguage(SearchService.LANGUAGE_LUCENE);
-		sp.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
-		sp.setSkipCount(page);
-		sp.setMaxItems(PAGE_SIZE);
-		sp.setAuthorities(new String[] {PermissionService.ALL_AUTHORITIES});
-		sp.setQuery("TYPE:\"ccm:io\" OR TYPE:\"ccm:map\"");
-		
-		ResultSet resultSet = searchService.query(sp);
-		logger.info("page " + page + " from " + resultSet.getNumberFound());
-		
-		for(NodeRef nodeRef : resultSet.getNodeRefs()) {
-			worker.work(nodeRef);
-		}
-		
-		if(resultSet.hasMore()) {
-			run(execute, page + PAGE_SIZE);
+	public void run(boolean execute) {
+		NodeRef companyHome = repositoryHelper.getCompanyHome();
+		work(companyHome);
+	}
+	
+	private void work(NodeRef nodeRef) {
+		QName type = nodeService.getType(nodeRef);
+		worker.work(nodeRef);
+		if(type.equals(ContentModel.TYPE_FOLDER) 
+				|| type.equals(QName.createQName(CCConstants.CCM_TYPE_MAP))) {
+			
+			String mapType = (String) nodeService.getProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_MAP_TYPE));
+			if(CCConstants.CCM_VALUE_MAP_TYPE_EDUGROUP.equals(mapType)) {
+				return;
+			}
+			
+			List<ChildAssociationRef> childRefs = nodeService.getChildAssocs(nodeRef);
+			for(ChildAssociationRef childRef : childRefs) {
+				this.work(childRef.getChildRef());
+			}
 		}
 	}
 
 	@Override
 	public Class[] getJobClasses() {
-		this.addJobClass(TakeBackGlobalShare.class);
+		this.addJobClass(TakeBackGlobalShareNodeService.class);
 		return this.allJobs;
 	}
 
