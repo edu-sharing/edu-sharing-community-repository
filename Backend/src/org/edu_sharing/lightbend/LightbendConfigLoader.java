e13d47d9d53cbb6a8afc7dd28f5190e2abb2d669
Herr Frank Thomschke <thomschke@edu-sharing.net>
2020-05-15 16:02:56 +0200

    append deployment level for edu-sharing.conf

--- Backend/src/org/edu_sharing/lightbend/LightbendConfigLoader.java
+++ Backend/src/org/edu_sharing/lightbend/LightbendConfigLoader.java
@@ -1,25 +1,28 @@
 package org.edu_sharing.lightbend;
 
 import com.typesafe.config.Config;
-import com.typesafe.config.ConfigBeanFactory;
 import com.typesafe.config.ConfigFactory;
 import org.edu_sharing.repository.server.tools.HttpQueryTool;
 import org.edu_sharing.service.connector.ConnectorServiceFactory;
 
 public class LightbendConfigLoader {
-    public static String PATH_PREFIX="config/";
-    public static String BASE_FILE="edu-sharing.base.conf";
-    public static String CUSTOM_FILE="edu-sharing.conf";
-    public static Config get(){
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        String base = PATH_PREFIX+BASE_FILE;
-        String custom = PATH_PREFIX+CUSTOM_FILE;
-        return ConfigFactory.parseResourcesAnySyntax(custom).withFallback(ConfigFactory.parseResourcesAnySyntax(base)).resolve();
-        //ConfigBeanFactory.create()
+    public static String PATH_PREFIX = "config/";
+    public static String BASE_FILE = "edu-sharing.base.conf";
+    public static String DEPLOYMENT_FILE = "edu-sharing.deployment.conf";
+    public static String CUSTOM_FILE = "edu-sharing.conf";
+
+    public static Config get() {
+        String base = PATH_PREFIX + BASE_FILE;
+        String deployment = PATH_PREFIX + DEPLOYMENT_FILE;
+        String custom = PATH_PREFIX + CUSTOM_FILE;
+        return ConfigFactory.parseResourcesAnySyntax(custom)
+                .withFallback(ConfigFactory.parseResourcesAnySyntax(deployment)
+                        .withFallback(ConfigFactory.parseResourcesAnySyntax(base)))
+                .resolve();
     }
 
     public static void refresh() {
         ConnectorServiceFactory.invalidate(); // reinit connectors data
-        HttpQueryTool.initFinished=false; // reinit proxy settings
+        HttpQueryTool.initFinished = false; // reinit proxy settings
     }
 }
