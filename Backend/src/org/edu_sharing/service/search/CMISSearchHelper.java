de0d9e26d945d650b2490dd411418c1bd3fb0894
Daniel Rudolph <rudolph@metaventis.com>
2020-06-04 12:04:51 +0200

    fix: allow results > 1000 and paging for CMISSearchHelper

--- Backend/src/org/edu_sharing/service/search/CMISSearchHelper.java
+++ Backend/src/org/edu_sharing/service/search/CMISSearchHelper.java
@@ -1,7 +1,10 @@
 package org.edu_sharing.service.search;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.alfresco.service.ServiceRegistry;
-import org.alfresco.service.cmr.dictionary.AspectDefinition;
 import org.alfresco.service.cmr.dictionary.PropertyDefinition;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
@@ -9,34 +12,34 @@ import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
 import org.alfresco.service.namespace.QName;
-import org.apache.chemistry.opencmis.client.api.QueryStatement;
 import org.apache.log4j.Logger;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
 import org.edu_sharing.repository.client.tools.CCConstants;
 import org.springframework.context.ApplicationContext;
-import org.springframework.jdbc.core.SqlParameterValue;
-
-import java.util.List;
-import java.util.Map;
 
 public class CMISSearchHelper {
 
     private static Logger logger= Logger.getLogger(CMISSearchHelper.class);
 
-    public static List<NodeRef> fetchNodesByTypeAndFilters(String nodeType, Map<String,String> filters){
-        ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
+    public static ResultSet fetchNodesByTypeAndFilters(String nodeType, Map<String,String> filters, int from, int pageSize, int maxPermissionChecks){
+    	logger.info("from: "+from+ " pageSize:"+ pageSize);
+    	ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
         ServiceRegistry serviceRegistry = (ServiceRegistry) applicationContext.getBean(ServiceRegistry.SERVICE_REGISTRY);
 
         SearchParameters params=new SearchParameters();
         params.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
         // will use the database
         params.setLanguage(SearchService.LANGUAGE_CMIS_ALFRESCO);
-        params.setMaxItems(Integer.MAX_VALUE);
+        
+        params.setSkipCount(from);
+        params.setMaxItems(pageSize);
+        params.setMaxPermissionChecks(maxPermissionChecks);
         String tableName=CCConstants.getValidLocalName(nodeType);
         String tableNameAlias=tableName.split(":")[1];
         StringBuilder join= new StringBuilder();
         StringBuilder where= new StringBuilder();
         if(filters!=null && filters.size()>0){
+            List<String> joinedTable = new ArrayList<>();
             for(Map.Entry<String,String> filter : filters.entrySet()){
                 if(filter.getKey().startsWith("cmis")){
                     if(where.length() > 0) {
@@ -50,9 +53,12 @@ public class CMISSearchHelper {
                     // join the needed aspect, and access this ones value
                     PropertyDefinition property = serviceRegistry.getDictionaryService().getProperty(QName.createQName(filter.getKey()));
                     String aspectTable=CCConstants.getValidLocalName(property.getContainerClass().getName().toString());
-                    String aspectTableAlias=property.getContainerClass().getName().getLocalName();
-                    join.append("JOIN ").append(aspectTable).append(" AS ").append(aspectTableAlias)
-                        .append(" ON ").append(aspectTableAlias).append(".cmis:objectId = ").append(tableNameAlias).append(".cmis:objectId ");
+                    String aspectTableAlias = property.getContainerClass().getName().getLocalName();
+                    if(!joinedTable.contains(aspectTable)) {
+                        join.append("JOIN ").append(aspectTable).append(" AS ").append(aspectTableAlias)
+                                .append(" ON ").append(aspectTableAlias).append(".cmis:objectId = ").append(tableNameAlias).append(".cmis:objectId ");
+                        joinedTable.add(aspectTable);
+                    }
                     if(where.length() > 0) {
                         where.append(" AND ");
                     }else {
@@ -70,8 +76,31 @@ public class CMISSearchHelper {
         String query="SELECT "+tableNameAlias+".cmis:name FROM "+ tableName + " AS " + tableNameAlias + " " + join + where;
         params.setQuery(query);
         ResultSet result = serviceRegistry.getSearchService().query(params);
-        logger.info(query+": "+result.getNodeRefs().size());
-        return result.getNodeRefs();
+        
+        logger.info(query+": "+result.getNumberFound() +" "+ result.length() +" "+ result.getClass().getName() +" getBulkFetchSize: "+ result.getBulkFetchSize()+" "+result);
+        return result;
+    }
+    
+    public static List<NodeRef> fetchNodesByTypeAndFilters(String nodeType, Map<String,String> filters, int maxPermissionChecks){
+    	List<NodeRef> result = new ArrayList<NodeRef>();
+        
+        int from = 0;
+        
+        int pageSize = 1000;
+        
+        ResultSet resultSet = null;
+        do {
+     	   resultSet = fetchNodesByTypeAndFilters(nodeType, filters, from, pageSize, maxPermissionChecks);
+     	   result.addAll(resultSet.getNodeRefs());
+     	   from += pageSize;
+        }while(resultSet.length() > 0);
+          
+        logger.info("result:" + result.size());
+        return result;
+    }
+    
+    public static List<NodeRef> fetchNodesByTypeAndFilters(String nodeType, Map<String,String> filters){
+       return fetchNodesByTypeAndFilters(nodeType,filters,1000);
     }
 
     /**
