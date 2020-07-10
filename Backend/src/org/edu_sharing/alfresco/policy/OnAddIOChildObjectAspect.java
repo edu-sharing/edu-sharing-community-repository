d971cdffa30d45132fc53ede4200b6ceeb45fcc2
Daniel Rudolph <rudolph@metaventis.com>
2020-07-10 16:59:32 +0200

    Fix: comments will not be seen by owner

--- Backend/src/org/edu_sharing/alfresco/policy/OnAddIOChildObjectAspect.java
+++ Backend/src/org/edu_sharing/alfresco/policy/OnAddIOChildObjectAspect.java
@@ -1,19 +1,29 @@
 package org.edu_sharing.alfresco.policy;
 
+import org.alfresco.model.ContentModel;
+import org.alfresco.model.ImapModel;
 import org.alfresco.repo.node.NodeServicePolicies;
+import org.alfresco.repo.policy.Behaviour;
 import org.alfresco.repo.policy.JavaBehaviour;
 import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.security.AccessPermission;
 import org.alfresco.service.cmr.security.OwnableService;
 import org.alfresco.service.cmr.security.PermissionService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 import org.edu_sharing.repository.client.tools.CCConstants;
 
+import java.util.Set;
 
-public class OnAddIOChildObjectAspect implements NodeServicePolicies.OnAddAspectPolicy {
+/**
+ * set owner of parent object to coordinator so that this permission inherits to child object (cild io or comments)
+ *
+ */
+public class OnAddIOChildObjectAspect implements NodeServicePolicies.OnAddAspectPolicy, NodeServicePolicies.OnCreateChildAssociationPolicy {
 
     PolicyComponent policyComponent;
     NodeService nodeService;
@@ -26,10 +36,15 @@ public class OnAddIOChildObjectAspect implements NodeServicePolicies.OnAddAspect
         policyComponent.bindClassBehaviour(NodeServicePolicies.OnAddAspectPolicy.QNAME,
                 QName.createQName(CCConstants.CCM_ASPECT_IO_CHILDOBJECT ),
                 new JavaBehaviour(this, "onAddAspect"));
+
+        policyComponent.bindAssociationBehaviour(NodeServicePolicies.OnCreateChildAssociationPolicy.QNAME,
+                QName.createQName(CCConstants.CCM_TYPE_IO), QName.createQName(CCConstants.CCM_ASSOC_COMMENT),
+                new JavaBehaviour(this, "onCreateChildAssociation"));
+
     }
 
     /**
-     * set owner of parent object to coordinator so that this permission inherits to child_io
+     *
      * @param nodeRef
      * @param aspectTypeQName
      */
@@ -38,11 +53,34 @@ public class OnAddIOChildObjectAspect implements NodeServicePolicies.OnAddAspect
 
         ChildAssociationRef parentAssoc = nodeService.getPrimaryParent(nodeRef);
         if(QName.createQName(CCConstants.CCM_TYPE_IO).equals(nodeService.getType(parentAssoc.getParentRef()))){
-            String owner = ownableService.getOwner(parentAssoc.getParentRef());
-            permissionService.setPermission(parentAssoc.getParentRef(), owner, PermissionService.COORDINATOR, true);
+            ownerAsCoordinator(parentAssoc.getParentRef());
         }else{
             logger.error("parent is no ccm:io");
         }
+    }
+
+    /**
+     *
+     * @param childAssociationRef
+     * @param b
+     */
+    @Override
+    public void onCreateChildAssociation(ChildAssociationRef childAssociationRef, boolean b) {
+        ownerAsCoordinator(childAssociationRef.getParentRef());
+    }
+
+    private void ownerAsCoordinator(NodeRef nodeRef){
+        AuthenticationUtil.runAsSystem(()->{
+            String owner = ownableService.getOwner(nodeRef);
+            Set<AccessPermission> allSetPermissions = permissionService.getAllSetPermissions(nodeRef);
+            for(AccessPermission ap : allSetPermissions){
+                if(ap.getAuthority().equals(owner) && PermissionService.COORDINATOR.equals(ap.getPermission())){
+                    return null;
+                }
+            }
+            permissionService.setPermission(nodeRef, owner, PermissionService.COORDINATOR, true);
+            return null;
+        });
     }
 
     public void setPolicyComponent(PolicyComponent policyComponent) {
@@ -60,4 +98,6 @@ public class OnAddIOChildObjectAspect implements NodeServicePolicies.OnAddAspect
     public void setOwnableService(OwnableService ownableService) {
         this.ownableService = ownableService;
     }
+
+
 }
