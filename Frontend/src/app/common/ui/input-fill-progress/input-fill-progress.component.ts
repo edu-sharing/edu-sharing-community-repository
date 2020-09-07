5656069e5541dcf4412c85d715b7f472b10cc363
Torsten Simon <simon@edu-sharing.net>
2020-09-07 11:17:58 +0200

    feat:prepare input fill progress component

--- Frontend/src/app/common/ui/input-fill-progress/input-fill-progress.component.ts
+++ Frontend/src/app/common/ui/input-fill-progress/input-fill-progress.component.ts
@@ -31,7 +31,14 @@ export class InputFillProgressComponent {
     }
 
     getFullProgress() {
-        return ((this.current.required || 0) + (this.current.requiredPublish || 0)) / ((this.maximum.required || 0) + (this.maximum.requiredPublish || 0))
+        let sum=this.current.required;
+        if(this.current.required === this.maximum.required) {
+            sum += (this.current.requiredPublish || 0)
+            if(this.current.requiredPublish === this.maximum.requiredPublish) {
+                sum += (this.current.optional || 0)
+            }
+        }
+        return sum;
     }
     getPrimaryProgress() {
         return (this.current as any)[this.getStatus()] / (this.maximum as any)[this.getStatus()];
@@ -43,6 +50,13 @@ export class InputFillProgressComponent {
         }
         return this.maximum.required + (this.maximum.requiredPublish || 0);
     }
+    getSum(what: 'current' | 'maximum') {
+        let sum = 0;
+        for(const type of Object.keys(FillType)) {
+            sum += (this as any)[what][type] || 0;
+        }
+        return sum;
+    }
 }
 export type FillTypeStatus = {
     [key in FillType]: number;
@@ -52,4 +66,5 @@ export enum FillType {
     required = 'required',
     requiredPublish = 'requiredPublish',
     //recommended = 'recommended'
+    optional = 'optional'
 }
