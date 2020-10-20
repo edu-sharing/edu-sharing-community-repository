e489ff5462c341008fc89628d5f24ff615150431
Edmond Kacaj <programim95@gmail.com>
2020-10-20 12:31:36 +0200

    Add a property call "workSpaceViewType" for make configurable viewType in workspace component

--- Backend/src/org/edu_sharing/service/config/model/Values.java
+++ Backend/src/org/edu_sharing/service/config/model/Values.java
@@ -43,6 +43,7 @@ public class Values implements Serializable {
 	@XmlElement	public AvailableMds[] availableMds;
 	@XmlElement	public String[] availableRepositories;
 	@XmlElement	public Integer searchViewType;
+	@XmlElement	public Integer workSpaceViewType;
 	@XmlElement	public Integer itemsPerRequest;
 	@XmlElement	public Rendering rendering;
 	@XmlElement	public SessionExpiredDialog sessionExpiredDialog;
