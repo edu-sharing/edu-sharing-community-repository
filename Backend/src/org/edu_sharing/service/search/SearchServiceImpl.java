3fdd21e3b0d1002dd7c0708d08e978cf014c58f4
Torsten Simon <simon@edu-sharing.net>
2020-10-27 14:25:28 +0100

    fix:shared to me + by me via mds

--- Backend/src/org/edu_sharing/service/search/SearchServiceImpl.java
+++ Backend/src/org/edu_sharing/service/search/SearchServiceImpl.java
@@ -26,6 +26,8 @@ import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
 import org.edu_sharing.metadataset.v2.MetadataQueries;
 import org.edu_sharing.metadataset.v2.MetadataSetV2;
 import org.edu_sharing.metadataset.v2.SearchCriterias;
+import org.edu_sharing.metadataset.v2.tools.MetadataHelper;
+import org.edu_sharing.metadataset.v2.tools.MetadataSearchHelper;
 import org.edu_sharing.repository.client.rpc.Authority;
 import org.edu_sharing.repository.client.rpc.EduGroup;
 import org.edu_sharing.repository.client.tools.CCConstants;
@@ -86,10 +88,11 @@ public class SearchServiceImpl implements SearchService {
 		token.setMaxResult(maxItems);
 		token.setSortDefinition(sortDefinition);
 		token.setContentType(contentType);
-
-		token.setLuceneString("(TYPE:\"" + CCConstants.CCM_TYPE_IO + "\" OR TYPE:\"" + CCConstants.CCM_TYPE_MAP +"\") "
-				+ " AND NOT ASPECT:\""+CCConstants.CCM_ASPECT_COLLECTION_IO_REFERENCE+"\""
-				+ " AND NOT ASPECT:\""+CCConstants.CCM_ASPECT_COLLECTION+"\" AND @ccm\\:ph_users:\"" + QueryParser.escape(username) + "\"");
+		String mdsQuery = MetadataSearchHelper.getLuceneString(
+				"sharedByMe",
+				null
+		);
+		token.setLuceneString(mdsQuery + " AND @ccm\\:ph_users:\"" + QueryParser.escape(username) + "\"");
 		return search(token);
 	}
 
@@ -108,11 +111,12 @@ public class SearchServiceImpl implements SearchService {
 		memberships.add(username);
 		memberships.addAll(serviceRegistry.getAuthorityService().getAuthorities());
 		memberships.remove(CCConstants.AUTHORITY_GROUP_EVERYONE);
-
-		StringBuilder query= new StringBuilder("(TYPE:\"" + CCConstants.CCM_TYPE_IO + "\" OR TYPE:\"" + CCConstants.CCM_TYPE_MAP + "\") "
-				+ "AND ISNOTNULL:\"ccm:ph_users\" "
-				+ "AND NOT ASPECT:\""+CCConstants.CCM_ASPECT_COLLECTION_IO_REFERENCE+"\""
-				+ "AND NOT ASPECT:\""+CCConstants.CCM_ASPECT_COLLECTION+"\" AND NOT @ccm\\:ph_users:\"" + QueryParser.escape(username) + "\""
+		String mdsQuery = MetadataSearchHelper.getLuceneString(
+				"sharedToMe",
+				null
+		);
+		StringBuilder query= new StringBuilder(mdsQuery + " AND ("
+				+ "NOT @ccm\\:ph_users:\"" + QueryParser.escape(username) + "\""
 				+ " AND (");
 		int i=0;
 		for(String m : memberships){
@@ -120,6 +124,7 @@ public class SearchServiceImpl implements SearchService {
 				query.append(" OR ");
 			query.append("@ccm\\:ph_invited:\"").append(QueryParser.escape(m)).append("\"");
 		}
+		query.append(")");
 		query.append(")");
 		token.setLuceneString(query.toString());
 
