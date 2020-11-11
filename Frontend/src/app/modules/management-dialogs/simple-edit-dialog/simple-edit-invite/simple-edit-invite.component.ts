ff01a592b5f7314fa93d16d2a7bc9e6ab2aec8e3
Torsten Simon <simon@edu-sharing.net>
2020-11-11 17:39:44 +0100

    fix:simple edit handle org error

--- Frontend/src/app/modules/management-dialogs/simple-edit-dialog/simple-edit-invite/simple-edit-invite.component.ts
+++ Frontend/src/app/modules/management-dialogs/simple-edit-dialog/simple-edit-invite/simple-edit-invite.component.ts
@@ -205,6 +205,7 @@ export class SimpleEditInviteComponent {
     subscribe((permissions) => {
       this.nodesPermissions = permissions.map((p) => p.permissions);
       this.inherited = permissions.some((p) => p.permissions.localPermissions.inherited);
+      // The amount of orgs is still limited to the maximum amount returned by default!
       this.organizationApi.getOrganizations().subscribe((orgs) => {
         const filter = this.configService.instant('simpleEdit.organizationFilter');
         if(filter) {
@@ -228,6 +229,10 @@ export class SimpleEditInviteComponent {
                     groups.forEach((g) => {
                       o.groups[g.group.profile.groupType] = g.group;
                     });
+                    observer.next(o);
+                    observer.complete();
+                  }, error => {
+                    console.warn('Some group roles could not be found', error)
                     observer.next(o);
                     observer.complete();
                   });
