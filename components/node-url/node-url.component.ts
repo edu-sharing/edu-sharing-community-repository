4d4c1a4a8871e1bc7ff16111b243a74e316f2999
Christopher Lübbemeier <luebbemeier@metaventis.com>
2020-12-02 11:20:44 +0100

    style: format node-url

--- components/node-url/node-url.component.ts
+++ components/node-url/node-url.component.ts
@@ -1,75 +1,80 @@
-import {Component, Input} from '@angular/core';
-import {Node} from '../../../core-module/rest/data-object';
-import {Params} from "@angular/router";
-import {NodeHelper} from "../../node-helper";
-import {UIConstants} from "../../../core-module/ui/ui-constants";
-import {RestNetworkService} from "../../../core-module/rest/services/rest-network.service";
+import { Component, Input } from '@angular/core';
+import { Params } from '@angular/router';
+import { Node } from '../../../core-module/rest/data-object';
+import { RestNetworkService } from '../../../core-module/rest/services/rest-network.service';
+import { UIConstants } from '../../../core-module/ui/ui-constants';
+import { NodeHelper } from '../../node-helper';
 
 @Component({
-  selector: 'app-node-url',
-  template: `
-    <ng-template #content><ng-content></ng-content></ng-template>
-    <a *ngIf="unclickable">
-      <ng-container *ngTemplateOutlet="content"></ng-container>
-    </a>
-    <a *ngIf="!unclickable"
-      [routerLink]="get('routerLink')"
-      [state]="getState()"
-      [queryParams]="get('queryParams')">
-      <ng-container *ngTemplateOutlet="content"></ng-container>
-    </a>`,
+    selector: 'app-node-url',
+    template: ` <ng-template #content><ng-content></ng-content></ng-template>
+        <a *ngIf="unclickable">
+            <ng-container *ngTemplateOutlet="content"></ng-container>
+        </a>
+        <a
+            *ngIf="!unclickable"
+            [routerLink]="get('routerLink')"
+            [state]="getState()"
+            [queryParams]="get('queryParams')"
+        >
+            <ng-container *ngTemplateOutlet="content"></ng-container>
+        </a>`,
 })
 export class NodeUrlComponent {
-  private _node: Node;
-  @Input() set node (node: Node) {
-    this._node = node;
-  }
-  @Input() nodes: Node[];
-  @Input() scope: string;
-  @Input() unclickable: boolean;
-  constructor() {
-  }
-  getState() {
-    return {
-      nodes: this.nodes,
-      scope: this.scope
-    };
-  }
-  get(mode: 'routerLink' | 'queryParams'): any {
-    return NodeUrlComponent.getInfo(mode, this._node);
-  }
-  static getInfo(mode: 'routerLink' | 'queryParams', node: Node){
-    if (!node) {
-      return null;
+    @Input() set node(node: Node) {
+        this._node = node;
     }
-    let data: { routerLink: string, queryParams: Params } = null;
-    if (NodeHelper.isNodeCollection(node)) {
-      data = {
-        routerLink: UIConstants.ROUTER_PREFIX + 'collections',
-        queryParams: { id: node.ref.id },
-      };
-    } else {
-      if (node.isDirectory) {
-        data = {
-          routerLink: UIConstants.ROUTER_PREFIX + 'workspace',
-          queryParams: { id: node.ref.id },
+    @Input() nodes: Node[];
+    @Input() scope: string;
+    @Input() unclickable: boolean;
+
+    private _node: Node;
+
+    static getInfo(mode: 'routerLink' | 'queryParams', node: Node) {
+        if (!node) {
+            return null;
+        }
+        let data: { routerLink: string; queryParams: Params } = null;
+        if (NodeHelper.isNodeCollection(node)) {
+            data = {
+                routerLink: UIConstants.ROUTER_PREFIX + 'collections',
+                queryParams: { id: node.ref.id },
+            };
+        } else {
+            if (node.isDirectory) {
+                data = {
+                    routerLink: UIConstants.ROUTER_PREFIX + 'workspace',
+                    queryParams: { id: node.ref.id },
+                };
+            } else if (node.ref) {
+                const fromHome = RestNetworkService.isFromHomeRepo(node);
+                data = {
+                    routerLink: UIConstants.ROUTER_PREFIX + 'render/' + node.ref.id,
+                    queryParams: {
+                        repository: fromHome ? null : node.ref.repo,
+                    },
+                };
+            }
+        }
+        if (data === null) {
+            return '';
+        }
+        if (mode === 'routerLink') {
+            return '/' + data.routerLink;
+        }
+        return data.queryParams;
+    }
+
+    constructor() {}
+
+    getState() {
+        return {
+            nodes: this.nodes,
+            scope: this.scope,
         };
-      } else if(node.ref) {
-        const fromeHome = RestNetworkService.isFromHomeRepo(node);
-        data = {
-          routerLink: UIConstants.ROUTER_PREFIX + 'render/' + node.ref.id,
-          queryParams: {
-            repository: fromeHome ? null : node.ref.repo,
-          },
-        };
-      }
     }
-    if(data === null) {
-      return '';
+
+    get(mode: 'routerLink' | 'queryParams'): any {
+        return NodeUrlComponent.getInfo(mode, this._node);
     }
-    if(mode === 'routerLink') {
-      return '/' + data.routerLink;
-    }
-    return data.queryParams;
-  }
 }
