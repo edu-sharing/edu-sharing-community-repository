0a1f9e9ddf0e7913eb6d56239fe9d67a973ad130
Torsten Simon <simon@edu-sharing.net>
2021-01-05 16:06:14 +0100

    feat:simple edit global groups based on toolpermissions

--- Backend/src/org/edu_sharing/service/config/model/SimpleEditGlobalGroups.java
+++ Backend/src/org/edu_sharing/service/config/model/SimpleEditGlobalGroups.java
@@ -1,2 +1,11 @@
-package org.edu_sharing.service.config.model;public class SimpleEditGlobalGroups {
+package org.edu_sharing.service.config.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.io.Serializable;
+
+public class SimpleEditGlobalGroups implements Serializable {
+    @XmlElement
+    public String toolpermission;
+    @XmlElement
+    public String[] groups;
 }
