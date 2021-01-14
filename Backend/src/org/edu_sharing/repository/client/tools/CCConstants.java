52eb5f9ae9675340ce1c7a8c828c2ac2034542fa
Daniel Rudolph <rudolph@metaventis.com>
2021-01-14 10:38:40 +0100

    fix: revert of 4.2 nodes fails cause of missing mandatory aspect

--- Backend/src/org/edu_sharing/repository/client/tools/CCConstants.java
+++ Backend/src/org/edu_sharing/repository/client/tools/CCConstants.java
@@ -419,6 +419,8 @@ public class CCConstants {
 
 	public final static String CCM_ASPECT_TRACKING = "{http://www.campuscontent.de/model/1.0}tracking";
 
+	public final static String CCM_ASPECT_EDUCONTEXT = "{http://www.campuscontent.de/model/1.0}educontext";
+
 	public final static String CCM_PROP_TOOL_OBJECT_TOOLINSTANCEREF = "{http://www.campuscontent.de/model/1.0}tool_instance_ref";
 
 	public final static String CCM_PROP_IO_REF_TITLE = "{http://www.campuscontent.de/model/1.0}ref_title";
