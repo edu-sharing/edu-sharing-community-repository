6003bc083bfbd37396590a26ab68f471c16523d4
Torsten Simon <simon@edu-sharing.net>
2021-06-02 13:17:59 +0200

    fix:commit 527c208d331bee7f4eb42434586a59fe51a8dc5d lost

---
+++ Frontend/src/app/extension/extension-imports.ts
@@ -0,0 +1,5 @@
+// Overwrite this file in extensions to define additional routes.
+
+import { ModuleWithProviders, Type } from '@angular/core';
+
+export const extensionImports: (any[] | Type<any> | ModuleWithProviders<{}>)[] = [];
