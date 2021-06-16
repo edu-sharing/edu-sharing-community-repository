e19a367971e3e30efba1b5758d4102b93f2a9f5c
Torsten Simon <simon@edu-sharing.net>
2021-06-16 14:12:09 +0200

    feat:support splash, deprecate old preview tool via home app

--- Backend/src/org/edu_sharing/repository/server/tools/ApplicationInfo.java
+++ Backend/src/org/edu_sharing/repository/server/tools/ApplicationInfo.java
@@ -164,7 +164,8 @@ public class ApplicationInfo implements Comparable<ApplicationInfo>, Serializabl
 	public static final String AUTHBYAPP_USERNAME_PROP_USERNAME = "USERNAME";
 
 	public static final String AUTHBYAPP_USER_WHITELIST = "auth_by_app_user_whitelist";
-	
+
+	@Deprecated
 	public static final String WEBSITEPREVIEWRENDERSERVICE = "websitepreviewrenderservice";
 
 	public static final String NOTIFY_FETCH_LIMIT = "notify_fetch_limit";
@@ -899,6 +900,7 @@ public class ApplicationInfo implements Comparable<ApplicationInfo>, Serializabl
 		return validatorRegexCMName;
 	}
 
+	@Deprecated
 	public String getWebsitepreviewrenderservice() {
 		return websitepreviewrenderservice;
 	}	
