39ae3dc4a28278d8b10e76a6854a1dd5d1603c37
Frank Thomschke <frank@thomschke.de>
2021-06-24 08:43:57 +0200

    tomcat-8 support

--- Backend/tomcat/src/main/java/org/edu_sharing/catalina/startup/OrderedHostConfig.java
+++ Backend/tomcat/src/main/java/org/edu_sharing/catalina/startup/OrderedHostConfig.java
@@ -1,5 +1,7 @@
 package org.edu_sharing.catalina.startup;
 
+import org.apache.catalina.Container;
+import org.apache.catalina.Engine;
 import org.apache.catalina.startup.HostConfig;
 
 import java.io.File;
@@ -12,11 +14,34 @@ public class OrderedHostConfig extends HostConfig {
 
     @Override
     protected void deployApps() {
-        File appBase = appBase();
+        // Tomcat 7
+        // File appBase = appBase();
+        // Tomcat 8
+        // File appBase = host.getAppBaseFile();
+        // Tomcat 7+8
+        File appBase = returnCanonicalPath(host.getAppBase());
         String[] sortedApps = appBase.list();
         Arrays.sort(sortedApps, COMPARE);
 
-        File configBase = configBase();
+        // Tomcat 7
+        // File configBase = configBase();
+        // Tomcat 8
+        // File configBase = host.getConfigBaseFile();
+        // Tomcat 7+8
+        File configBase = null;
+        if (host.getXmlBase()!=null) {
+            configBase = returnCanonicalPath(host.getXmlBase());
+        } else {
+            StringBuilder xmlDir = new StringBuilder("conf");
+            Container parent = host.getParent();
+            if (parent instanceof Engine) {
+                xmlDir.append('/');
+                xmlDir.append(parent.getName());
+            }
+            xmlDir.append('/');
+            xmlDir.append(host.getName());
+            configBase = returnCanonicalPath(xmlDir.toString());
+        }
         String[] sortedContext = configBase.list();
         Arrays.sort(sortedContext, COMPARE);
 
