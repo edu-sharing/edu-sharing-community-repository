2db3def6f64483013543ffe4062c0cebe2156f1e
Torsten Simon <simon@edu-sharing.net>
2021-10-19 17:30:03 +0200

    fix:accessibility

--- Frontend/src/app/core-ui-module/components/node-url/node-url.component.ts
+++ Frontend/src/app/core-ui-module/components/node-url/node-url.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
 import { Node } from '../../../core-module/rest/data-object';
 import { NodeHelperService } from '../../node-helper.service';
 import { ListTableComponent } from '../list-table/list-table.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PlatformLocation} from '@angular/common';
 
 // TODO: Decide if providing focus highlights and ripples with this component is a good idea. When
 // using `app-node-url` for cards, we might need highlights and ripples for the whole card while
@@ -35,6 +37,18 @@ import { ListTableComponent } from '../list-table/list-table.component';
                 <ng-container *ngTemplateOutlet="content"></ng-container>
             </span>
         </ng-container>
+        <ng-container *ngIf="mode === 'wrapper'">
+            <div
+                class="node-url-wrapper"
+                #wrapper
+                matRipple
+                matRippleColor="primary"
+                [matRippleDisabled]="disabled && !alwaysRipple"
+                (click)="clickWrapper($event)"
+            >
+                <ng-container *ngTemplateOutlet="content"></ng-container>
+            </div>
+        </ng-container>
         <ng-container *ngIf="mode === 'button'">
             <button
                 [disabled]="disabled"
@@ -57,7 +71,12 @@ export class NodeUrlComponent {
     @Input() node: Node;
     @Input() nodes: Node[];
     @Input() scope: string;
-    @Input() mode: 'link' | 'button' = 'link';
+    /**
+     * link: a element
+     * button: button element
+     * wrapper: div element with behaviour "like" a link
+     */
+    @Input() mode: 'link' | 'button' | 'wrapper' = 'link';
     @Input() disabled = false;
     /**
      * Show the ripple effect even when disabled.
@@ -71,7 +90,11 @@ export class NodeUrlComponent {
 
     @Output() buttonClick = new EventEmitter<MouseEvent>();
 
-    constructor(private nodeHelper: NodeHelperService) {}
+    constructor(
+        private nodeHelper: NodeHelperService,
+        private router: Router,
+        private platformLocation: PlatformLocation,
+    ) {}
 
     getState() {
         return {
@@ -85,5 +108,24 @@ export class NodeUrlComponent {
 
     focus(): void {
         this.link.nativeElement.focus();
+    }
+
+    clickWrapper(event: MouseEvent) {
+        if(event.ctrlKey) {
+            const url = this.router.serializeUrl(this.router.createUrlTree(
+                [this.platformLocation.getBaseHrefFromDOM() + this.get('routerLink')], {
+                    queryParams: this.get('queryParams'),
+                    queryParamsHandling: 'merge',
+                }));
+            window.open(url);
+        } else {
+            this.router.navigate([this.get('routerLink')], {
+                queryParams: this.get('queryParams'),
+                state: this.getState(),
+                queryParamsHandling: 'merge',
+
+            });
+        }
+        event.preventDefault();
     }
 }
