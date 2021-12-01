85b8f2021c0370e7e87883fe75ac5d4ab2e258a7
Sebastian Wollner <wollner@edu-sharing.net>
2021-12-01 15:35:35 +0100

    feat: rendering: config can now be changed during the update; added config for proxy; configuration loaded via optional .env.base file and optional .env files; all other params have been removed; writes config to log file;

--- deploy/installer/rendering/scripts/debian/bullseye/src/main/scripts/install.sh
+++ deploy/installer/rendering/scripts/debian/bullseye/src/main/scripts/install.sh
@@ -19,67 +19,20 @@ pushd $WWW_ROOT
 #}
 
 
+# load the default configuration
+if [[ -f ".env.base" ]] ; then
+	source .env.base
+fi
+
 
 usage() {
 	echo "Options:"
 	echo ""
 
-	echo "--interalURL"
-	echo "internal application url"
-
-	echo "--externalURL"
-	echo "external application url"
-
-	echo "-d dbname"
-  echo "--dbname=dbname"
-  echo "Specifies the name of the database to connect to."
-	echo ""
-
-	echo "-h hostname"
-  echo "--host=hostname"
-  echo "Specifies the host name of the machine on which the server is running."
-	echo ""
-
-  echo "-p port"
-  echo "--port=port"
-  echo " Specifies the TCP port on which the server is listening for connections."
-	echo ""
-
-  echo "-U username"
-  echo "--username=username"
-  echo " Connect to the database as the user username."
-	echo ""
-
-  echo "-W"
-  echo "--password"
-  echo "Connect to the database with the user password."
-	echo ""
-
-  echo "-R"
-  echo "--repository"
-  echo "url of the repository to fetch properties and content from"
-  echo ""
-
-  echo "--repoUser"
-  echo "The user that will be used to automatically register the rendering service with the repository"
-  echo ""
-
-  echo "--repoPassword"
-  echo "The user password that is used to automatically register the rendering service with the repository"
-  echo ""
-
+	echo "-f = environment file"
+	echo "--file"
+	echo "Loads the configuration from the specified environment file"
 }
-
-internalURL=http://127.0.0.1:8080/esrender
-externalURL=http://127.0.0.1:8080/esrender
-db_host=127.0.0.1
-db_port=5432
-db_name=rendering
-db_user=rendering
-db_password=rendering
-repository_service_base=
-repoUser=admin
-repoPassword=
 
 while true; do
 	flag="$1"
@@ -88,16 +41,7 @@ while true; do
 
 	case "$flag" in
 			--help|'-?') usage && exit 0 ;;
-			--host|-h) db_host="$1" && shift	;;
-			--port|-p) db_port="$1" && shift	;;
-			--dbname|-d) db_name="$1" && shift	;;
-			--username|-U) db_user="$1" && shift	;;
-			--password|-W) db_password="$1" && shift	;;
-			--repository|-R) repository_service_base="$1" && shift ;;
-			--internalURL) internalURL="$1" && shift ;;
-			--externalURL) externalURL="$1" && shift ;;
-			--repoUser) repoUser="$1" && shift ;;
-			--repoPassword) repoPassword="$1" && shift ;;
+			--file|-f) source "$1" && shift	;;
 			*) {
 				echo "error: unknown flag: $flag"
 				usage
@@ -106,6 +50,108 @@ while true; do
 	esac
 done
 
+my_home_appid="${RENDERING_SERVICE_HOME_APPID:-esrender}" # Kundenprojekt ?
+
+my_prot_external="${RENDERING_SERVICE_PUBLIC_PROTOCOL:-http}"
+my_host_external="${RENDERING_SERVICE_PUBLIC_HOST:-localhost}"
+my_port_external="${RENDERING_SERVICE_PUBLIC_PORT:-80}"
+my_path_external="${RENDERING_SERVICE_PUBLIC_PATH:-$(basename "${RS_ROOT}")}"
+
+my_prot_internal="${RENDERING_SERVICE_PRIVATE_PROTOCOL:-http}"
+my_host_internal="${RENDERING_SERVICE_PRIVATE_HOST:-127.0.0.1}"
+my_port_internal="${RENDERING_SERVICE_PRIVATE_PORT:-80}"
+my_path_internal="${RENDERING_SERVICE_PRIVATE_PATH:-$(basename "${RS_ROOT}")}"
+
+my_internal_url="${my_prot_internal}://${my_host_internal}:${my_port_internal}/${my_path_internal}"
+my_external_url="${my_prot_external}://${my_host_external}:${my_port_external}/${my_path_external}"
+
+db_driver=pgsql
+db_host="${RENDERING_DATABASE_HOST:-127.0.0.1}"
+db_port="${RENDERING_DATABASE_PORT:-5432}"
+db_name="${RENDERING_DATABASE_NAME:-rendering}"
+db_user="${RENDERING_DATABASE_USER:-rendering}"
+db_password="${RENDERING_DATABASE_PASS:-rendering}"
+
+repository_service_base="${REPOSITORY_SERVICE:-}"
+repository_user="${REPOSITORY_SERVICE_USER:-}"
+repository_password="${REPOSITORY_SERVICE_PASS:-}"
+
+rendering_proxy_host="${RENDERING_PROXY_HOST:-}"
+rendering_proxy_port="${RENDERING_PROXY_PORT:-}"
+rendering_proxy_user="${RENDERING_PROXY_USER:-}"
+rendering_proxy_pass="${RENDERING_PROXY_PASS:-}"
+
+info() {
+	echo ""
+	echo "#########################################################################"
+	echo ""
+	echo "rendering-database:"
+	echo ""
+	echo "  Database:"
+	echo ""
+	echo "    Host:         ${db_host}"
+	echo "    Port:         ${db_port}"
+	echo "    Name:         ${db_name}"
+	echo ""
+	echo "  Credentials:"
+	echo ""
+	echo "     User:        ${db_user}"
+	echo "     Password:    ${db_password}"
+	echo ""
+	echo "#########################################################################"
+	echo ""
+	echo "rendering-service:"
+	echo ""
+	echo "  Common:"
+	echo ""
+	echo "    AppId:        ${my_home_appid}"
+	echo ""
+	echo "  Public:"
+	echo ""
+	echo "    Protocol:     ${my_prot_external}"
+	echo "    Host:         ${my_host_external}"
+	echo "    Port:         ${my_port_external}"
+	echo "    Path:         ${my_path_external}"
+	echo ""
+  echo "  Private:"
+  echo ""
+  echo "    Protocol:     ${my_prot_internal}"
+  echo "    Host:         ${my_host_internal}"
+  echo "    Port:         ${my_port_internal}"
+  echo "    Path:         ${my_path_internal}"
+  echo ""
+  echo "  Services:"
+  echo ""
+  echo "    Public:       ${my_external_url}"
+  echo "    Private:      ${my_internal_url}"
+  echo ""
+
+	if [[ -n $rendering_proxy_host ]] ; then
+	echo "#########################################################################"
+  echo ""
+  echo "proxy:"
+  echo ""
+  echo "  Host:           ${rendering_proxy_host}"
+  echo "  Port:           ${rendering_proxy_port}"
+  echo "  User:           ${rendering_proxy_user}"
+  echo "  Password:       ${rendering_proxy_pass}"
+  echo ""
+	fi
+
+	if [[ -n $repository_service_base ]] ; then
+  echo "#########################################################################"
+  echo ""
+  echo "repository-service:"
+  echo ""
+  echo "  URL:            ${repository_service_base}"
+  echo "  User:           ${repository_user}"
+  echo "  Password:       ${repository_password}"
+  echo ""
+  fi
+  echo "#########################################################################"
+  echo ""
+  echo ""
+}
 
 
 
@@ -142,9 +188,9 @@ if [[ ! -d "${RS_CACHE}" ]] ; then
   cat >/tmp/config.ini <<-EOF
 		[application]
 		; url for client requests (accessible from the internet)
-		application_url_client=${externalURL}
+		application_url_client=${my_external_url}
 		; url for requests from repository
-		application_url_repository=${internalURL}
+		application_url_repository=${my_internal_url}
 		; ip of the server
 		application_host=
 		; root directory of the rendering service application
@@ -158,7 +204,7 @@ if [[ ! -d "${RS_CACHE}" ]] ; then
 
 		[database]
 		; driver (mysql or pgsql)
-		db_driver=pgsql
+		db_driver=${db_driver}
 		; db host
 		db_host=${db_host}
 		; db port
@@ -175,28 +221,28 @@ if [[ ! -d "${RS_CACHE}" ]] ; then
 		repository_url=${repository_service_base}
 	EOF
 
-  php esrender/admin/cli/install.php -c /tmp/config.ini || {
+  php "${RS_ROOT}"/admin/cli/install.php -c /tmp/config.ini || {
   	result=$?;
   	rm -rf "${RS_CACHE}" 2> /dev/null
-  	rm -rf esrender 2> /dev/null
+  	rm -rf "${RS_ROOT}" 2> /dev/null
   	rm -f /tmp/config.ini 2> /dev/null
   	exit $result
   }
 
 
   rm -f /tmp/config.ini
-	mv esrender/install/ esrender/install.bak
+	mv "${RS_ROOT}"/install/ "${RS_ROOT}"/install.bak
 
-	if [[ -n $repository_service_base ]] && [[ -n $repoUser ]] && [[ -n $repoPassword ]] ; then
+	if [[ -n $repository_service_base ]] && [[ -n $repository_user ]] && [[ -n $repository_password ]] ; then
 		echo "- register rendering service with the repository"
 
-		until [[ $( curl -sSf -w "%{http_code}\n" -o /dev/null "${internalURL}/admin/" ) -eq 200 ]]
+		until [[ $( curl -sSf -w "%{http_code}\n" -o /dev/null "${my_internal_url}/admin/" ) -eq 200 ]]
     do
-    	echo >&2 "Waiting for ${internalURL} ..."
+    	echo >&2 "Waiting for ${my_internal_url} ..."
     	sleep 3
     done
 
-		my_meta_internal="${internalURL}/application/esmain/metadata.php"
+		my_meta_internal="${my_internal_url}/application/esmain/metadata.php"
 		my_appid=$( \
 			curl -sS "${my_meta_internal}" | xmlstarlet sel -t -v '/properties/entry[@key="appid"]' - | xargs echo \
 		)
@@ -204,7 +250,7 @@ if [[ ! -d "${RS_CACHE}" ]] ; then
 		access_token=$( \
   	curl -sS \
   		-XPOST \
-  		-d "grant_type=password&client_id=eduApp&client_secret=secret&username=${repoUser}&password=${repoPassword}" \
+  		-d "grant_type=password&client_id=eduApp&client_secret=secret&username=${repository_user}&password=${repository_password}" \
   		"${repository_service_base}/oauth2/token" | jq -r '.access_token' \
   	)
 
@@ -230,7 +276,7 @@ else
 
 	echo "- make a snapshot of the rendering service"
   snapshot_name=rendering-SNAPSHOT-$(date "+%Y.%m.%d-%H.%M.%S")".tar.gz"
-	tar -czf $snapshot_name esrender
+	tar -czf $snapshot_name $(basename "${RS_ROOT}")
 
 	echo "- cleanup rendering server"
 	rm -rf $RS_ROOT
@@ -238,15 +284,44 @@ else
 	install_edu_sharing
 
   echo "- restore base rendering config"
-  tar -zxf $snapshot_name esrender/conf
-  tar -zxf $snapshot_name --wildcards "*config.php" -C esrender
+  tar -zxf $snapshot_name $(basename "${RS_ROOT}")/conf
+  tar -zxf $snapshot_name --wildcards "*config.php" -C $(basename "${RS_ROOT}")
 
-	yes | php esrender/admin/cli/update.php
+	yes | php "${RS_ROOT}"/admin/cli/update.php
 
 	echo "- delete old rendering SNAPSHOTS (keep 3 backups)"
   ls -pt | grep -v / | grep "rendering-SNAPSHOT" | tail -n +4 | xargs -I {} rm {}
 
 fi
 
+if [[ -n $rendering_proxy_host ]] ; then
+	proxyConf="${RS_ROOT}"/conf/proxy.conf.php
+	cp -rf "${RS_ROOT}"/conf/proxy.conf.php.example "${proxyConf}"
+	sed -i -r "s|define\('HTTP_PROXY_HOST',.*);|define('HTTP_PROXY_HOST', '$rendering_proxy_host');|" "${proxyConf}"
+	sed -i -r "s|define\('HTTP_PROXY_PORT',.*);|define('HTTP_PROXY_PORT', $rendering_proxy_port);|" "${proxyConf}"
+	sed -i -r "s|define\('HTTP_PROXY_USER',.*);|define('HTTP_PROXY_USER', '$rendering_proxy_user');|" "${proxyConf}"
+	sed -i -r "s|define\('HTTP_PROXY_PASS',.*);|define('HTTP_PROXY_PASS', '$rendering_proxy_pass');|" "${proxyConf}"
+fi
+
+dbConf="${RS_ROOT}"/conf/db.conf.php
+sed -i -r "s|\$dsn.*|\$dsn = \"${db_driver}:host=${db_host};port=${db_port};dbname=${db_name}\";|" "${dbConf}"
+sed -i -r "s|\$dbuser.*|\$dbuser = \"$db_user\";|" "${dbConf}"
+sed -i -r "s|\$pwd.*|\pwd = \"$db_password\";|" "${dbConf}"
+
+systemConf="${RS_ROOT}"/conf/system.conf.php
+sed -i -r "s|\$MC_URL.*|\$MC_URL = '$db_user';|" "${systemConf}"
+sed -i -r "s|\$MC_DOCROOT.*|\$MC_DOCROOT = '$db_user';|" "${systemConf}"
+sed -i -r "s|\$CC_RENDER_PATH.*|\$CC_RENDER_PATH = '$db_user';|" "${systemConf}"
+
+xmlstarlet ed -L \
+	-u '/properties/entry[@key="scheme"]' -v "${my_prot_internal}" \
+	-u '/properties/entry[@key="host"]' -v "${my_host_internal}" \
+	-u '/properties/entry[@key="port"]' -v "${my_port_internal}" \
+	-u '/properties/entry[@key="appid"]' -v "${my_home_appid}" \
+	"${RS_ROOT}"/conf/esmain/homeApplication.properties.xml
+
 chown -R www-data:www-data "${RS_ROOT}"
 popd
+
+info >> "install_log-$(date "+%Y.%m.%d-%H.%M.%S").txt"
+info
