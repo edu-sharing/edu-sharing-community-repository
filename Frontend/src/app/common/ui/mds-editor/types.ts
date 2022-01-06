3c517ce2a788739362728d8b8bd4554931085c0b
Torsten Simon <simon@edu-sharing.net>
2022-01-06 13:24:27 +0100

    feat:vcard widget in angular native mds

--- Frontend/src/app/common/ui/mds-editor/types.ts
+++ Frontend/src/app/common/ui/mds-editor/types.ts
@@ -70,6 +70,7 @@ export enum MdsWidgetType {
     Month = 'month',
     Color = 'color',
     Textarea = 'textarea',
+    VCard = 'vcard',
     Checkbox = 'checkbox',
     RadioHorizontal = 'radioHorizontal',
     RadioVertical = 'radioVertical',
