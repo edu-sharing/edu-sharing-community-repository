efb63fd2c8c563b7b75604b2da90730795c87de0
wollner <wollner@edu-sharing.net>
2022-01-20 10:04:26 +0100

    fix: repo won't start without plugins

--- docker/repository/build/service/src/main/build/assets/reinstall.sh
+++ docker/repository/build/service/src/main/build/assets/reinstall.sh
@@ -3,14 +3,14 @@ set -eux
 
 ########################################################################################################################
 
-[[ -d amps/alfresco/0 ]] && {
-	java -jar bin/alfresco-mmt.jar install amps/alfresco/0 tomcat/webapps/alfresco -directory -nobackup -force
-}
+if [[ -d amps/alfresco/0 ]]  ; then
+  java -jar bin/alfresco-mmt.jar install amps/alfresco/0 tomcat/webapps/alfresco -directory -nobackup -force
+fi
 
-[[ -d amps/alfresco/1 ]] && {
+if [[ -d amps/alfresco/1 ]] ; then
 	java -jar bin/alfresco-mmt.jar install amps/alfresco/1 tomcat/webapps/alfresco -directory -nobackup -force
-}
+fi
 
-[[ -d amps/edu-sharing/1 ]] && {
+if [[ -d amps/edu-sharing/1 ]] ; then
 	java -jar bin/alfresco-mmt.jar install amps/edu-sharing/1 tomcat/webapps/edu-sharing -directory -nobackup -force
-}
+fi
