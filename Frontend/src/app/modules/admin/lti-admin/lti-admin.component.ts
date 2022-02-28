ee9f7c464f3e387c399c1304259f3a0fbf8c28fe
Daniel Rudolph <rudolph@edu-sharing.net>
2022-02-28 15:35:04 +0100

    feat lti: advanced registration

--- Frontend/src/app/modules/admin/lti-admin/lti-admin.component.ts
+++ Frontend/src/app/modules/admin/lti-admin/lti-admin.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { RestLtiService } from '../../../core-module/rest/services/rest-lti.service';
 import { LTIRegistrationToken, LTIRegistrationTokens } from '../../../core-module/rest/data-object';
+import { Toast } from '../../../core-ui-module/toast';
+import { AdminComponent } from '../admin.component';
 
 @Component({
   selector: 'es-lti-admin',
@@ -9,10 +11,29 @@ import { LTIRegistrationToken, LTIRegistrationTokens } from '../../../core-modul
   styleUrls: ['./lti-admin.component.scss']
 })
 export class LtiAdminComponent implements OnInit {
+
+    @Input()
+    adminComponent: AdminComponent;
+
+    /**
+     * dynamic
+     */
     tokens: LTIRegistrationTokens;
     displayedColumns: string[] = ['url', 'tsCreated', 'delete'];
 
-  constructor( private ltiService: RestLtiService) { }
+    /**
+     * advanced
+     *
+     */
+    platformId: string;
+    clientId: string;
+    deploymentId: string;
+    authenticationRequestUrl: string;
+    keysetUrl: string;
+    keyId: string;
+    authTokenUrl: string;
+
+  constructor( private ltiService: RestLtiService, private toast: Toast) { }
 
   ngOnInit(): void {
       this.refresh();
@@ -28,5 +49,20 @@ export class LtiAdminComponent implements OnInit {
 
     refresh() {
         this.ltiService.getTokensCall(false).subscribe((t: LTIRegistrationTokens) => {this.tokens = t; });
+    }
+
+    generate() {
+        this.ltiService.getTokensCall(true).subscribe((t: LTIRegistrationTokens) => {this.tokens = t; });
+    }
+
+    saveAdvanced() {
+        this.ltiService.registrationAdvanced(this.platformId, this.clientId, this.deploymentId,
+            this.authenticationRequestUrl, this.keysetUrl, this.keyId, this.authTokenUrl).subscribe( (t: void) => {
+            this.toast.toast('ADMIN.LTI.DATA.CREATED', null);
+            this.toast.closeModalDialog();
+            this.adminComponent.refreshAppList();
+        }, (error: any) => {
+            this.toast.error(error);
+            this.toast.closeModalDialog(); } );
     }
 }
