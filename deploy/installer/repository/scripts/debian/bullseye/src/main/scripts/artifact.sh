3f9000da51b4b60355cb031018a6b3f729dbb84b
Frank Thomschke <thomschke@edu-sharing.net>
2022-03-26 16:10:03 +0100

    fix ci/cd pipeline due to missing repository artifacts

--- deploy/installer/repository/scripts/debian/bullseye/src/main/scripts/artifact.sh
+++ deploy/installer/repository/scripts/debian/bullseye/src/main/scripts/artifact.sh
@@ -3,4 +3,4 @@ set -e
 set -o pipefail
 
 export ARTIFACT_ID="edu_sharing-projects-community-deploy-installer-repository-distribution"
-export ARTIFACT_VERSON="${org.edu_sharing:edu_sharing-projects-community-deploy-installer-services-rendering-distribution:tar.gz:bin.version}"
+export ARTIFACT_VERSION="${org.edu_sharing:edu_sharing-projects-community-deploy-installer-repository-distribution:tar.gz:bin.version}"
