2415bb75cdd5ff2d42a3b75050b3687061572ac9
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-06-16 16:37:46 +0200

    fix: wrong import

--- Frontend/src/app/modules/management-dialogs/node-relation-manager/node-relation-manager.component.ts
+++ Frontend/src/app/modules/management-dialogs/node-relation-manager/node-relation-manager.component.ts
@@ -1,4 +1,4 @@
-import {trigger} from '@angular/animations';
+import { trigger } from '@angular/animations';
 import {
     ChangeDetectionStrategy,
     Component,
@@ -7,7 +7,7 @@ import {
     OnInit,
     Output,
 } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {
     DialogButton,
     ListItem,
@@ -16,16 +16,16 @@ import {
     RestConstants,
     SearchRequestCriteria,
 } from '../../../core-module/core.module';
-import {UIAnimation} from '../../../core-module/ui/ui-animation';
-import {RelationData, RelationService, UserService} from 'ngx-edu-sharing-api';
-import {UIHelper} from '../../../core-ui-module/ui-helper';
-import {NodeHelperService} from '../../../core-ui-module/node-helper.service';
-import {BridgeService} from '../../../core-bridge-module/bridge.service';
-import {OPEN_URL_MODE} from '../../../core-module/ui/ui-constants';
-import {UniversalNode} from '../../../common/definitions';
-import {forkJoin} from 'rxjs';
-import {Toast} from '../../../core-ui-module/toast';
-import {first} from 'rxjs/internal/operators';
+import { UIAnimation } from '../../../core-module/ui/ui-animation';
+import { RelationData, RelationService, UserService } from 'ngx-edu-sharing-api';
+import { UIHelper } from '../../../core-ui-module/ui-helper';
+import { NodeHelperService } from '../../../core-ui-module/node-helper.service';
+import { BridgeService } from '../../../core-bridge-module/bridge.service';
+import { OPEN_URL_MODE } from '../../../core-module/ui/ui-constants';
+import { UniversalNode } from '../../../common/definitions';
+import { forkJoin } from 'rxjs';
+import { Toast } from '../../../core-ui-module/toast';
+import { first } from 'rxjs/operators';
 
 @Component({
     selector: 'es-node-relation-manager',
@@ -37,12 +37,12 @@ import {first} from 'rxjs/internal/operators';
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NodeRelationManagerComponent implements OnInit{
+export class NodeRelationManagerComponent implements OnInit {
     readonly Relations = Object.values(Relations);
     readonly RelationsInverted = {
         [Relations.isBasedOn]: 'isBasisFor',
         [Relations.isPartOf]: 'hasPart',
-        [Relations.references]: 'references'
+        [Relations.references]: 'references',
     };
     source: UniversalNode;
     _nodes: UniversalNode[];
@@ -52,39 +52,33 @@ export class NodeRelationManagerComponent implements OnInit{
     swapRelation: boolean;
     @Input() set nodes(nodes: Node[]) {
         console.log(nodes);
-        if(nodes?.length > 1) {
+        if (nodes?.length > 1) {
             throw new Error('relation manager does currently not support bulk features');
         }
         this._nodes = nodes;
         this.source = nodes[0];
-        this.relationService.getRelations(
-            this._nodes[0].ref.id
-        ).subscribe((relations) => {
-            this.relations = relations.relations;
-            this.loading = false;
-        },e => {
-               this.close.emit(false);
-            });
+        this.relationService.getRelations(this._nodes[0].ref.id).subscribe(
+            (relations) => {
+                this.relations = relations.relations;
+                this.loading = false;
+            },
+            (e) => {
+                this.close.emit(false);
+            },
+        );
     }
     @Output() close = new EventEmitter<boolean>();
 
     readonly form = new FormGroup({
         relation: new FormControl(Relations.isBasedOn, Validators.required),
     });
-    readonly buttons = [new DialogButton('CLOSE',
-        DialogButton.TYPE_CANCEL,
-        () => this.cancel()
-    ),
-        new DialogButton('SAVE',
-            DialogButton.TYPE_PRIMARY,
-            () => this.save(),
-        )
+    readonly buttons = [
+        new DialogButton('CLOSE', DialogButton.TYPE_CANCEL, () => this.cancel()),
+        new DialogButton('SAVE', DialogButton.TYPE_PRIMARY, () => this.save()),
     ];
     permissions = [RestConstants.PERMISSION_WRITE];
     target: UniversalNode;
-    columns = [
-        new ListItem('NODE', RestConstants.LOM_PROP_TITLE)
-    ];
+    columns = [new ListItem('NODE', RestConstants.LOM_PROP_TITLE)];
     loading = true;
 
     constructor(
@@ -93,17 +87,17 @@ export class NodeRelationManagerComponent implements OnInit{
         private userService: UserService,
         private toast: Toast,
         private bridgeService: BridgeService,
-    ) {
-    }
+    ) {}
 
     ngOnInit() {
         this.updateButtons();
     }
 
     getRelationKeys() {
-        return [...new Set(this.addRelations.concat(this.relations || [])?.map(r => r.type))].sort();
+        return [
+            ...new Set(this.addRelations.concat(this.relations || [])?.map((r) => r.type)),
+        ].sort();
     }
-
 
     swap() {
         this.swapRelation = !this.swapRelation;
@@ -113,10 +107,12 @@ export class NodeRelationManagerComponent implements OnInit{
     }
 
     getCriterias(): SearchRequestCriteria[] {
-        return [{
-            property: "sourceNode",
-            values: [this._nodes[0].ref.id]
-        }];
+        return [
+            {
+                property: 'sourceNode',
+                values: [this._nodes[0].ref.id],
+            },
+        ];
     }
     getAllExistingRelations() {
         return this.getAllRelations().filter((r) => !this.deleteRelations.includes(r));
@@ -126,20 +122,20 @@ export class NodeRelationManagerComponent implements OnInit{
         return this.relations.concat(this.addRelations);
     }
 
-    getRelations(key: 'isPartOf' | 'isBasedOn' | 'references' | 'hasPart' | 'isBasisFor'): RelationData[] {
-        return this.getAllRelations().filter(r => r.type === key).sort((a,b) => a.timestamp > b.timestamp ? 1 : -1);
+    getRelations(
+        key: 'isPartOf' | 'isBasedOn' | 'references' | 'hasPart' | 'isBasisFor',
+    ): RelationData[] {
+        return this.getAllRelations()
+            .filter((r) => r.type === key)
+            .sort((a, b) => (a.timestamp > b.timestamp ? 1 : -1));
     }
 
     openNode(node: UniversalNode) {
-        UIHelper.openUrl(
-            this.nodeHelper.getNodeUrl(node),
-            this.bridgeService,
-            OPEN_URL_MODE.Blank
-        );
+        UIHelper.openUrl(this.nodeHelper.getNodeUrl(node), this.bridgeService, OPEN_URL_MODE.Blank);
     }
 
     removeRelation(relation: RelationData) {
-        if(!this.deleteRelations.includes(relation)) {
+        if (!this.deleteRelations.includes(relation)) {
             this.deleteRelations.push(relation);
         }
         this.updateButtons();
@@ -149,7 +145,7 @@ export class NodeRelationManagerComponent implements OnInit{
         let source = this.source.ref.id;
         let target = r.node.ref.id;
         let type: string = r.type;
-        if(inverted) {
+        if (inverted) {
             source = r.node.ref.id;
             target = this.source.ref.id;
             type = inverted;
@@ -157,48 +153,51 @@ export class NodeRelationManagerComponent implements OnInit{
         return {
             source,
             target,
-            type
-        }
+            type,
+        };
     }
     private async save() {
         this.toast.showProgressDialog();
         try {
-            await forkJoin(this.addRelations.map(r => {
-                const data = this.resolveRelationSendData(r);
-                return this.relationService.createRelation(
-                    data.source,
-                    data.target,
-                    data.type as any
-                )
-            })).toPromise();
-            await forkJoin(this.deleteRelations.map(r => {
-                const data = this.resolveRelationSendData(r);
-                return this.relationService.deleteRelation(
-                    data.source,
-                    data.target,
-                    data.type as any
-                )
-            })).toPromise();
+            await forkJoin(
+                this.addRelations.map((r) => {
+                    const data = this.resolveRelationSendData(r);
+                    return this.relationService.createRelation(
+                        data.source,
+                        data.target,
+                        data.type as any,
+                    );
+                }),
+            ).toPromise();
+            await forkJoin(
+                this.deleteRelations.map((r) => {
+                    const data = this.resolveRelationSendData(r);
+                    return this.relationService.deleteRelation(
+                        data.source,
+                        data.target,
+                        data.type as any,
+                    );
+                }),
+            ).toPromise();
             this.close.emit(true);
-        } catch(e) {
-        }
+        } catch (e) {}
         this.toast.closeModalDialog();
     }
 
     private updateButtons() {
         this.buttons[1].disabled = !this.addRelations.length && !this.deleteRelations.length;
     }
-    getCurrentType() {
-
-    }
+    getCurrentType() {}
     async createRelation() {
         let type = this.form.get('relation').value;
-        if(this.swapRelation) {
+        if (this.swapRelation) {
             type = (this.RelationsInverted as any)[type];
         }
-        if(this.getAllExistingRelations().find((r) =>
-            r.node.ref.id === this.target.ref.id && r.type === type
-        )) {
+        if (
+            this.getAllExistingRelations().find(
+                (r) => r.node.ref.id === this.target.ref.id && r.type === type,
+            )
+        ) {
             this.toast.error(null, 'NODE_RELATIONS.RELATION_EXISTS');
             return;
         }
@@ -207,17 +206,19 @@ export class NodeRelationManagerComponent implements OnInit{
             type,
             // @TODO: check if api model is invalid
             timestamp: new Date().getTime() as any,
-            creator: (await this.userService.observeCurrentUser().pipe(first()).toPromise()).person
+            creator: (await this.userService.observeCurrentUser().pipe(first()).toPromise()).person,
         });
         this.form.reset();
-        this.form.setValue({relation: Relations.isBasedOn});
+        this.form.setValue({ relation: Relations.isBasedOn });
         this.swapRelation = false;
         this.updateButtons();
         this.target = null;
     }
 
     private isInverted(r: RelationData) {
-        return Object.keys(this.RelationsInverted).find(k => (this.RelationsInverted as any)[k] === r.type && k !== r.type)
+        return Object.keys(this.RelationsInverted).find(
+            (k) => (this.RelationsInverted as any)[k] === r.type && k !== r.type,
+        );
     }
 
     isPublishedCopy() {
@@ -230,7 +231,11 @@ export class NodeRelationManagerComponent implements OnInit{
     }
 
     canModify(relation: RelationData) {
-        return this.nodeHelper.getNodesRight([relation.node], RestConstants.PERMISSION_WRITE, NodesRightMode.Original);
+        return this.nodeHelper.getNodesRight(
+            [relation.node],
+            RestConstants.PERMISSION_WRITE,
+            NodesRightMode.Original,
+        );
     }
 
     private cancel() {
@@ -261,5 +266,5 @@ export class NodeRelationManagerComponent implements OnInit{
 export enum Relations {
     isBasedOn = 'isBasedOn',
     isPartOf = 'isPartOf',
-    references = 'references'
+    references = 'references',
 }
