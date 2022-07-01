d91c79d06b728ee404b0e427bbd3b68b2cd74b3e
wollner <wollner@edu-sharing.net>
2022-07-01 12:24:10 +0200

    cleanup: removed obsolete classes

--- Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/metadata/TikaAudioMetadataExtracter.java
+++ Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/metadata/TikaAudioMetadataExtracter.java
@@ -7,7 +7,7 @@ import org.edu_sharing.alfresco.apache.tika.parser.mp4.MP4Parser;
 import org.gagravarr.tika.FlacParser;
 import org.gagravarr.tika.VorbisParser;
 
-import org.alfresco.transformer.metadataExtractors.TikaAudioMetadataExtractor;
+//import org.alfresco.transformer.metadataExtractors.TikaAudioMetadataExtractor;
 
 import java.util.ArrayList;
 
@@ -19,28 +19,28 @@ import java.util.ArrayList;
  * Uses same name as alfresco version to make
  * AbstractMappingMetadataExtracter.getDefaultMapping works
  */
-public class TikaAudioMetadataExtracter extends TikaAudioMetadataExtractor
+public class TikaAudioMetadataExtracter //extends TikaAudioMetadataExtractor
 {
 
     // The Audio related parsers we use
-    private static Parser[] parsers = new Parser[] {
-            new VorbisParser(),
-            new FlacParser(),
-            new MP4Parser()
-    };
-
-    public TikaAudioMetadataExtracter()
-    {
-        super();
-    }
-
-    @Override
-    protected Parser getParser()
-    {
-        return new CompositeParser(
-                tikaConfig.getMediaTypeRegistry(), parsers
-        );
-    }
+    //    private static Parser[] parsers = new Parser[] {
+    //            new VorbisParser(),
+    //            new FlacParser(),
+    //            new MP4Parser()
+    //    };
+    //
+    //    public TikaAudioMetadataExtracter()
+    //    {
+    //        super();
+    //    }
+    //
+    //    @Override
+    //    protected Parser getParser()
+    //    {
+    //        return new CompositeParser(
+    //                tikaConfig.getMediaTypeRegistry(), parsers
+    //        );
+    //    }
 
 }
 
