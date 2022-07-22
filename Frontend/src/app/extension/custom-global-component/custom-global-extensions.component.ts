4f906bb32515c04bd9a5843b024f6d74a68e8780
Torsten Simon <simon@edu-sharing.net>
2022-07-22 15:26:59 +0200

    feat:add empty custom component for overrides

---
+++ Frontend/src/app/extension/custom-global-component/custom-global-extensions.component.ts
@@ -0,0 +1,10 @@
+import {AfterViewInit, Component} from "@angular/core";
+
+@Component({
+    selector: 'es-custom-global-extensions',
+    templateUrl: './custom-global-extensions.component.html',
+    styleUrls: ['./custom-global-extensions.component.scss'],
+})
+export class CustomGlobalExtensionsComponent {
+
+}
