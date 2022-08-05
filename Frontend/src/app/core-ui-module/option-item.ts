52fefa04a627cee23379948dcb18fd2f063054ad
Torsten Simon <simon@edu-sharing.net>
2022-08-05 10:01:10 +0200

    fix:migrate sharing to new list layout

--- Frontend/src/app/core-ui-module/option-item.ts
+++ Frontend/src/app/core-ui-module/option-item.ts
@@ -170,6 +170,7 @@ export enum Scope {
     CollectionsCollection = 'CollectionsProposals',
     WorkspaceList = 'WorkspaceList',
     WorkspaceTree = 'WorkspaceTree',
+    Sharing = 'Sharing',
     Oer = 'Oer',
     UserManagement = 'UserManagement',
     Stream = 'Stream',
