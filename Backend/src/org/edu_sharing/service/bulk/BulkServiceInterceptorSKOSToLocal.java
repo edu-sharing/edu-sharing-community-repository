8520c1cdbdff63da557fffcb557263d8faaf75df
Torsten Simon <simon@edu-sharing.net>
2022-10-19 17:15:11 +0200

    fix:bulk service skos remap methods

--- Backend/src/org/edu_sharing/service/bulk/BulkServiceInterceptorSKOSToLocal.java
+++ Backend/src/org/edu_sharing/service/bulk/BulkServiceInterceptorSKOSToLocal.java
@@ -1,13 +1,11 @@
 package org.edu_sharing.service.bulk;
 
-import org.alfresco.service.cmr.repository.NodeRef;
 import org.apache.log4j.Logger;
-import org.codehaus.groovy.reflection.stdclasses.CachedClosureClass;
 import org.edu_sharing.metadataset.v2.MetadataKey;
 import org.edu_sharing.metadataset.v2.MetadataSetV2;
+import org.edu_sharing.metadataset.v2.MetadataWidget;
 import org.edu_sharing.metadataset.v2.tools.MetadataHelper;
 import org.edu_sharing.repository.client.tools.CCConstants;
-import org.edu_sharing.repository.server.tools.ApplicationInfo;
 import org.edu_sharing.repository.server.tools.ApplicationInfoList;
 
 import java.util.*;
@@ -25,28 +23,19 @@ public class BulkServiceInterceptorSKOSToLocal implements BulkServiceInterceptor
      */
     public HashMap<String, Object> preprocessProperties(HashMap<String, Object> properties) {
         String mdsId = (String) properties.getOrDefault(CCConstants.CM_PROP_METADATASET_EDU_METADATASET, CCConstants.metadatasetdefault_id);
-        MetadataSetV2 mds = null;
         try {
-            mds = MetadataHelper.getMetadataset(ApplicationInfoList.getHomeRepository(), mdsId);
-            mds.getWidgets().stream().filter(w -> w.getValues() != null && !w.getValues().isEmpty()).forEach(w -> {
+            MetadataSetV2 mds = MetadataHelper.getMetadataset(ApplicationInfoList.getHomeRepository(), mdsId);
+            mds.getWidgets().stream().filter(
+                    w -> w.getTemplate() == null && w.getCondition() == null && w.getValues() != null && !w.getValues().isEmpty()
+            ).forEach(w -> {
                 Object values = properties.get(CCConstants.getValidGlobalName(w.getId()));
                 if(values != null) {
-                    Map<String, MetadataKey> map = w.getValuesAsMap();
                     if(values instanceof String) {
                         values = Collections.singletonList(values);
                     }
                     List<?> valuesList = ((List<?>) values);
                     if(valuesList.stream().allMatch(v -> v.toString().toLowerCase().startsWith("http"))) {
-                        List<String> result = valuesList.stream().map(v -> (String) v).map(v -> {
-                            if(map.containsKey(v)) {
-                                return v;
-                            }
-                            String[] splitted = v.split("/");
-                            return splitted[splitted.length - 1];
-                        }).filter(map::containsKey)
-                                .map(map::get)
-                                .map(MetadataKey::getKey)
-                                .collect(Collectors.toList());
+                        List<String> result = mapValues(w, valuesList);
                         logger.info("transformed SKOS properties of " + w.getId() + "("  + result.size() +" mapped from total of " + valuesList.size() + ")");
                         if(result.isEmpty()) {
                             properties.remove(CCConstants.getValidGlobalName(w.getId()));
@@ -61,5 +50,19 @@ public class BulkServiceInterceptorSKOSToLocal implements BulkServiceInterceptor
             logger.warn("Could not transform SKOS properties: " + e.getMessage(), e);
         }
         return properties;
+    }
+
+    protected List<String> mapValues(MetadataWidget widget, List<?> valuesList) {
+        Map<String, MetadataKey> map = widget.getValuesAsMap();
+        return valuesList.stream().map(v -> (String) v).map(v -> {
+                    if(map.containsKey(v)) {
+                        return v;
+                    }
+                    String[] splitted = v.split("/");
+                    return splitted[splitted.length - 1];
+                }).filter(map::containsKey)
+                .map(map::get)
+                .map(MetadataKey::getKey)
+                .collect(Collectors.toList());
     }
 }
