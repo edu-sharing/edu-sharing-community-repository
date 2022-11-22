053d73cea197da3c7a7be8ae7b897e433bebd38b
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-11-22 14:28:37 +0100

    feat: focus dialog buttons with arrow keys

--- Frontend/src/app/features/dialogs/card-dialog/card-dialog-container/card-actions/card-actions.component.ts
+++ Frontend/src/app/features/dialogs/card-dialog/card-dialog-container/card-actions/card-actions.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import {
+    Component,
+    HostListener,
+    Input,
+    QueryList,
+    TemplateRef,
+    ViewChildren,
+} from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { DialogButton } from '../../../../../core-module/ui/dialog-button';
 
 @Component({
@@ -14,6 +22,31 @@ export class CardActionsComponent {
         this.buttonsLeft = buttons?.filter((button) => button.position === 'opposite');
     }
 
+    @ViewChildren(MatButton) buttonElements: QueryList<MatButton>;
+
     buttonsRight: DialogButton[];
     buttonsLeft: DialogButton[];
+
+    @HostListener('keydown.ArrowLeft') onArrowLeft() {
+        this.focusButton('left');
+    }
+
+    @HostListener('keydown.ArrowRight') onArrowRight() {
+        this.focusButton('right');
+    }
+
+    /**
+     * Focuses a button by relative position to the currently focused button.
+     */
+    private focusButton(direction: 'left' | 'right'): void {
+        const buttons = this.buttonElements.filter((button) => !button.disabled);
+        const currentlyFocusedIndex = buttons.findIndex(
+            (button) => button._elementRef.nativeElement === document.activeElement,
+        );
+        if (currentlyFocusedIndex >= 0) {
+            const delta = direction === 'left' ? -1 : 1;
+            const toFocusIndex = (currentlyFocusedIndex + delta + buttons.length) % buttons.length;
+            buttons[toFocusIndex].focus('keyboard');
+        }
+    }
 }
