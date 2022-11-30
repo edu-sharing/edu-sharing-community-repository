d0199cb478bc74fdb31c6cc2058bc01a28a4557e
Torsten Simon <simon@edu-sharing.net>
2022-11-30 16:02:54 +0100

    fix:content servlet ignore versions for published materials

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/ContentServlet.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/ContentServlet.java
@@ -123,9 +123,10 @@ public class ContentServlet extends HttpServlet{
 					isCollectionRef = true;
 				}
 
+				boolean isPublishedMaterial = serviceRegistry.getNodeService().getProperty(nodeRef, QName.createQName(CCConstants.CCM_PROP_IO_PUBLISHED_ORIGINAL)) != null;
 				// we only fetch a specific version if it's not a ref
 				// and it's not a remote node
-				if (!isCollectionRef && version != null && !version.trim().equals("") && homeAppInfo.getAppId().equals(repId)) {
+				if (!isCollectionRef && !isPublishedMaterial && version != null && !version.trim().equals("") && homeAppInfo.getAppId().equals(repId)) {
 					VersionHistory versionHistory = serviceRegistry.getVersionService().getVersionHistory(nodeRef);
 					Version versionObj = null;
 					if(versionHistory != null) {
