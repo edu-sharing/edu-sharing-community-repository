52a0bbfc09be601923bed9eb5bd2fd0bd96224ad
Frank Thomschke <thomschke@edu-sharing.net>
2022-11-30 07:14:06 +0100

    fix:soap skeletons not serializable issue (reddis)

--- Backend/services/soap/server/src/main/java/org/edu_sharing/webservices/render/RenderInfoSoapBindingSkeleton.java
+++ Backend/services/soap/server/src/main/java/org/edu_sharing/webservices/render/RenderInfoSoapBindingSkeleton.java
@@ -8,7 +8,7 @@
 package org.edu_sharing.webservices.render;
 
 public class RenderInfoSoapBindingSkeleton implements org.edu_sharing.webservices.render.RenderInfo, org.apache.axis.wsdl.Skeleton {
-    private org.edu_sharing.webservices.render.RenderInfo impl;
+    private transient org.edu_sharing.webservices.render.RenderInfo impl;
     private static java.util.Map _myOperations = new java.util.Hashtable();
     private static java.util.Collection _myOperationsList = new java.util.ArrayList();
 
