879e44e21fdc5927a7756b662eff89384371adf8
Christopher Lübbemeier <luebbemeier@metaventis.com>
2022-12-16 16:16:02 +0100

    test: improve stability
    Retry setting preferences when backend doesn't reflect changes.

--- Frontend/tests/util/saveLogin.ts
+++ Frontend/tests/util/saveLogin.ts
@@ -1,4 +1,4 @@
-import { Browser, request } from '@playwright/test';
+import { APIRequestContext, Browser, Page, request } from '@playwright/test';
 import * as fs from 'fs';
 import { getStorageStatePath, LoginCredentials } from './util';
 
@@ -13,8 +13,27 @@ export async function saveLogin(
     if (await fileExistsAndIsNoOlderThan(storageStatePath, 600)) {
         return;
     }
+    let successfullyCreated = false;
+    while (!successfullyCreated) {
+        const page = await browser.newPage();
+        const context = await createStorageState(page, baseURL, credentials, storageStatePath);
+        // Retry setting preferences until successful. We should include a test for this to always work
+        // as expected, but when it fails here, it will mess up all our tests.
+        successfullyCreated = await setPreferences(credentials.username, context, {
+            language: 'en',
+            accessibility_toastDuration: 1,
+        });
+        await page.close();
+    }
+}
+
+async function createStorageState(
+    page: Page,
+    baseURL: string,
+    credentials: LoginCredentials,
+    storageStatePath: string,
+) {
     // Login
-    const page = await browser.newPage();
     await page.goto(baseURL);
     await page.locator('input[name="username"]').fill(credentials.username);
     await page.locator('input[type="password"]').fill(credentials.password);
@@ -31,13 +50,30 @@ export async function saveLogin(
     const storageState = await page.context().storageState({ path: storageStatePath });
     // (Re)set user preferences
     const context = await request.newContext({ baseURL, storageState });
-    await context.put('./rest/iam/v1/people/-home-/-me-/preferences', {
-        data: {
-            language: 'en',
-            accessibility_toastDuration: 1,
-        },
-    });
-    await page.close();
+    return context;
+}
+
+async function setPreferences(
+    username: string,
+    context: APIRequestContext,
+    preferences: { [key: string]: string | number },
+): Promise<boolean> {
+    await context.put('./rest/iam/v1/people/-home-/-me-/preferences', { data: preferences });
+
+    // Check if backend reflects preferences
+    const response = await context.get('./rest/iam/v1/people/-home-/-me-/preferences');
+    const currentPreferences = (await response.json()).preferences;
+    const successfullySet = currentPreferences === JSON.stringify(preferences);
+    if (!successfullySet) {
+        console.error(
+            `Failed to set preferences for ${username}!`,
+            'Expected:',
+            preferences,
+            'Found:',
+            currentPreferences,
+        );
+    }
+    return successfullySet;
 }
 
 async function fileExistsAndIsNoOlderThan(path: string, seconds: number): Promise<boolean> {
