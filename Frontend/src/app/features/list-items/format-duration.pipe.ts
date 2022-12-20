c92309f758ac3e83a878cec7948d1fffa2e76b0f
Torsten Simon <simon@edu-sharing.net>
2022-12-20 16:08:32 +0100

    fix:format duration use value instead of whole node

--- Frontend/src/app/features/list-items/format-duration.pipe.ts
+++ Frontend/src/app/features/list-items/format-duration.pipe.ts
@@ -1,14 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Node, RestHelper } from '../../core-module/core.module';
+import { RestHelper } from '../../core-module/core.module';
 
 @Pipe({
     name: 'formatDuration',
 })
 export class FormatDurationPipe implements PipeTransform {
-    transform(node: Node): string {
-        // TODO: Take the actual field instead of `Node`.
-        //
-        // Is `cclom:duration` always given in seconds or do we have to handle other formats?
-        return RestHelper.getDurationFormatted(node);
+    transform(value: string): string {
+        return RestHelper.getDurationFormatted(value);
     }
 }
