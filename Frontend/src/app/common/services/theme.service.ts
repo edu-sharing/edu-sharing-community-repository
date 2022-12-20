22716c05df531733064caafa73d8062b11952a49
Torsten Simon <simon@edu-sharing.net>
2022-12-20 12:04:46 +0100

    fix:dynamic theme all vars

--- Frontend/src/app/common/services/theme.service.ts
+++ Frontend/src/app/common/services/theme.service.ts
@@ -1,11 +1,14 @@
-import { Injectable, NgZone } from '@angular/core';
-import { MaterialCssVarsService } from 'angular-material-css-vars';
-import { ColorHelper } from '../../core-module/ui/color-helper';
+import { Injectable } from '@angular/core';
+import { MaterialCssVariables, MaterialCssVarsService } from 'angular-material-css-vars';
 import { ConfigService } from 'ngx-edu-sharing-api';
+import { UIHelper } from '../../core-ui-module/ui-helper';
+import { ColorHelper } from '../../core-module/ui/color-helper';
+import { HueValue } from 'angular-material-css-vars/lib/model';
 
 export enum Variable {
     Primary = 'primary',
     Accent = 'accent',
+    Warn = 'warn',
 }
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
@@ -13,6 +16,11 @@ export class ThemeService {
         private materialCssVarsService: MaterialCssVarsService,
         private configService: ConfigService,
     ) {
+        // set defaults
+        this.setColor(Variable.Primary, '#48708e');
+        this.setColor(Variable.Accent, '#48708e');
+        this.setColor(Variable.Warn, '#cd2457');
+
         this.configService.observeConfig().subscribe(
             (config) => {
                 const colors = config.themeColors?.color;
@@ -21,8 +29,6 @@ export class ThemeService {
                     colors.forEach((c) => this.setColor(c.variable, c.value));
                 } else {
                     console.info('no branding colors in config, using defaults');
-                    this.setColor(Variable.Primary, '#48708e');
-                    this.setColor(Variable.Accent, '#48708e');
                 }
             },
             (error) => {
@@ -38,10 +44,23 @@ export class ThemeService {
         switch (variable) {
             case Variable.Primary:
                 this.materialCssVarsService.setPrimaryColor(color);
+                this.materialCssVarsService.setVariable(
+                    MaterialCssVariables.ForegroundDivider,
+                    this.fromPalette(color, '500'),
+                );
                 break;
             case Variable.Accent:
                 this.materialCssVarsService.setAccentColor(color);
                 break;
+            case Variable.Warn:
+                this.materialCssVarsService.setWarnColor(color);
+                break;
         }
+    }
+
+    private fromPalette(color: string, hueValue: HueValue) {
+        const palette = this.materialCssVarsService.getPaletteWithContrastForColor(color);
+        const result = palette.filter((p) => p.hue === hueValue)[0].color;
+        return `${result.r}, ${result.g}, ${result.b}`;
     }
 }
