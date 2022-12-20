58a4d7a1944f1e540ccf00acec808ce652a39273
Torsten Simon <simon@edu-sharing.net>
2022-12-20 16:23:48 +0100

    fix:hms/colon formatting for duration

--- Frontend/src/app/features/list-items/format-duration.pipe.ts
+++ Frontend/src/app/features/list-items/format-duration.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { RestHelper } from '../../core-module/core.module';
+import { DurationFormat, RestHelper } from '../../core-module/core.module';
 
 @Pipe({
     name: 'formatDuration',
 })
 export class FormatDurationPipe implements PipeTransform {
-    transform(value: string): string {
-        return RestHelper.getDurationFormatted(value);
+    transform(
+        value: string,
+        args = {
+            format: DurationFormat.Hms,
+        },
+    ): string {
+        return RestHelper.getDurationFormatted(value, args.format);
     }
 }
