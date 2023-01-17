a9ccbe5f7d3e1dee98768f34d7cc8b9bfe57d16f
Torsten Simon <simon@edu-sharing.net>
2023-01-17 18:02:57 +0100

    fix:resolve access original also for remote nodes

--- Backend/src/org/edu_sharing/restservices/NodeDao.java
+++ Backend/src/org/edu_sharing/restservices/NodeDao.java
@@ -1002,14 +1002,14 @@ public class NodeDao {
 	private void fillNodeReference(CollectionReference reference) throws DAOException {
 		final String originalId = getReferenceOriginalId();
 		reference.setOriginalId(originalId);
-		// not supported and used by remote repositories
-		if(isFromRemoteRepository()){
-			return;
-		}
 		try {
 			reference.setAccessOriginal(NodeDao.getNode(repoDao, originalId).asNode(false).getAccess());
 		} catch (Throwable t) {
 			// user may has no access to the original or it is deleted, this is okay
+		}
+		// not supported and used by remote repositories
+		if(isFromRemoteRepository()){
+			return;
 		}
 		AuthenticationUtil.runAsSystem(new RunAsWork<Void>() {
 
