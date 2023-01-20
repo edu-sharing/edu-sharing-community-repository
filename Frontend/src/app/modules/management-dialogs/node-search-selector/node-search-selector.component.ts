76b8fd9cd998d76d58a53be46ecebbf89bb8bd9b
Torsten Simon <simon@edu-sharing.net>
2023-01-20 11:33:23 +0100

    fix:node search selector show progress spinner, reduce default size

--- Frontend/src/app/modules/management-dialogs/node-search-selector/node-search-selector.component.ts
+++ Frontend/src/app/modules/management-dialogs/node-search-selector/node-search-selector.component.ts
@@ -1,5 +1,6 @@
 import {
     AfterViewInit,
+    ChangeDetectorRef,
     Component,
     ContentChild,
     EventEmitter,
@@ -8,10 +9,10 @@ import {
     TemplateRef,
     ViewChild,
 } from '@angular/core';
-import { combineLatest, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
-import { debounceTime, map, switchMap } from 'rxjs/operators';
+import { debounceTime, map, switchMap, tap } from 'rxjs/operators';
 import { RestSearchService } from '../../../core-module/rest/services/rest-search.service';
 import { Node, NodesRightMode, SearchRequestCriteria } from '../../../core-module/rest/data-object';
 import { RestConstants } from '../../../core-module/rest/rest-constants';
@@ -24,6 +25,10 @@ import { trigger } from '@angular/animations';
 import { UIAnimation } from '../../../core-module/ui/ui-animation';
 import { MdsEditorWrapperComponent } from '../../../features/mds/mds-editor/mds-editor-wrapper/mds-editor-wrapper.component';
 
+type Status = {
+    loading: boolean;
+    result?: Node[];
+};
 @Component({
     selector: 'es-node-search-selector',
     templateUrl: 'node-search-selector.component.html',
@@ -60,9 +65,11 @@ export class NodeSearchSelectorComponent implements AfterViewInit {
     /**
      * count of items to search
      */
-    @Input() itemCount = 100;
+    @Input() itemCount = 25;
     @Output() onSelect = new EventEmitter<Node>();
-    searchResult$: Observable<Node[]>;
+    searchStatus: Status = {
+        loading: false,
+    };
     input = new FormControl('');
     columns: ListItem[];
     showMds = false;
@@ -74,6 +81,7 @@ export class NodeSearchSelectorComponent implements AfterViewInit {
         private mdsService: RestMdsService,
         private translate: TranslateService,
         private nodeHelper: NodeHelperService,
+        private changeDetectorRef: ChangeDetectorRef,
     ) {}
 
     setOption(event: MatAutocompleteSelectedEvent) {
@@ -129,13 +137,19 @@ export class NodeSearchSelectorComponent implements AfterViewInit {
         this.mdsService.getSet().subscribe((set) => {
             this.columns = MdsHelper.getColumns(this.translate, set, this.columnsIds);
         });
-        this.searchResult$ = combineLatest([
-            this.input.valueChanges,
-            this.mdsEditor.mdsEditorInstance.values,
-        ]).pipe(
-            debounceTime(200),
-            switchMap(() => this.searchNodes()),
-        );
+        combineLatest([this.input.valueChanges, this.mdsEditor.mdsEditorInstance.values])
+            .pipe(debounceTime(200))
+            .subscribe(() => {
+                this.searchStatus.loading = true;
+                this.changeDetectorRef.detectChanges();
+                this.searchNodes().subscribe((result) => {
+                    this.searchStatus = {
+                        loading: false,
+                        result,
+                    };
+                    this.changeDetectorRef.detectChanges();
+                });
+            });
         this.mdsEditor.loadMds();
         this.mdsEditor.mdsEditorInstance.values.subscribe((v) => (this.values = v));
     }
