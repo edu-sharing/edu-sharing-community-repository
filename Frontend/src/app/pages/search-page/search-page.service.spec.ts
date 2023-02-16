21368e7da16dd24dd533c6734a158cb36c0027c9
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-02-16 11:00:45 +0100

    test: fix unit tests

--- Frontend/src/app/pages/search-page/search-page.service.spec.ts
+++ Frontend/src/app/pages/search-page/search-page.service.spec.ts
@@ -1,9 +1,11 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateService } from '@ngx-translate/core';
 import { EduSharingApiModule } from 'ngx-edu-sharing-api';
-
 import { SearchPageService } from './search-page.service';
+
+const translateServiceStub = {} as TranslateService;
 
 describe('SearchPageService', () => {
     let service: SearchPageService;
@@ -11,7 +13,13 @@ describe('SearchPageService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule, EduSharingApiModule.forRoot(), RouterTestingModule],
-            providers: [SearchPageService],
+            providers: [
+                SearchPageService,
+                {
+                    provide: TranslateService,
+                    useValue: translateServiceStub,
+                },
+            ],
         });
         service = TestBed.inject(SearchPageService);
     });
