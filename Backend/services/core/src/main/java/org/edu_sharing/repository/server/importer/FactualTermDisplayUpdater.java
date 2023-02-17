53e7b28876cd6564a3a2a7bad066b4d370f309d8
Daniel Rudolph <rudolph@edu-sharing.net>
2023-02-17 09:37:33 +0100

    fix: allow FactualTermDisplayUpdater for version2store

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermDisplayUpdater.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermDisplayUpdater.java
@@ -3,6 +3,7 @@ package org.edu_sharing.repository.server.importer;
 import com.sun.star.lang.IllegalArgumentException;
 import org.alfresco.repo.policy.BehaviourFilter;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.version.Version2Model;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -20,6 +21,7 @@ import org.edu_sharing.repository.server.tools.cache.RepositoryCache;
 import org.edu_sharing.service.search.Suggestion;
 import org.springframework.context.ApplicationContext;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,6 +47,7 @@ public class FactualTermDisplayUpdater {
         AuthenticationUtil.runAsSystem(()->{
             runforStore(key, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
             runforStore(key, StoreRef.STORE_REF_ARCHIVE_SPACESSTORE);
+            runforStore(key, new StoreRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE.getProtocol(),Version2Model.STORE_ID));
             return null;
         });
     }
@@ -67,13 +70,21 @@ public class FactualTermDisplayUpdater {
                 try {
 
                     policyBehaviourFilter.disableBehaviour(nodeRef);
-                    nodeService.setProperty(nodeRef,QName.createQName(CCConstants.CCM_PROP_IO_REPL_CLASSIFICATION_KEYWORD_DISPLAY),displays);
+                    setProperty(nodeRef,QName.createQName(CCConstants.CCM_PROP_IO_REPL_CLASSIFICATION_KEYWORD_DISPLAY),displays);
                     new RepositoryCache().remove(nodeRef.getId());
                 }finally {
                     policyBehaviourFilter.enableBehaviour(nodeRef);
                 }
                 return null;
             });
+        }
+    }
+
+    private void setProperty(NodeRef nodeRef, QName qName, Serializable serializable){
+        nodeService.setProperty(nodeRef, qName, serializable);
+        if(nodeRef.getStoreRef().getIdentifier().equals(Version2Model.STORE_ID)){
+            QName qnameV = QName.createQName(Version2Model.NAMESPACE_URI, Version2Model.PROP_METADATA_PREFIX + qName.toString());
+            nodeService.setProperty(nodeRef, qnameV, serializable);
         }
     }
 
