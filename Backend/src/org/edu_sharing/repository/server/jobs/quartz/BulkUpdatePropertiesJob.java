63c73b401b4fbd3e2e4e3e8d316eb85c0070c285
Torsten Simon <simon@edu-sharing.net>
2023-02-23 14:53:45 +0100

    fix:bulk update job support modified date check

--- Backend/src/org/edu_sharing/repository/server/jobs/quartz/BulkUpdatePropertiesJob.java
+++ Backend/src/org/edu_sharing/repository/server/jobs/quartz/BulkUpdatePropertiesJob.java
@@ -27,6 +27,7 @@
  */
 package org.edu_sharing.repository.server.jobs.quartz;
 
+import org.alfresco.model.ContentModel;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
@@ -45,6 +46,8 @@ import org.springframework.context.ApplicationContext;
 import java.io.BufferedReader;
 import java.io.Serializable;
 import java.io.StringReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -52,11 +55,14 @@ import java.util.stream.Collectors;
 public class BulkUpdatePropertiesJob extends AbstractJob{
 	protected Logger logger = Logger.getLogger(BulkUpdatePropertiesJob.class);
 	private org.alfresco.service.cmr.repository.NodeService nodeService;
+	@JobFieldDescription(sampleValue = "YYYY-MM-DD", description = "Only update nodes when they're not modified since the given date. If a node was modified, it won't be updated with a new value")
+	private String notModifiedSince;
 	@JobFieldDescription(file = true, description = "CSV file to use for update data (Requires \"node_id\", \"property\" and \"value\" as rows")
 	private String data;
 	@JobFieldDescription(description = "test mode only", sampleValue = "true")
 	private Boolean test;
 	private CSVTool.CSVResult csv;
+	private Date notModifiedSinceDate;
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -70,6 +76,16 @@ public class BulkUpdatePropertiesJob extends AbstractJob{
 		data = (String) context.getJobDetail().getJobDataMap().get(JobHandler.FILE_DATA);
 		test = context.getJobDetail().getJobDataMap().getBooleanFromString("test");
 		csv = readCSV(data);
+		notModifiedSince = (String) context.getJobDetail().getJobDataMap().get("notModifiedSince");
+		if(notModifiedSince != null) {
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+			try {
+				notModifiedSinceDate = formatter.parse(notModifiedSince);
+			} catch (ParseException e) {
+				throw new RuntimeException(e);
+			}
+			logger.info("notModifiedSince was set, will not update nodes which were modified after " + notModifiedSinceDate);
+		}
 		NodeRunner runner = new NodeRunner();
 		Set<NodeRef> nodeList = csv.getLines().stream().map(
 				l -> new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, l.get("node_id"))
@@ -87,6 +103,13 @@ public class BulkUpdatePropertiesJob extends AbstractJob{
 						if(!nodeService.exists(nodeRef)) {
 							logger.warn("Given node with id " + nodeRef.getId() + " doesn't exist, skipping");
 							return;
+						}
+						if(notModifiedSinceDate != null) {
+							Date modified = (Date) nodeService.getProperty(nodeRef, ContentModel.PROP_MODIFIED);
+							if(modified.after(notModifiedSinceDate)) {
+								logger.warn("Node " + nodeRef + " has been modified after given notModifiedSince value, will skip it " + modified);
+								return;
+							}
 						}
 						logger.info("Set property " + property + "=" + value + ", node: " + ref.getId());
 						if(!test) {
