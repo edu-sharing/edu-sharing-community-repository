9a09853603fd62f0c4531980507def10917fc288
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-03-01 12:04:45 +0100

    fix: node-image pipe uses uninitialized repo list

--- Frontend/projects/edu-sharing-api/src/lib/wrappers/network.service.ts
+++ Frontend/projects/edu-sharing-api/src/lib/wrappers/network.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import * as rxjs from 'rxjs';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { NetworkV1Service } from '../api/services';
+import { HOME_REPOSITORY } from '../constants';
+import { Node } from '../models';
 import { shareReplayReturnValue } from '../utils/decorators';
 
 interface Repository {
@@ -29,5 +32,43 @@ export class NetworkService {
         return this.networkV1
             .getRepositories()
             .pipe(map((repos) => (repos as unknown as NetworkRepositories).repositories));
+    }
+
+    getRepository(id: string): Observable<Repository | null> {
+        return this.getRepositories().pipe(
+            map((repositories) => {
+                if (id === HOME_REPOSITORY) {
+                    return repositories.find((r) => r.isHomeRepo) ?? null;
+                } else {
+                    return repositories.find((r) => r.id === id) ?? null;
+                }
+            }),
+        );
+    }
+
+    getHomeRepository(): Observable<Repository> {
+        return this.getRepositories().pipe(
+            map((repositories) => repositories.find((r) => r.isHomeRepo)!),
+        );
+    }
+
+    isHomeRepository(id: string): Observable<boolean> {
+        return this.getRepository(id).pipe(map((repository) => repository?.isHomeRepo ?? false));
+    }
+
+    isFromHomeRepository(node: Node): Observable<boolean> {
+        if (node.ref.isHomeRepo) {
+            return rxjs.of(true);
+        } else {
+            return this.isHomeRepository(node.ref.repo);
+        }
+    }
+
+    getRepositoryOfNode(node: Node): Observable<Repository | null> {
+        if (node.ref.isHomeRepo) {
+            return this.getHomeRepository();
+        } else {
+            return this.getRepository(node.ref.repo);
+        }
     }
 }
