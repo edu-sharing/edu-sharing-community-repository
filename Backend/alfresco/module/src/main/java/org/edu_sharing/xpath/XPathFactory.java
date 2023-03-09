4ad021cd005d78d439f07d56b1141ddf18e8f5c1
Daniel Rudolph <rudolph@edu-sharing.net>
2023-03-09 16:27:29 +0100

    security: enable XMLConstants.FEATURE_SECURE_PROCESSING for XPathFactory

---
+++ Backend/alfresco/module/src/main/java/org/edu_sharing/xpath/XPathFactory.java
@@ -0,0 +1,18 @@
+package org.edu_sharing.xpath;
+
+
+import javax.xml.XMLConstants;
+import javax.xml.xpath.XPathFactoryConfigurationException;
+
+public class XPathFactory extends org.apache.xpath.jaxp.XPathFactoryImpl {
+
+    public XPathFactory(){
+
+        try {
+            setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        } catch (XPathFactoryConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
