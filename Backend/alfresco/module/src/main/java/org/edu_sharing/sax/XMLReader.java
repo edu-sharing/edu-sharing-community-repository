27a207f275b3098add02dcd6139aeed2379c1715
Daniel Rudolph <rudolph@edu-sharing.net>
2023-03-13 13:13:06 +0100

    security: enable owasp security config for SaxParser (sax XMLReader)
    https://cheatsheetseries.owasp.org/cheatsheets/XML_External_Entity_Prevention_Cheat_Sheet.html#xmlreader

---
+++ Backend/alfresco/module/src/main/java/org/edu_sharing/sax/XMLReader.java
@@ -0,0 +1,21 @@
+package org.edu_sharing.sax;
+
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
+
+public class XMLReader extends org.apache.xerces.parsers.SAXParser{
+    public XMLReader(){
+        super();
+        try {
+            super.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            // This may not be strictly required as DTDs shouldn't be allowed at all, per previous line.
+            super.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            super.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            super.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        } catch (SAXNotRecognizedException e) {
+            throw new RuntimeException(e);
+        } catch (SAXNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
