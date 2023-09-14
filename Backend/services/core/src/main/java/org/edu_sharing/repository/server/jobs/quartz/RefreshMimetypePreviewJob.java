bca4c82bc140449d3d16dce37cc7f752802b9369
Daniel Rudolph <rudolph@edu-sharing.net>
2023-09-14 08:00:05 +0200

    fix: RefreshMimetypePreviewJob: alfresco thumbnail service does not trigger for existing generated thumbnail. add option to remove existing generated thumbnail. fix: elastic tracker did not recognize thumbnail change cause thumbnail nodes are ignored. so we trigger an update on the io node to force elastic tracker.

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/RefreshMimetypePreviewJob.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/RefreshMimetypePreviewJob.java
@@ -28,21 +28,25 @@
 package org.edu_sharing.repository.server.jobs.quartz;
 
 import org.alfresco.model.ContentModel;
+import org.alfresco.model.RenditionModel;
 import org.alfresco.repo.policy.BehaviourFilter;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ActionService;
+import org.alfresco.service.cmr.action.ActionStatus;
 import org.alfresco.service.cmr.lock.LockService;
 import org.alfresco.service.cmr.lock.LockType;
 import org.alfresco.service.cmr.repository.*;
 import org.alfresco.service.cmr.search.ResultSet;
 import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
 import org.edu_sharing.repository.client.tools.CCConstants;
+import org.edu_sharing.repository.server.MCAlfrescoAPIClient;
 import org.edu_sharing.repository.server.jobs.quartz.annotation.JobDescription;
 import org.edu_sharing.repository.server.jobs.quartz.annotation.JobFieldDescription;
 import org.edu_sharing.repository.server.tools.ActionObserver;
@@ -51,6 +55,7 @@ import org.quartz.JobExecutionException;
 import org.springframework.context.ApplicationContext;
 
 import javax.transaction.UserTransaction;
+import java.util.List;
 
 @JobDescription(description = "refresh the mimetype and preview for a given solr search filter")
 public class RefreshMimetypePreviewJob extends AbstractJob{
@@ -60,6 +65,9 @@ public class RefreshMimetypePreviewJob extends AbstractJob{
 
 	@JobFieldDescription(description = "if true only thumbail generation will be don's. mimetype fix will be skipped. default is false.")
 	Boolean skipMimeTypeFix;
+
+	@JobFieldDescription(description = "thumbnailservice checks if thumbnail already exists and skips process when exists. when cleanUpExistingThumbnail is true the thumbnail node with name imagepreview will be deleted. default is false.")
+	Boolean cleanUpExistingThumbnail;
 
 	ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
 	ServiceRegistry serviceRegistry = (ServiceRegistry) applicationContext.getBean(ServiceRegistry.SERVICE_REGISTRY);
@@ -95,6 +103,10 @@ public class RefreshMimetypePreviewJob extends AbstractJob{
 			skipMimeTypeFix = context.getJobDetail().getJobDataMap().getBoolean("skipMimeTypeFix");
 			if(skipMimeTypeFix == null) skipMimeTypeFix = false;
 			logger.info("using skipMimeTypeFix:" + skipMimeTypeFix);
+
+			cleanUpExistingThumbnail = context.getJobDetail().getJobDataMap().getBoolean("cleanUpExistingThumbnail");
+			if(cleanUpExistingThumbnail == null) cleanUpExistingThumbnail = false;
+			logger.info("using cleanUpExistingThumbnail:" + cleanUpExistingThumbnail);
 
 
 			SearchParameters sp = new SearchParameters();
@@ -182,6 +194,23 @@ public class RefreshMimetypePreviewJob extends AbstractJob{
 	}
 
 	private void triggerThumbnailAction(NodeRef nodeRef) {
+
+		if(this.cleanUpExistingThumbnail){
+			List<ChildAssociationRef> imgpreview = this.nodeService
+					.getChildAssocs(nodeRef, RenditionModel.ASSOC_RENDITION, QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "imgpreview"));
+			if(imgpreview != null){
+				if(imgpreview.size() == 0){
+					logger.error("no preview found to delete.");
+				}else if(imgpreview.size() > 1){
+					logger.error("number of previews > 1. don't know which one to delete");
+				}else {
+					NodeRef thumbnail = imgpreview.get(0).getChildRef();
+					logger.info("remove generated thumbnail: "+ thumbnail +" from io:" + nodeRef);
+					new MCAlfrescoAPIClient().removeNode(thumbnail.getId(),null,false);
+				}
+			}
+		}
+
 		ActionService actionService = serviceRegistry.getActionService();
 		Action thumbnailAction = actionService.createAction(CCConstants.ACTION_NAME_CREATE_THUMBNAIL);
 		thumbnailAction.setTrackStatus(true);
@@ -191,5 +220,13 @@ public class RefreshMimetypePreviewJob extends AbstractJob{
 
 		//cause its already async set executeAsynchronously to false
 		actionService.executeAction(thumbnailAction, nodeRef, true, false);
+
+		if(ActionStatus.Completed.equals(thumbnailAction.getExecutionStatus())){
+			logger.info("action was successfull. trigger io update for elastic trackr");
+			String name = (String)nodeService.getProperty(nodeRef,ContentModel.PROP_NAME);
+			nodeService.setProperty(nodeRef,ContentModel.PROP_NAME,name);
+		}else{
+			logger.error("action status." + thumbnailAction.getExecutionStatus() +" will not trigger update on io. tracker will not retrack preview");
+		}
 	}
 }
