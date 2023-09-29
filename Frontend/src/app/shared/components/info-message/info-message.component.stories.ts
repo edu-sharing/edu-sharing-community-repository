a7d46e4b8e455dd741ef7cada43c75328b51e6a7
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-09-29 15:35:20 +0200

    test: add info-message stories

---
+++ Frontend/src/app/shared/components/info-message/info-message.component.stories.ts
@@ -0,0 +1,64 @@
+import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
+import { InfoMessageComponent } from './info-message.component';
+import { SharedModule } from '../../shared.module';
+import { HttpClient, HttpHandler, HttpXhrBackend } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { EduSharingApiModule } from 'ngx-edu-sharing-api';
+
+// More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
+const meta: Meta<InfoMessageComponent> = {
+    title: 'Shared/Info Message',
+    component: InfoMessageComponent,
+    decorators: [
+        moduleMetadata({
+            declarations: [], // Prevent duplicate declaration of InfoMessageComponent
+            imports: [SharedModule],
+        }),
+        applicationConfig({
+            providers: [
+                HttpClient,
+                {
+                    provide: HttpHandler,
+                    useValue: new HttpXhrBackend({ build: () => new XMLHttpRequest() }),
+                },
+                importProvidersFrom(
+                    EduSharingApiModule.forRoot({
+                        rootUrl: '/api',
+                    }),
+                ),
+            ],
+        }),
+    ],
+    tags: ['autodocs'],
+    args: {
+        message: 'Your message here!',
+    },
+    argTypes: {
+        mode: {
+            control: 'radio',
+            options: ['info', 'warning', 'error'],
+        },
+    },
+};
+
+export default meta;
+type Story = StoryObj<InfoMessageComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
+export const Info: Story = {
+    args: {
+        mode: 'info',
+    },
+};
+
+export const Warning: Story = {
+    args: {
+        mode: 'warning',
+    },
+};
+
+export const Error: Story = {
+    args: {
+        mode: 'error',
+    },
+};
