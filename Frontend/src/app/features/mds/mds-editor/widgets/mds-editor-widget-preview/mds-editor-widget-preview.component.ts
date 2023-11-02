f25fd68884ee30c565d374dc03551c073b306c6e
Christopher Lübbemeier <luebbemeier@metaventis.com>
2023-11-02 16:07:36 +0100

    fix: broken image src in mds preview widget

--- Frontend/src/app/features/mds/mds-editor/widgets/mds-editor-widget-preview/mds-editor-widget-preview.component.ts
+++ Frontend/src/app/features/mds/mds-editor/widgets/mds-editor-widget-preview/mds-editor-widget-preview.component.ts
@@ -1,19 +1,22 @@
-import { forkJoin as observableForkJoin, BehaviorSubject, Observable, forkJoin } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Component, OnInit } from '@angular/core';
-import { MdsEditorInstanceService } from '../../mds-editor-instance.service';
+import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { RestNodeService } from '../../../../../core-module/rest/services/rest-node.service';
-import { Node } from '../../../../../core-module/rest/data-object';
 import { Metadata } from 'ngx-edu-sharing-graphql';
+import * as rxjs from 'rxjs';
+import { BehaviorSubject, forkJoin, forkJoin as observableForkJoin } from 'rxjs';
+import { map, take, takeWhile } from 'rxjs/operators';
+import { Node } from '../../../../../core-module/rest/data-object';
+import { RestNodeService } from '../../../../../core-module/rest/services/rest-node.service';
+import { MdsEditorInstanceService } from '../../mds-editor-instance.service';
 
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { RepoUrlService } from 'ngx-edu-sharing-ui';
 import { NativeWidgetComponent } from '../../../types/types';
 @Component({
     selector: 'es-mds-editor-widget-preview',
     templateUrl: './mds-editor-widget-preview.component.html',
     styleUrls: ['./mds-editor-widget-preview.component.scss'],
 })
-export class MdsEditorWidgetPreviewComponent implements OnInit, NativeWidgetComponent {
+export class MdsEditorWidgetPreviewComponent implements NativeWidgetComponent {
     static readonly constraints = {
         requiresNode: true,
         supportsBulk: false,
@@ -24,6 +27,7 @@ export class MdsEditorWidgetPreviewComponent implements OnInit, NativeWidgetComp
         'info.preview.mimetype',
         'info.preview.type',
     ];
+
     hasChanges = new BehaviorSubject<boolean>(false);
     src: SafeResourceUrl | string;
     nodeSrc: string;
@@ -31,72 +35,64 @@ export class MdsEditorWidgetPreviewComponent implements OnInit, NativeWidgetComp
     node: Node;
     metadata: Metadata;
     delete = false;
+
     constructor(
         private mdsEditorValues: MdsEditorInstanceService,
         private nodeService: RestNodeService,
+        private repoUrlService: RepoUrlService,
         private sanitizer: DomSanitizer,
-    ) {}
-
-    ngOnInit(): void {
-        forkJoin([this.mdsEditorValues.nodes$, this.mdsEditorValues.graphqlMetadata$]).subscribe(
-            (data) => {
-                if (data[0]?.length === 1) {
-                    this.node = data[0][0];
+    ) {
+        forkJoin([
+            this.mdsEditorValues.nodes$.pipe(take(1)),
+            this.mdsEditorValues.graphqlMetadata$.pipe(take(1)),
+        ])
+            .pipe(takeUntilDestroyed())
+            .subscribe(([nodes, graphqlMetadata]) => {
+                if (nodes?.length === 1) {
+                    this.node = nodes[0];
                     this.nodeSrc =
                         this.node.preview.url + '&crop=true&width=400&height=300&dontcache=:cache';
-                } else if (data[1]?.length === 1) {
-                    this.metadata = data[1][0];
+                } else if (graphqlMetadata?.length === 1) {
+                    this.metadata = graphqlMetadata[0];
                     this.nodeSrc =
                         this.metadata.info.preview.url +
                         '&crop=true&width=400&height=300&dontcache=:cache';
                 }
-                if (data[0]?.length === 1 || data[1]?.length === 1) {
+                if (nodes?.length === 1 || graphqlMetadata?.length === 1) {
                     this.updateSrc();
                     // we need to reload the image since we don't know if the image (e.g. video file) is still being processed
-                    // FIXME: this will run forever!
-                    setInterval(() => {
-                        if (this.file) {
-                            return;
-                        }
-                        this.updateSrc();
-                    }, 5000);
+                    rxjs.interval(5000)
+                        .pipe(
+                            takeUntilDestroyed(),
+                            takeWhile(() => !this.file),
+                        )
+                        .subscribe(() => this.updateSrc());
                 }
-            },
-        );
-        this.mdsEditorValues.graphqlMetadata$.subscribe((metadata) => {
-            if (metadata?.length === 1) {
-                console.log(metadata);
-                this.nodeSrc =
-                    metadata[0].info.preview.url +
-                    '&crop=true&width=400&height=300&dontcache=:cache';
-                this.metadata = metadata[0];
-                this.updateSrc();
-                // we need to reload the image since we don't know if the image (e.g. video file) is still being processed
-                // FIXME: this will run forever!
-                setInterval(() => {
-                    if (this.file) {
-                        return;
-                    }
-                    this.updateSrc();
-                }, 5000);
-            }
-        });
+            });
     }
+
     setPreview(event: Event): void {
         this.file = (event.target as HTMLInputElement).files[0];
         this.delete = false;
         this.updateSrc();
     }
-    updateSrc() {
+
+    async updateSrc() {
         if (this.file) {
             this.src = this.sanitizer.bypassSecurityTrustResourceUrl(
                 window.URL.createObjectURL(this.file),
             );
         } else {
-            this.src = this.nodeSrc.replace(':cache', new Date().getTime().toString());
+            const src = this.nodeSrc.replace(':cache', new Date().getTime().toString());
+            if (this.node) {
+                this.src = await this.repoUrlService.getRepoUrl(src, this.node);
+            } else {
+                this.src = src;
+            }
         }
         this.hasChanges.next(this.file != null || this.delete);
     }
+
     onSaveNode(nodes: Node[]) {
         if (this.delete) {
             return observableForkJoin(
