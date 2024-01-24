4e1006dce8b7d89ca060a0ccdd1d62d0132c7ff8
Daniel Rudolph <rudolph@edu-sharing.net>
2024-01-24 10:54:55 +0100

    feature: alfresco 23.2 / openIdConnect logout config

--- Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SecurityConfigurationOpenIdConnect.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SecurityConfigurationOpenIdConnect.java
@@ -2,7 +2,6 @@ package org.edu_sharing.spring.security.openid;
 
 import com.typesafe.config.Config;
 import org.edu_sharing.alfresco.lightbend.LightbendConfigLoader;
-import org.edu_sharing.spring.security.saml2.SecurityConfigurationSaml;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,11 +9,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ClientRegistrations;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Profile("openidEnabled")
 @EnableWebSecurity(debug = true)
@@ -32,11 +33,24 @@ public class SecurityConfigurationOpenIdConnect {
                         .requestMatchers("/**").permitAll()
                 )
 
-                .csrf(AbstractHttpConfigurer::disable).oauth2Login(Customizer.withDefaults());
+                .oauth2Login(Customizer.withDefaults())
+                .logout((logout) -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
+                .csrf(AbstractHttpConfigurer::disable);
 
         // @formatter:on
 
         return http.build();
+    }
+
+    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository());
+
+        // Sets the location that the End-User's User Agent will be redirected to
+        // after the logout has been performed at the Provider
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/shibboleth");
+
+        return oidcLogoutSuccessHandler;
     }
 
     @Bean
