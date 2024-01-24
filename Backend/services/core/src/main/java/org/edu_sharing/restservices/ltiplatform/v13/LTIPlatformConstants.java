ae576f6845416440e80e1b9443321aec3c8f26b4
Torsten Simon <simon@edu-sharing.net>
2024-01-24 12:28:22 +0100

    fix:lti typo

--- Backend/services/core/src/main/java/org/edu_sharing/restservices/ltiplatform/v13/LTIPlatformConstants.java
+++ Backend/services/core/src/main/java/org/edu_sharing/restservices/ltiplatform/v13/LTIPlatformConstants.java
@@ -5,7 +5,7 @@ public class LTIPlatformConstants {
 
     public static final String CUSTOM_CLAIM_APP_ID = "appId";
 
-    public static final String CUSTOM_CLAIM_RESOURCE_TYPE = "resoureType";
+    public static final String CUSTOM_CLAIM_RESOURCE_TYPE = "resourceType";
 
     public static final String CUSTOM_CLAIM_USER = "user";
 
