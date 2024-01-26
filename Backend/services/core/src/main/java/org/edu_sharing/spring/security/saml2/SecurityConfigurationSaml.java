d2181b713bac4c45e29f9f03ca637862b5bb6d84
Daniel Rudolph <rudolph@edu-sharing.net>
2024-01-26 15:39:12 +0100

    feature: alfresco 23.2 / saml remove use of "samllogout path", fix sp triggerd logout, prepare dynamic logoutsuccess url, docu

--- Backend/services/core/src/main/java/org/edu_sharing/spring/security/saml2/SecurityConfigurationSaml.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/security/saml2/SecurityConfigurationSaml.java
@@ -1,6 +1,9 @@
 package org.edu_sharing.spring.security.saml2;
 
 import com.typesafe.config.Config;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.edu_sharing.alfresco.lightbend.LightbendConfigLoader;
 import org.edu_sharing.repository.server.tools.ApplicationInfoList;
@@ -9,11 +12,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.converter.RsaKeyConverters;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.saml2.core.Saml2X509Credential;
 import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
@@ -21,6 +24,7 @@ import org.springframework.security.saml2.provider.service.registration.RelyingP
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.IOException;
@@ -28,6 +32,8 @@ import java.io.InputStream;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Profile("samlEnabled")
 @EnableWebSecurity(debug = true)
@@ -38,13 +44,9 @@ public class SecurityConfigurationSaml {
 
     @Bean
     SecurityFilterChain app(HttpSecurity http) throws Exception {
-        System.out.println("this is the new version");
-        // @formatter:off
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        //.anyRequest().authenticated()
                         .requestMatchers(new AntPathRequestMatcher("/shibboleth")).authenticated()
-                        .requestMatchers(new AntPathRequestMatcher("/samllogout")).authenticated()
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
                 )
 
@@ -53,13 +55,18 @@ public class SecurityConfigurationSaml {
                          * SavedRequestAwareAuthenticationSuccessHandler is default which always redircts to /samllogout url with GET which is not vailable
                          * after logout/login again
                          */
-                        .successHandler(new MySuccsessHandler())
+                        .successHandler(new EduAuthSuccsessHandler())
                         //don't use this cause it uses SavedRequestAwareAuthenticationSuccessHandler
                         //.defaultSuccessUrl("/shibboleth")
                 )
                  //change that cause of logout servlet
-                .saml2Logout((req) -> req.logoutUrl("/samllogout"))
-                .saml2Metadata(Customizer.withDefaults())
+                .saml2Logout(withDefaults())
+                /**
+                 * saml2 logout is using logoutSuccessHandler from default logout config
+                 * @see org.springframework.security.config.annotation.web.configurers.saml2Saml2LogoutConfigurer
+                 */
+                .logout(logout -> logout.logoutSuccessHandler(new EduSimpleUrlLogoutSuccessHandler()))
+                .saml2Metadata(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
 
         // @formatter:on
@@ -67,8 +74,21 @@ public class SecurityConfigurationSaml {
         return http.build();
     }
 
-    public class MySuccsessHandler extends SimpleUrlAuthenticationSuccessHandler{
-        MySuccsessHandler(){
+    public class EduSimpleUrlLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler{
+        @Override
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+            //override at runtime
+            this.setDefaultTargetUrl("/shibboleth");
+            super.onLogoutSuccess(request, response, authentication);
+        }
+    }
+
+    /**
+     * @TODO make redirect dynamic depending on repository configuration
+     */
+    public class EduAuthSuccsessHandler extends SimpleUrlAuthenticationSuccessHandler{
+        EduAuthSuccsessHandler(){
             super("/shibboleth");
         }
     }
@@ -88,7 +108,7 @@ public class SecurityConfigurationSaml {
                             (c) -> c.add(Saml2X509Credential.decryption(privateKey, relyingPartyCertificate())))
                     .signingX509Credentials(
                             (c) -> c.add(Saml2X509Credential.signing(privateKey, relyingPartyCertificate())))
-                    .singleLogoutServiceLocation(ApplicationInfoList.getHomeRepository().getClientBaseUrl()+"/samllogout")
+                    .singleLogoutServiceLocation("{baseUrl}/logout/saml2/slo")
                     //.singleLogoutServiceResponseLocation(ApplicationInfoList.getHomeRepository().getClientBaseUrl()+"/shibboleth")
                     //.singleLogoutServiceBinding(Saml2MessageBinding.POST)
                     .build();
