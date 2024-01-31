edu-sharing/edu-sharing-community-repository

commit 31ec901927335445f628422be2435da6363c0fe4

fix: libcheck.sh won't recognize version like '-RC1'

deploy/docker/build/service/src/main/build/assets/libcheck.sh
@@ -32,13 +32,13 @@ for file in "${whitelist[@]}" ; do
 done
 
 groupedFiles=()
-while IFS='' read -r line; do groupedFiles+=("$line"); done < <(printf "%s\n" "${files[@]}" | sed 's|-[0-9.][0-9.]*[a-zA-Z0-9_]*\.jar:.*$||' | sort | uniq -c | sed 's| *||') #| sed 's| |:|g')
+while IFS='' read -r line; do groupedFiles+=("$line"); done < <(printf "%s\n" "${files[@]}" | sed 's|-[0-9.][0-9.]*[a-zA-Z0-9_-]*\.jar:.*$||' | sort | uniq -c | sed 's| *||') #| sed 's| |:|g')
 
 duplicates=()
 for group in "${groupedFiles[@]}" ; do
   if [[ -n ${group##* } ]] && [[ ${group%% *} != 1 ]] ; then
     while IFS='' read -r line; do duplicates+=("$line"); done < <(echo "Multiple library versions of ${group##* } (${group%% *}) found:")
-    while IFS='' read -r line; do duplicates+=("$line"); done < <(printf "%s\n" "${files[@]}" | grep "^${group##* }-[0-9.][0-9.]*[a-zA-Z0-9_]*\.jar:.*$")
+    while IFS='' read -r line; do duplicates+=("$line"); done < <(printf "%s\n" "${files[@]}" | grep "^${group##* }-[0-9.][0-9.]*[a-zA-Z0-9_-]*\.jar:.*$")
     while IFS='' read -r line; do duplicates+=("$line"); done < <(echo "")
   fi
 done

commit 11202db5f064f5c61fa999bbcfec235d87c237c8

fix:ignore stax api (was always duplicated and caused by 31ec9019)

deploy/docker/build/service/src/main/build/assets/libcheck.sh
@@ -7,6 +7,8 @@ whitelist=(
   # swagger gen vs swagger usage conflict -> new api
   "xmlsec-1.5.8.jar:alf-lib" # axis-1.4
   "xmlsec-1.4.5.jar:edu-lib" # axis-1.4
+  "stax-api-1.0-2.jar:alf-lib"
+  "stax-api-1.0.1.jar:alf-lib"
 )
 
 files=()