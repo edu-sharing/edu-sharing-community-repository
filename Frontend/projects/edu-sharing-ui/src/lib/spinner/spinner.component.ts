edaec0cdba8f15edacd8231984e3e0c901ec09ce
Torsten Simon <simon@edu-sharing.net>
2024-04-02 12:33:52 +0200

    fix:spinner import translate module deps

--- Frontend/projects/edu-sharing-ui/src/lib/spinner/spinner.component.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/spinner/spinner.component.ts
@@ -1,10 +1,12 @@
 import { Component, HostBinding, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'es-spinner',
     templateUrl: 'spinner.component.html',
     styleUrls: ['spinner.component.scss'],
     standalone: true,
+    imports: [TranslateModule],
 })
 export class SpinnerComponent {
     @HostBinding('attr.data-test') readonly dataTest = 'loading-spinner';
