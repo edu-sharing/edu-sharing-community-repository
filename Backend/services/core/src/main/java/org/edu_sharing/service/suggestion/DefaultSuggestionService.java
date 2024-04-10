587b3fd063b4548a5a47fb61c271841a92f0467e
Torsten Simon <simon@edu-sharing.net>
2024-04-10 16:24:55 +0200

    fix:dummy suggestions (WPO)

--- Backend/services/core/src/main/java/org/edu_sharing/service/suggestion/DefaultSuggestionService.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/suggestion/DefaultSuggestionService.java
@@ -28,6 +28,14 @@ public class DefaultSuggestionService implements SuggestionService {
     public Map<String, List<Suggestion>> getSuggestionsByNodeId(String nodeId) {
 
         return Map.of(
+                "cclom:general_description", List.of(
+                        new Suggestion("1","cclom:general_description", "hello world", SuggestionType.AI, SuggestionStatus.PENDING, "Lorem Ipsum", 1.0, new Date(), "chat-gpt-3.5", null, null),
+                        new Suggestion("2","cclom:general_description", "lorem ipsum\ndolor sit amet", SuggestionType.AI, SuggestionStatus.PENDING, "Lorem Ipsum", 1.0, new Date(), "chat-gpt-4.0", null, null)
+                ),
+                "cclom:title", List.of(
+                        new Suggestion("1","cclom:title", "Guter Titel", SuggestionType.AI, SuggestionStatus.PENDING, "Lorem Ipsum", 1.0, new Date(), "chat-gpt-3.5", null, null),
+                        new Suggestion("2","cclom:title", "Noch besserer Titel", SuggestionType.AI, SuggestionStatus.PENDING, "Lorem Ipsum", 1.0, new Date(), "chat-gpt-4.0", null, null)
+                ),
                 "cclom:general_keyword", List.of(new Suggestion("1","cclom:general_keyword", "hello world", SuggestionType.AI, SuggestionStatus.PENDING, "Test", 1.0, new Date(), "chat-gpt-3.5", null, null)),
                 "ccm:educationallearningresourcetype", List.of(new Suggestion("1","ccm:educationallearningresourcetype", "exercise", SuggestionType.AI, SuggestionStatus.PENDING, "Test", 0.5, new Date(), "chat-gpt-3.5", null, null))
         );
