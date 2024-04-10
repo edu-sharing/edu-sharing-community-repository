b0ca7dc58e317faa295a71d4d108990a856e2401
Sebastian Wollner <wollner@edu-sharing.net>
2024-04-10 14:39:19 +0200

    feat: added SuggestionApi test data(WPO)

--- Backend/services/core/src/main/java/org/edu_sharing/service/suggestion/DefaultSuggestionService.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/suggestion/DefaultSuggestionService.java
@@ -1,8 +1,10 @@
 package org.edu_sharing.service.suggestion;
 
 import org.apache.commons.lang.NotImplementedException;
+import org.edu_sharing.repository.client.tools.CCConstants;
 import org.edu_sharing.restservices.suggestions.v1.dto.CreateSuggestionRequestDTO;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -25,7 +27,11 @@ public class DefaultSuggestionService implements SuggestionService {
 
     @Override
     public Map<String, List<Suggestion>> getSuggestionsByNodeId(String nodeId) {
-        throw new NotImplementedException();
+
+        return Map.of(
+                "cclom:general_keyword", List.of(new Suggestion("1","cclom:general_keyword", "hello world", SuggestionType.AI, SuggestionStatus.PENDING, "Test", 1.0, new Date(), "chat-gpt-3.5", null, null)),
+                "ccm:educationallearningresourcetype", List.of(new Suggestion("1","ccm:educationallearningresourcetype", "exercise", SuggestionType.AI, SuggestionStatus.PENDING, "Test", 0.5, new Date(), "chat-gpt-3.5", null, null))
+        );
     }
 
 }
