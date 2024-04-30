4ccb68c5a7b98c174c53d913e7fa75d8d1647cca
Sebastian Wollner <wollner@edu-sharing.net>
2024-04-30 12:03:40 +0200

    refactoring: Trackingservice -  TrackingServiceCustomInterface from beans and obsolete from class

--- Backend/services/core/src/main/java/org/edu_sharing/service/tracking/TrackingServiceFactory.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/tracking/TrackingServiceFactory.java
@@ -1,24 +1,41 @@
 package org.edu_sharing.service.tracking;
 
+import org.apache.http.annotation.Obsolete;
 import org.apache.log4j.Logger;
-import org.edu_sharing.repository.server.DownloadServlet;
-import org.edu_sharing.service.nodeservice.NodeService;
 import org.edu_sharing.spring.ApplicationContextFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Component
 public class TrackingServiceFactory {
     private static Logger logger = Logger.getLogger(TrackingServiceFactory.class);
 
-    public static TrackingService getTrackingService() {
-			return (TrackingService) ApplicationContextFactory.getApplicationContext().getBean("trackingService");
-	}
+    private final TrackingServiceCustomInterface trackingServiceCustomInterface;
 
-	public static TrackingServiceCustomInterface getTrackingServiceCustom() {
-		try {
-			return (TrackingServiceCustomInterface) Class.forName(TrackingService.class.getName() + "Custom").newInstance();
-		} catch(ClassNotFoundException t) {
-			logger.debug("no class "+TrackingService.class.getName()+"Custom"+" found, will use default implementation for tracking");
-			return null;
-		} catch (InstantiationException | IllegalAccessException e) {
+    public TrackingServiceFactory(Optional<TrackingServiceCustomInterface> trackingServiceCustomInterface) {
+        this.trackingServiceCustomInterface = trackingServiceCustomInterface.orElseGet(this::getTrackingServiceCustomInterfaceByClassName);
+
+    }
+
+
+    public static TrackingService getTrackingService() {
+        return (TrackingService) ApplicationContextFactory.getApplicationContext().getBean("trackingService");
+    }
+
+    public TrackingServiceCustomInterface getTrackingServiceCustom() {
+        return trackingServiceCustomInterface;
+    }
+
+    @Obsolete
+    @Nullable private TrackingServiceCustomInterface getTrackingServiceCustomInterfaceByClassName() {
+        try {
+            return (TrackingServiceCustomInterface) Class.forName(TrackingService.class.getName() + "Custom").newInstance();
+        } catch (ClassNotFoundException t) {
+            logger.debug("no class " + TrackingService.class.getName() + "Custom" + " found, will use default implementation for tracking");
+            return null;
+        } catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
