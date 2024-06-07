5ae6fe1a5b23bda833d54490a4642f17ab0b5987
Torsten Simon <simon@edu-sharing.net>
2024-06-07 15:37:24 +0200

    fix:redirect sso when local=true
    ESPUB-98

--- Frontend/src/app/modules/login/login.component.ts
+++ Frontend/src/app/modules/login/login.component.ts
@@ -121,7 +121,8 @@ export class LoginComponent implements OnInit, OnDestroy, AfterViewInit {
                         } else if (data.currentScope === this.scope) {
                             if (
                                 data.statusCode === RestConstants.STATUS_CODE_OK &&
-                                params.local !== 'true'
+                                // force redirect when local was NOT requested or redirectFromSSO was enforced
+                                (params.local !== 'true' || params.redirectFromSSO === 'true')
                             ) {
                                 this.goToNext(data);
                             }
