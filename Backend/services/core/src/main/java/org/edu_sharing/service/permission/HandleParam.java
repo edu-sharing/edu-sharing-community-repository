9c75e0cef9ea1271c2e131a5b95e618e7f6389f3
Daniel Rudolph <rudolph@edu-sharing.net>
2024-06-21 09:08:20 +0200

    feat: DOIService missing constructor for HandleParam

--- Backend/services/core/src/main/java/org/edu_sharing/service/permission/HandleParam.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/permission/HandleParam.java
@@ -1,8 +1,12 @@
 package org.edu_sharing.service.permission;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class HandleParam {
     public HandleMode handleService;
     public HandleMode doiService;
