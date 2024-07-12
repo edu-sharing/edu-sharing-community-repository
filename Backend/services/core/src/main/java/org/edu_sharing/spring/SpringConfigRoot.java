785ba172b0fca40c9056965e04138e3c8624ee3b
Daniel Rudolph <rudolph@edu-sharing.net>
2024-07-12 14:11:00 +0200

    feature: allow using spring @Controller @RestController annotations

--- Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
@@ -6,11 +6,13 @@ import org.edu_sharing.spring.security.saml2.SecurityConfigurationSaml;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Import({SecurityConfigurationSaml.class, SecurityConfigurationOpenIdConnect.class, SecurityConfigurationBasic.class})
 //component scan to enable edu-sharing custom condition annotations
 @ComponentScan(basePackages = {"org.edu_sharing"})
-public class SpringConfigRoot {
+@EnableWebMvc
+public class SpringConfigRoot implements WebMvcConfigurer {
 }
