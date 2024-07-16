ff336a6619c2a913b4ee0502cc59585d263f7c18
Daniel Rudolph <rudolph@edu-sharing.net>
2024-07-16 16:11:06 +0200

    Revert "feature: allow using spring @Controller @RestController annotations"
    This reverts commit 785ba172b0fca40c9056965e04138e3c8624ee3b.

--- Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
@@ -6,13 +6,11 @@ import org.edu_sharing.spring.security.saml2.SecurityConfigurationSaml;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import({SecurityConfigurationSaml.class, SecurityConfigurationOpenIdConnect.class, SecurityConfigurationBasic.class})
 //component scan to enable edu-sharing custom condition annotations
 @ComponentScan(basePackages = {"org.edu_sharing"})
-@EnableWebMvc
-public class SpringConfigRoot implements WebMvcConfigurer {
+public class SpringConfigRoot {
 }
