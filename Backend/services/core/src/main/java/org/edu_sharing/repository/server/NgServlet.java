c01ed7514a1dab24d0fd4608f8cbc6005e64cd17
Torsten Simon <simon@edu-sharing.net>
2024-08-08 11:51:46 +0200

    fix:render remote provider wrong app id (InvalidNodeRefException)

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/NgServlet.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/NgServlet.java
@@ -143,7 +143,7 @@ public class NgServlet extends HttpServlet {
 				return addToHead(data, html);
 			}
 		}catch(Throwable t){
-			logger.error("Failed to load node license for attaching to head:",t);
+			logger.warn("Failed to load node license for attaching to head:",t);
 		}
 		return html;	}
 
