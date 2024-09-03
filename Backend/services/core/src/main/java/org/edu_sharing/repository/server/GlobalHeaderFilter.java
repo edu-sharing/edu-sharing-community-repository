57bb425eba215b1361d21df986f166ed9810ce68
Torsten Simon <simon@edu-sharing.net>
2024-09-03 14:52:48 +0200

    feat:open to disable custom paths like specific angular / js assets

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/GlobalHeaderFilter.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/GlobalHeaderFilter.java
@@ -3,39 +3,11 @@ package org.edu_sharing.repository.server;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import net.sf.acegisecurity.AuthenticationCredentialsNotFoundException;
-import org.alfresco.repo.security.authentication.AuthenticationComponent;
-import org.alfresco.repo.security.authentication.AuthenticationUtil;
-import org.alfresco.service.ServiceRegistry;
-import org.alfresco.service.cmr.security.AuthenticationService;
-import org.apache.log4j.Logger;
-import org.edu_sharing.alfresco.authentication.HttpContext;
-import org.edu_sharing.alfresco.authentication.subsystems.SubsystemChainingAuthenticationService;
 import org.edu_sharing.alfresco.lightbend.LightbendConfigLoader;
-import org.edu_sharing.alfresco.policy.NodeCustomizationPolicies;
-import org.edu_sharing.alfresco.repository.server.authentication.Context;
-import org.edu_sharing.alfresco.service.config.model.AvailableMds;
-import org.edu_sharing.alfresco.workspace_administration.NodeServiceInterceptor;
-import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
-import org.edu_sharing.metadataset.v2.QueryUtils;
-import org.edu_sharing.repository.TrackingApplicationInfo;
-import org.edu_sharing.repository.client.tools.CCConstants;
-import org.edu_sharing.repository.server.tools.ApplicationInfoList;
-import org.edu_sharing.repository.server.tools.security.SignatureVerifier;
-import org.edu_sharing.restservices.NodeDao;
-import org.edu_sharing.restservices.RepositoryDao;
-import org.edu_sharing.service.authentication.SSOAuthorityMapper;
-import org.edu_sharing.service.authentication.ScopeAuthenticationServiceFactory;
-import org.edu_sharing.service.authority.AuthorityServiceFactory;
-import org.edu_sharing.service.config.ConfigServiceFactory;
-import org.edu_sharing.service.usage.Usage;
-import org.edu_sharing.service.usage.Usage2Exception;
-import org.edu_sharing.service.usage.Usage2Service;
-import org.edu_sharing.webservices.util.AuthenticationUtils;
-import org.springframework.context.ApplicationContext;
+import org.edu_sharing.service.authority.AuthorityServiceHelper;
+import org.edu_sharing.service.nodeservice.CallSourceHelper;
 
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.Map;
 
 
@@ -52,6 +24,23 @@ public class GlobalHeaderFilter implements Filter {
 				}
 			}
 		}
+		if (LightbendConfigLoader.get().hasPath("security.access.paths")) {
+			String pathInfo = ((HttpServletRequest)req).getPathInfo();
+			if(pathInfo == null) {
+				String uri = ((HttpServletRequest)req).getRequestURI();
+				pathInfo = uri == null ? null : uri.substring(CallSourceHelper.WEBAPP_BASE_PATH.length());
+			}
+			if(pathInfo != null) {
+				for (Map.Entry<String, Object> entry : LightbendConfigLoader.get().getObject("security.access.paths").unwrapped().entrySet()) {
+					if (pathInfo.startsWith(entry.getKey()) && ("disabled".equalsIgnoreCase(entry.getValue().toString()) || "admin".equalsIgnoreCase(entry.getValue().toString()) && !AuthorityServiceHelper.isAdmin())) {
+						((HttpServletResponse) res).setStatus(HttpServletResponse.SC_FORBIDDEN);
+						res.flushBuffer();
+						res.getWriter().print("This path is disabled via config");
+						return;
+					}
+				}
+			}
+        }
 		chain.doFilter(req, res);
 	}
 
