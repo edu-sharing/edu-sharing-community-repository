0737ba30a37383d8ec5c7ddbbf7e7ee316fee50b
Daniel Rudolph <rudolph@edu-sharing.net>
2024-09-05 13:30:45 +0200

    feat: allow permission enhancement by custom implementation: hasPermission interceptor

--- Backend/services/core/src/main/java/org/edu_sharing/service/nodeservice/NodeServiceInterceptorPermissions.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/nodeservice/NodeServiceInterceptorPermissions.java
@@ -2,4 +2,8 @@ package org.edu_sharing.service.nodeservice;
 
 public interface NodeServiceInterceptorPermissions {
     public boolean accessable(String nodeId, int recursionDepth);
+
+    public default boolean hasPermission(String nodeId, String permission){
+        return false;
+    }
 }
