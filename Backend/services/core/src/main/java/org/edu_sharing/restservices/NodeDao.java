84b6272dcb2bc39c7e8c12fa96d0cc3f49627c2b
Torsten Simon <simon@edu-sharing.net>
2024-09-25 09:03:56 +0200

    fix:expose Embed permission via DAO

--- Backend/services/core/src/main/java/org/edu_sharing/restservices/NodeDao.java
+++ Backend/services/core/src/main/java/org/edu_sharing/restservices/NodeDao.java
@@ -104,6 +104,7 @@ public class NodeDao {
             org.alfresco.service.cmr.security.PermissionService.WRITE,
             org.alfresco.service.cmr.security.PermissionService.DELETE,
             CCConstants.PERMISSION_COMMENT,
+            CCConstants.PERMISSION_EMBED,
             CCConstants.PERMISSION_DOWNLOAD_CONTENT,
             CCConstants.PERMISSION_FEEDBACK,
             CCConstants.PERMISSION_CC_PUBLISH,
