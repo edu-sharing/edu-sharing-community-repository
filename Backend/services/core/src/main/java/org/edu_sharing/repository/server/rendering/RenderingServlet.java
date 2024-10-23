8aa12f313da60e8fde4f2dfa4192b1e90005335a
Daniel Rudolph <rudolph@edu-sharing.net>
2024-10-23 21:10:20 +0200

    feat: embed permission: revert always render original

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/rendering/RenderingServlet.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/rendering/RenderingServlet.java
@@ -10,6 +10,7 @@ import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.edu_sharing.alfresco.RestrictedAccessException;
 import org.edu_sharing.repository.client.tools.CCConstants;
 import org.edu_sharing.repository.server.SecurityHeadersFilter;
 import org.edu_sharing.repository.server.tools.ApplicationInfoList;
@@ -33,16 +34,10 @@ import java.util.Map;
 public class RenderingServlet extends HttpServlet {
     private static final String DEFAULT_DISPLAY_MODE = RenderingTool.DISPLAY_EMBED;
     private static Logger logger = Logger.getLogger(RenderingServlet.class);
-    private static PermissionChecking permissionChecking;
 
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp)
                 throws ServletException, IOException {
-
-            if(permissionChecking == null) {
-                // @TODO: Is there a more streamlined way?
-                permissionChecking = RequestContextUtils.findWebApplicationContext(req).getBean("permissionChecking", PermissionChecking.class);
-            }
 
             // new, preferred parameter
             String node_id = req.getParameter("nodeId");
@@ -78,20 +73,27 @@ public class RenderingServlet extends HttpServlet {
             resp.getWriter().write("<body class= \"eduservlet-render-body\">");
             String response;
             try {
-                /*if(!PermissionServiceFactory.getLocalService().hasPermission(StoreRef.PROTOCOL_WORKSPACE,
-                        StoreRef.STORE_REF_WORKSPACE_SPACESSTORE.getIdentifier(),
-                        node_id,
-                        CCConstants.PERMISSION_EMBED)){
-                     throw new AccessDeniedException(CCConstants.PERMISSION_EMBED);
-                }*/
-                String original = checkAndGetCollectionRef(node_id);
-                if(original != null) {
-                    response = AuthenticationUtil.runAsSystem(() -> renderInternal(renderingService,original,version,params));
-                }else {
-                    response = renderInternal(renderingService,original,version,params);
+                NodeRef ref = new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, node_id);
+                if(NodeServiceHelper.hasAspect(ref,CCConstants.CCM_ASPECT_COLLECTION_IO_REFERENCE)){
+                    try {
+                        NodeServiceHelper.validatePermissionRestrictedAccess(ref, CCConstants.PERMISSION_READ_ALL, CCConstants.PERMISSION_EMBED);
+                    } catch (RestrictedAccessException e) {
+                        throw new AccessDeniedException(CCConstants.PERMISSION_EMBED);
+                    }
+                }else{
+                    if(!PermissionServiceFactory.getLocalService().hasPermission(ref.getStoreRef().getProtocol(),
+                            ref.getStoreRef().getIdentifier(),
+                            node_id,
+                            CCConstants.PERMISSION_EMBED)) {
+                        throw new AccessDeniedException(CCConstants.PERMISSION_EMBED);
+                    }
                 }
 
-
+                response = renderingService.getDetails(ApplicationInfoList.getHomeRepository().getAppId(), node_id, version,DEFAULT_DISPLAY_MODE, params).getDetails();
+                response = response.replace("{{{LMS_INLINE_HELPER_SCRIPT}}}", URLHelper.getNgRenderNodeUrl(node_id,version)+"?");
+                // add nonce to render styles
+                response = response.replace("<style", "<style nonce=\"" +SecurityHeadersFilter.ngCspNonce.get() + "\"");
+                TrackingServiceFactory.getTrackingService().trackActivityOnNode(new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, node_id), null, TrackingService.EventType.VIEW_MATERIAL_EMBEDDED);
             } catch (Throwable t) {
                 RenderingException exception = RenderingException.fromThrowable(t);
                 response = RenderingErrorServlet.errorToHTML(req,
@@ -103,25 +105,4 @@ public class RenderingServlet extends HttpServlet {
             resp.getWriter().write("</body>");
             resp.getWriter().write("</html>");
         }
-
-        private String renderInternal(RenderingService renderingService, String node_id, String version, Map<String, String> params) throws Exception {
-            String response = renderingService.getDetails(ApplicationInfoList.getHomeRepository().getAppId(), node_id, version,DEFAULT_DISPLAY_MODE, params).getDetails();
-            response = response.replace("{{{LMS_INLINE_HELPER_SCRIPT}}}", URLHelper.getNgRenderNodeUrl(node_id,version)+"?");
-            // add nonce to render styles
-            response = response.replace("<style", "<style nonce=\"" +SecurityHeadersFilter.ngCspNonce.get() + "\"");
-            TrackingServiceFactory.getTrackingService().trackActivityOnNode(new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, node_id), null, TrackingService.EventType.VIEW_MATERIAL_EMBEDDED);
-            return response;
-        }
-
-
-    private static String checkAndGetCollectionRef(String nodeId) throws InsufficientPermissionException {
-        NodeRef ref = new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, nodeId);
-        if(NodeServiceHelper.hasAspect(ref,CCConstants.CCM_ASPECT_COLLECTION_IO_REFERENCE)){
-            NodeServiceHelper.validatePermissionRestrictedAccess(ref, CCConstants.PERMISSION_READ_ALL, CCConstants.PERMISSION_EMBED);
-            return NodeServiceHelper.getProperty(ref, CCConstants.CCM_PROP_IO_ORIGINAL);
-        } else {
-            permissionChecking.checkNodePermissions(nodeId, new String[]{CCConstants.PERMISSION_READ_ALL, CCConstants.PERMISSION_EMBED});
-        }
-        return null;
-    }
 }
