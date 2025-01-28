1ab06d209c0f4e5dec11db7f5a27c28bfcde8f32
Daniel Rudolph <rudolph@edu-sharing.net>
2025-01-28 17:58:42 +0100

    feat no solr: ProtocolDiffDBSolrJob

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/ProtocolDiffDBSolrJob.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/ProtocolDiffDBSolrJob.java
@@ -1,5 +1,7 @@
 package org.edu_sharing.repository.server.jobs.quartz;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
@@ -10,7 +12,12 @@ import org.alfresco.service.cmr.search.SearchParameters;
 import org.alfresco.service.cmr.search.SearchService;
 import org.apache.log4j.Logger;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
+import org.edu_sharing.repository.server.SearchResultNodeRef;
 import org.edu_sharing.repository.server.jobs.helper.NodeHelper;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobDescription;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobFieldDescription;
+import org.edu_sharing.service.search.SearchServiceFactory;
+import org.edu_sharing.service.search.model.SearchToken;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.context.ApplicationContext;
@@ -19,25 +26,29 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class ProtocolDiffDBSolrJob extends AbstractJob{
+import static org.apache.camel.model.rest.RestParamType.query;
 
-    public static final String PARAM_START_FOLDER = "START_FOLDER";
+@JobDescription(description = "logs diff elastic and db")
+public class ProtocolDiffDBSolrJob extends AbstractJobMapAnnotationParams{
+
     public static final String DESCRIPTION = "logs diff solr and db";
     ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
     ServiceRegistry serviceRegistry = (ServiceRegistry) applicationContext.getBean(ServiceRegistry.SERVICE_REGISTRY);
 
     Logger logger = Logger.getLogger(ProtocolDiffDBSolrJob.class);
 
+    @JobFieldDescription(description = "folder that needs to be compared")
+    String startFolder;
+
     int PAGE_SIZE = 1000;
 
     @Override
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-
-        String startFolder = (String)jobExecutionContext.getJobDetail().getJobDataMap().get(PARAM_START_FOLDER);
+    public void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
         if(startFolder == null){
-            logger.error("no "+PARAM_START_FOLDER+ " provided");
+            logger.error("no startFolder provided");
             return;
         }
         AuthenticationUtil.runAsSystem(() -> {
@@ -51,36 +62,37 @@ public class ProtocolDiffDBSolrJob extends AbstractJob{
 
         String pathPrefixString = path.toPrefixString(serviceRegistry.getNamespaceService());
         logger.info("pathPrefixString:"+pathPrefixString);
-        String query = "PATH:\""+pathPrefixString+"//*\" AND TYPE:\"ccm:io\"";
+        pathPrefixString = pathPrefixString.replaceFirst("/","");
+        pathPrefixString = pathPrefixString+"*";
+        String pathQuery = pathPrefixString;
 
 
-        logger.info("collect solr nodes " + query);
-        ResultSet rs = null;
-        int skipCount = 0;
-        List<NodeRef> nodesInSolr = new ArrayList<>();
-        do{
-            SearchParameters sp = new SearchParameters();
-            sp.setQuery(query);
-            sp.setLanguage(SearchService.LANGUAGE_LUCENE);
-            sp.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
-            sp.setMaxItems(PAGE_SIZE);
-            sp.setSkipCount(skipCount);
-            rs = serviceRegistry.getSearchService().query(sp);
-            logger.info("found "+ rs.getNumberFound() +" in solr index. skipCount:" + skipCount+". currentPageSize: "+rs.length());
-            nodesInSolr.addAll(rs.getNodeRefs());
-            skipCount = skipCount + PAGE_SIZE;
-        }while( rs.getNumberFound() > skipCount);
+        SearchToken token = new SearchToken();
+        token.setFrom(0);
+        token.setMaxResult(Integer.MAX_VALUE);
+        token.setElasticQuery(
+            QueryBuilders.bool()
+                    .must(m -> m.wildcard(w -> w.field("fulldisplaypath").value(pathQuery)))
+                    .must(m -> m.term(t -> t.field("type").value("ccm:io")))
+                    .build());
+
+        org.edu_sharing.service.search.SearchService searchService = SearchServiceFactory.getLocalService();
+
+        logger.info("collect elastic nodes " + token.getElasticQuery());
+        SearchResultNodeRef search = searchService.search(token);
+        List<NodeRef> nodesInElastic = search.getData().stream()
+                .map(n -> new NodeRef(new StoreRef(n.getStoreProtocol(),n.getStoreId()),n.getNodeId()))
+                .collect(Collectors.toList());
 
         logger.info("collection db nodes");
         List<NodeRef> nodesInDb = new NodeHelper().getNodes(startFolder);
-
         List<NodeRef> diff = new ArrayList<>(nodesInDb);
-        diff.removeAll(nodesInSolr);
+        diff.removeAll(nodesInElastic);
         if(diff.size() == 0){
-            logger.info("no diff between database and solr in this folder " + startFolder);
+            logger.info("no diff between database and elastic in this folder " + startFolder);
         }
         for(NodeRef node : diff){
-            logger.info("in db not in solr:"+node);
+            logger.info("in db not in elastic:"+node);
         }
     }
 }
