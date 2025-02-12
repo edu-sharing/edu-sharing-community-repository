e96d20dd3eafbcf27c65569c8a2898e17c71eed5
Daniel Rudolph <rudolph@edu-sharing.net>
2025-02-12 10:15:53 +0100

    feat no solr: TakeBackGlobalShare

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/TakeBackGlobalShare.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/TakeBackGlobalShare.java
@@ -1,8 +1,6 @@
 package org.edu_sharing.repository.server.jobs.quartz;
 
-import java.util.Set;
-
-import org.alfresco.model.ContentModel;
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import org.alfresco.repo.search.impl.solr.ESSearchParameters;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
@@ -10,68 +8,66 @@ import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.search.ResultSet;
-import org.alfresco.service.cmr.search.SearchService;
-import org.alfresco.service.cmr.security.AccessPermission;
 import org.alfresco.service.cmr.security.PermissionService;
-import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 import org.edu_sharing.alfrescocontext.gate.AlfAppContextGate;
-import org.edu_sharing.repository.client.tools.CCConstants;
+import org.edu_sharing.repository.server.SearchResultNodeRef;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobDescription;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobFieldDescription;
+import org.edu_sharing.service.search.SearchService;
+import org.edu_sharing.service.search.SearchServiceFactory;
+import org.edu_sharing.service.search.model.SearchToken;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.context.ApplicationContext;
 
-public class TakeBackGlobalShare extends AbstractJob {
+import java.util.List;
+
+@JobDescription(description = "removes ALL_AUTHORITIES permission from io's and map's")
+public class TakeBackGlobalShare extends AbstractJobMapAnnotationParams {
 	
 	ApplicationContext applicationContext = AlfAppContextGate.getApplicationContext();
 	ServiceRegistry serviceRegistry = (ServiceRegistry) applicationContext.getBean(ServiceRegistry.SERVICE_REGISTRY);
 	private NodeService nodeService = serviceRegistry.getNodeService();
-	private SearchService searchService = serviceRegistry.getSearchService();
 	private PermissionService permissionService = serviceRegistry.getPermissionService();
 	
 	Logger logger = Logger.getLogger(TakeBackGlobalShare.class);
-	
-	public static String PARAM_EXECUTE = "EXECUTE";
-	
-	int PAGE_SIZE = 100;
-	
+
+	@JobFieldDescription(description = "if false job runs in protocol mode")
+	Boolean execute;
+
 	TakeBackGlobalShareWorker worker;
 	
 	@Override
-	public void execute(JobExecutionContext context) throws JobExecutionException {
-		boolean execute = new Boolean((String)context.getJobDetail().getJobDataMap().get(PARAM_EXECUTE));
+	public void executeInternal(JobExecutionContext context) throws JobExecutionException {
 		
 		worker = new TakeBackGlobalShareWorker(nodeService, permissionService, execute);
-		
-		AuthenticationUtil.RunAsWork<Void> runAs = new AuthenticationUtil.RunAsWork<Void>() {
-			@Override
-			public Void doWork() throws Exception {
-				run(execute, 0);
-				return null;
-			}
-		};
+		if(execute == null) execute = Boolean.FALSE;
+
+		AuthenticationUtil.RunAsWork<Void> runAs = () -> {
+            run();
+            return null;
+        };
 		AuthenticationUtil.runAsSystem(runAs);
 	}
 	
-	private void run(boolean execute, int page) {
-		ESSearchParameters sp = new ESSearchParameters();
-		sp.setLanguage(SearchService.LANGUAGE_LUCENE);
-		sp.addStore(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
-		sp.setSkipCount(page);
-		sp.setMaxItems(PAGE_SIZE);
-		sp.setAuthorities(new String[] {PermissionService.ALL_AUTHORITIES});
-		sp.setQuery("TYPE:\"ccm:io\" OR TYPE:\"ccm:map\"");
-		
-		ResultSet resultSet = searchService.query(sp);
-		logger.info("page " + page + " from " + resultSet.getNumberFound());
-		
-		for(NodeRef nodeRef : resultSet.getNodeRefs()) {
+	private void run() {
+
+		SearchToken searchToken = new SearchToken();
+		searchToken.setFrom(0);
+		searchToken.setMaxResult(Integer.MAX_VALUE);
+		searchToken.setAuthorityScope(List.of(PermissionService.ALL_AUTHORITIES));
+		searchToken.setElasticQuery(QueryBuilders.bool()
+				.minimumShouldMatch("1")
+				.should(s -> s.term(t -> t.field("type").value("ccm:io")))
+				.should(s -> s.term(t -> t.field("type").value("ccm:map")))
+				.build());
+		SearchService searchService = SearchServiceFactory.getLocalService();
+		SearchResultNodeRef search = searchService.search(searchToken);
+		search.getData().forEach(n -> {
+			NodeRef nodeRef = new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE,n.getNodeId());
 			worker.work(nodeRef);
-		}
-		
-		if(resultSet.hasMore()) {
-			run(execute, page + PAGE_SIZE);
-		}
+		});
 	}
 
 	@Override
