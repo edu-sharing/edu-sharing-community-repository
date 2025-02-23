bf86fabf8c2b96504a3bfcfc7926ce469d9ab995
Torsten Simon <simon@edu-sharing.net>
2025-02-23 22:09:57 +0100

    fix:render 2 web application

--- Frontend/src/app/pages/render-main-page/render-main-page.component.ts
+++ Frontend/src/app/pages/render-main-page/render-main-page.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {AboutService} from 'ngx-edu-sharing-api';
-import {firstValueFrom} from 'rxjs';
-import {SpinnerComponent} from 'ngx-edu-sharing-ui';
-import {RenderLegacyPageComponent} from '../render-legacy-page/render-legacy-page.component';
-import {Render2PageComponent} from '../render2-page/render2-page.component';
-import {CommonModule} from "@angular/common";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AboutService } from 'ngx-edu-sharing-api';
+import { firstValueFrom } from 'rxjs';
+import { SpinnerComponent } from 'ngx-edu-sharing-ui';
+import { RenderLegacyPageComponent } from '../render-legacy-page/render-legacy-page.component';
+import { Render2PageComponent } from '../render2-page/render2-page.component';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 /**
  * wrapper that only decides which renderer (render2 or legacy) to lazy-load based on backend config
@@ -14,19 +15,21 @@ import {CommonModule} from "@angular/common";
     templateUrl: 'render-main-page.component.html',
     styleUrls: ['render-main-page.component.scss'],
     standalone: true,
-    imports: [
-        CommonModule,
-        RenderLegacyPageComponent,
-        Render2PageComponent,
-        SpinnerComponent],
+    imports: [CommonModule, RenderLegacyPageComponent, Render2PageComponent, SpinnerComponent],
 })
-export class RenderMainPageComponent implements OnInit {
+export class RenderMainPageComponent implements AfterViewInit {
     renderer: 'legacy' | 'render2' = null;
-    constructor(private about: AboutService) {
+    private queryParams: Params;
+    constructor(private about: AboutService, private route: ActivatedRoute) {
+        this.route.queryParams.subscribe((params) => (this.queryParams = params));
         return;
     }
-    async ngOnInit() {
+    async ngAfterViewInit() {
         try {
+            if (this.queryParams.renderer) {
+                this.renderer = this.queryParams.renderer;
+                return;
+            }
             this.renderer = (await firstValueFrom(this.about.getAbout())).plugins?.find(
                 (f) => f.id === 'rendering-service-2',
             )
