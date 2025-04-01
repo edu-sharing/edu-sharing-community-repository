071164b819f3b7be186b083d30febdae688c370e
Daniel Rudolph <rudolph@edu-sharing.net>
2025-04-01 14:42:34 +0200

    feat: job to touch nodes so that they get reindexed

---
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/TouchNodesJob.java
@@ -0,0 +1,68 @@
+package org.edu_sharing.repository.server.jobs.quartz;
+
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch.core.search.Hit;
+import jakarta.transaction.*;
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.repo.transaction.RetryingTransactionHelper;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobFieldDescription;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class TouchNodesJob extends FixElasticSearchBase{
+
+    @JobFieldDescription(description = "either to keep modified date or not")
+    boolean keepModifiedDate = true;
+
+    @JobFieldDescription(description = "if false (default) no changes will be done.")
+    boolean execute;
+
+    @JobFieldDescription(description = "query that delivers a result of nodes that have to be checked. optional. if not set all nodes will be searched.", sampleValue = "{\"term\":{\"type\":\"ccm:io\"}}")
+    protected String query;
+
+    @Override
+    public void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        AuthenticationUtil.runAsSystem(() -> {
+            try {
+                Query.Builder builder = getBuilder(query);
+                search( builder.build(), new TouchHandler());
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+            return null;
+        });
+    }
+
+    public class TouchHandler implements SearchResultHandler {
+
+        @Override
+        public void handleSearchHit(Hit<Map> searchHit) throws IOException {
+            NodeRef nodeRef = getNodeRef(searchHit);
+            String name = (String)nodeService.getProperty(nodeRef, ContentModel.PROP_NAME);
+            if(name == null){
+                logger.warn("ignoring node cause it has no name:" +nodeRef);
+                return;
+            }
+
+            logger.info("touching node:"+nodeRef +" name:"+name);
+            if(execute){
+                RetryingTransactionHelper th = serviceRegistry.getTransactionService().getRetryingTransactionHelper();
+                th.doInTransaction(() -> {
+                    try {
+                        if (keepModifiedDate) policyBehaviourFilter.disableBehaviour(nodeRef);
+                        nodeService.addAspect(nodeRef, ContentModel.ASPECT_INDEX_CONTROL,null);
+                    } finally {
+                        nodeService.removeAspect(nodeRef, ContentModel.ASPECT_INDEX_CONTROL);
+                        if(keepModifiedDate) policyBehaviourFilter.enableBehaviour(nodeRef);
+                    }
+                    return null;
+                },false,true);
+            }
+        }
+    }
+}
