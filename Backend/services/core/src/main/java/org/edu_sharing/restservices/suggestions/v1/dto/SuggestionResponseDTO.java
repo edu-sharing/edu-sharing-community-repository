53b3a829e2602fcab65744597ef1443bf7c5c1f0
Sebastian Wollner <wollner@edu-sharing.net>
2025-04-22 11:06:11 +0200

    fix: Suggestion created and modified date are serialized as unix timestamp instead of ISO8601 Dateformat

--- Backend/services/core/src/main/java/org/edu_sharing/restservices/suggestions/v1/dto/SuggestionResponseDTO.java
+++ Backend/services/core/src/main/java/org/edu_sharing/restservices/suggestions/v1/dto/SuggestionResponseDTO.java
@@ -1,36 +1,54 @@
 package org.edu_sharing.restservices.suggestions.v1.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Value;
-import org.edu_sharing.restservices.PersonDao;
-import org.edu_sharing.restservices.shared.User;
 import org.edu_sharing.restservices.shared.UserSimple;
 import org.edu_sharing.service.suggestion.SuggestionStatus;
 import org.edu_sharing.service.suggestion.SuggestionType;
 
-import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SuggestionResponseDTO {
+   @JsonProperty(required = true)
    private String id;
+
+   @JsonProperty(required = true)
    private String nodeId;
+
+   @JsonProperty(required = true)
    private String version;
 
+   @JsonProperty(required = true)
    private String propertyId;
+
+   @JsonProperty(required = true)
    private Object value;
 
+   @JsonProperty(required = true)
    private SuggestionType type;
+
+   @JsonProperty(required = true)
    private SuggestionStatus status;
+
    private String description;
+
+   @JsonProperty(required = true)
    private double confidence = 0;
 
+   @JsonProperty(required = true)
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssX")
    private Date created;
+
+   @JsonProperty(required = true)
    private UserSimple createdBy;
+
+   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssX")
    private Date modified;
    private UserSimple modifiedBy;
 }
