ebeefbd184aed88f84ae2d1dbd823192449d1f2a
Torsten Simon <simon@edu-sharing.net>
2025-05-21 16:23:24 +0200

    fix:cleanup job docs

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/TouchNodesJob.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/jobs/quartz/TouchNodesJob.java
@@ -7,6 +7,7 @@ import org.alfresco.model.ContentModel;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.edu_sharing.repository.server.jobs.quartz.annotation.JobDescription;
 import org.edu_sharing.repository.server.jobs.quartz.annotation.JobFieldDescription;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -14,15 +15,16 @@ import org.quartz.JobExecutionException;
 import java.io.IOException;
 import java.util.Map;
 
+@JobDescription(description = "touch nodes so they get re-indexed by search index, in case they have a wrong state in the index")
 public class TouchNodesJob extends FixElasticSearchBase{
 
-    @JobFieldDescription(description = "either to keep modified date or not")
+    @JobFieldDescription(description = "either to keep modified date or not", sampleValue = "true")
     boolean keepModifiedDate = true;
 
-    @JobFieldDescription(description = "if false (default) no changes will be done.")
+    @JobFieldDescription(description = "if false (default) no changes will be done.", sampleValue = "false")
     boolean execute;
 
-    @JobFieldDescription(description = "query that delivers a result of nodes that have to be checked. optional. if not set all nodes will be searched.", sampleValue = "{\"term\":{\"type\":\"ccm:io\"}}")
+    @JobFieldDescription(description = "query that delivers a result of nodes that have to be checked. optional. if not set all nodes will be searched.", sampleValue = "{\"term\":{\"path\":\"<parent-uuid>\"}}")
     protected String query;
 
     @Override
