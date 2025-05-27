f57d249e2f8abd5e5ace890d06b82d79d3feecc3
Torsten Simon <simon@edu-sharing.net>
2025-05-27 21:40:45 +0200

    fix:test

--- Frontend/projects/edu-sharing-ui/src/lib/services/node-helper.service.spec.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/services/node-helper.service.spec.ts
@@ -12,6 +12,7 @@ describe('Test NodeHelper', () => {
     describe('Test cc license links', () => {
         const underTest = new NodeHelperService(translateServiceMock, null, null, null, null, null);
         it('cc by 4.0', async () => {
+            translateServiceMock.currentLang = 'de';
             expect(await underTest.getLicenseUrlByString('CC_BY', '4.0', '').toPromise()).toBe(
                 'https://creativecommons.org/licenses/by/4.0/deed.de',
             );
@@ -20,6 +21,7 @@ describe('Test NodeHelper', () => {
             );
         });
         it('cc by sa 3.0', async () => {
+            translateServiceMock.currentLang = 'de';
             expect(await underTest.getLicenseUrlByString('CC_BY_SA', '3.0', '').toPromise()).toBe(
                 'https://creativecommons.org/licenses/by-sa/3.0/deed.de',
             );
