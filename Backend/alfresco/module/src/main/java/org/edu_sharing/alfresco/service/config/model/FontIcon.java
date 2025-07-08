924e957a3df518781aaaa2052bfab6495627b5c2
Torsten Simon <simon@metaventis.com>
2025-07-08 22:06:26 +0200

    feat:add scope to esIcon to replace icon types by a per-scope basis

--- Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/service/config/model/FontIcon.java
+++ Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/service/config/model/FontIcon.java
@@ -1,11 +1,15 @@
 package org.edu_sharing.alfresco.service.config.model;
 
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.xml.bind.annotation.XmlElement;
 import java.io.Serializable;
 
 public class FontIcon implements Serializable {
     @XmlElement
     public String original;
+    @JsonPropertyDescription("Context for this icon to replace. When null, all contexts are replaced")
+    @XmlElement
+    public String context;
     @XmlElement public String replace;
     @XmlElement public String cssClass;
 }
