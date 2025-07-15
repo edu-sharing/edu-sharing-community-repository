e7e73d34c331598c590fa961d3f1c48e32592f39
Sebastian Wollner <wollner@edu-sharing.net>
2025-07-15 09:53:55 +0200

    refactor: clean up unused annotations in Application class

--- Backend/transform/src/main/java/org/edu_sharing/alfresco/transformer/Application.java
+++ Backend/transform/src/main/java/org/edu_sharing/alfresco/transformer/Application.java
@@ -4,11 +4,8 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"org.edu_sharing.alfresco.transformer","org.alfresco.transform"})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
