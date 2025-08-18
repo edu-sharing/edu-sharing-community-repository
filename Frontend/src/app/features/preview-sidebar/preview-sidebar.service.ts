1e1afd043b0c9ce9f0eaea4d2e9acc19d10de804
tkubica-edu <kubica@edu-sharing.net>
2025-08-18 17:21:53 +0200

    feat: extend preview-sidebar service by currentNode subject

--- Frontend/src/app/features/preview-sidebar/preview-sidebar.service.ts
+++ Frontend/src/app/features/preview-sidebar/preview-sidebar.service.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Node } from 'ngx-edu-sharing-api';
 import { BehaviorSubject } from 'rxjs';
 import { PreviewSidebarComponent } from './preview-sidebar.component';
 
@@ -11,6 +12,7 @@ import { PreviewSidebarComponent } from './preview-sidebar.component';
     providedIn: 'root',
 })
 export class PreviewSidebarService {
+    private currentNode$ = new BehaviorSubject<Node | null>(null);
     private instance$ = new BehaviorSubject<PreviewSidebarComponent>(null);
 
     registerInstance(instance: PreviewSidebarComponent) {
@@ -22,7 +24,14 @@ export class PreviewSidebarService {
             );
         }
         this.instance$.next(instance);
+        // the currentNode might already be set as input
+        this.currentNode$.next(instance.node);
+        // listen to the closed event
+        this.instance$.value.closed.subscribe(() => {
+            this.currentNode$.next(null);
+        });
     }
+
     unregisterInstance(instance: PreviewSidebarComponent) {
         if (this.instance$.value !== instance) {
             console.error('This sidebar is not registered!', this.instance$.value, instance);
@@ -33,5 +42,17 @@ export class PreviewSidebarService {
 
     getInstance() {
         return this.instance$;
+    }
+
+    getCurrentNode() {
+        return this.currentNode$;
+    }
+
+    handleNodeClick(node: Node) {
+        if (!this.instance$.value) {
+            return;
+        }
+        this.currentNode$.next(this.instance$.value.node !== node ? node : null);
+        this.instance$.value.node = this.currentNode$.value;
     }
 }
