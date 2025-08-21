af3660b88a1d30d05e5059f31638639dabc1a6f4
Sebastian Wollner <wollner@edu-sharing.net>
2025-08-21 14:53:58 +0200

    feat: enable async processing with configurable task executor
    Added an `@EnableAsync` annotation to support asynchronous method execution. Configured a `taskExecutor` bean with customizable thread pool settings. Updated `SpringConfigRoot` to include these enhancements.

--- Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/SpringConfigRoot.java
@@ -3,14 +3,30 @@ package org.edu_sharing.spring;
 import org.edu_sharing.spring.security.basic.SecurityConfigurationBasic;
 import org.edu_sharing.spring.security.openid.SecurityConfigurationOpenIdConnect;
 import org.edu_sharing.spring.security.saml2.SecurityConfigurationSaml;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
+@EnableAsync
 @Configuration
 @Import({SecurityConfigurationSaml.class, SecurityConfigurationOpenIdConnect.class, SecurityConfigurationBasic.class})
 //component scan to enable edu-sharing custom condition annotations
 @ComponentScan(basePackages = {"org.edu_sharing"})
 public class SpringConfigRoot {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(Math.max(2, Runtime.getRuntime().availableProcessors()));
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("TaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
 }
