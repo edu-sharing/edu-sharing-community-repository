150152ceb72cbff612af96e07b512cef22816fb8
Sebastian Wollner <wollner@edu-sharing.net>
2025-09-16 10:58:37 +0200

    fix: is passive login obey the local = true parameter

--- Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SilentLoginModeRedirect.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SilentLoginModeRedirect.java
@@ -14,73 +14,85 @@ public class SilentLoginModeRedirect {
     public static String SESS_ATT_SILENT_LOGIN_TARGET = "SILENT_LOGIN_TARGET";
     public static String SESS_ATT_SILENT_LOGIN_RESULT = "SILENT_LOGIN_RESULT";
 
-    public static boolean process(HttpServletRequest request, HttpServletResponse response) throws Exception{
+    public static boolean process(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        if(!checkConditions(request,response)){
+        if (!checkConditions(request, response)) {
             return false;
         }
 
-        if(request.getServletPath().equals("/rest")){
+        //http://localhost:8080/edu-sharing/components/login?local=true
+        if (isLocalLoginForced(request)) {
             return false;
         }
 
-        String result = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_RESULT);
-        if(result != null) {
+        if (request.getServletPath().equals("/rest")) {
+            return false;
+        }
+
+
+        String result = (String) request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_RESULT);
+        if (result != null) {
             request.getSession().removeAttribute(SESS_ATT_SILENT_LOGIN_RESULT);
             return false;
         }
 
         request.getSession().setAttribute(SESS_ATT_SILENT_LOGIN_TARGET, (request.getContextPath()
-                        + request.getServletPath()
-                        + (request.getPathInfo() != null ? request.getPathInfo() : "")
-                        + (request.getQueryString() != null ? ("?"+ request.getQueryString()) : ""))
+                + request.getServletPath()
+                + (request.getPathInfo() != null ? request.getPathInfo() : "")
+                + (request.getQueryString() != null ? ("?" + request.getQueryString()) : ""))
         );
+
+
         response.sendRedirect(request.getContextPath() + SilentLoginAuthorizationRequestResolver.DEFAULT_SILENT_LOGIN_PATH);
         return true;
     }
 
-    public static boolean processError(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        if(!checkConditions(request,response)){
+    private static boolean isLocalLoginForced(HttpServletRequest request) {
+        return request.getRequestURI().equals(request.getContextPath() + AuthenticationFilter.PATH_LOGIN_ANGULAR)
+                && "true".equalsIgnoreCase(request.getParameter("local"));
+    }
+
+    public static boolean processError(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!checkConditions(request, response)) {
             return false;
         }
-        String target = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
-        if(target == null){
+        String target = (String) request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
+        if (target == null) {
             return false;
         }
-        request.getSession().setAttribute(SESS_ATT_SILENT_LOGIN_RESULT,"login_required");
+        request.getSession().setAttribute(SESS_ATT_SILENT_LOGIN_RESULT, "login_required");
         response.sendRedirect(target);
         return true;
     }
 
-    public static boolean processSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        if(!checkConditions(request,response)){
+    public static boolean processSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!checkConditions(request, response)) {
             return false;
         }
-        String target = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
-        if(target == null){
+        String target = (String) request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
+        if (target == null) {
             return false;
         }
 
-        request.getSession().setAttribute(AuthenticationFilter.LOGIN_SUCCESS_REDIRECT_URL,target);
+        request.getSession().setAttribute(AuthenticationFilter.LOGIN_SUCCESS_REDIRECT_URL, target);
         response.sendRedirect(request.getContextPath() + "/shibboleth");
         return true;
     }
 
-    private static boolean checkConditions(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        if(!HttpMethod.GET.equals(request.getMethod())){
+    private static boolean checkConditions(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!HttpMethod.GET.equals(request.getMethod())) {
             return false;
         }
 
-        if(!ApplicationContextFactory.getApplicationContext().getEnvironment()
-                .acceptsProfiles(Profiles.of(SecurityConfigurationOpenIdConnect.PROFILE_ID))){
+        if (!ApplicationContextFactory.getApplicationContext().getEnvironment()
+                .acceptsProfiles(Profiles.of(SecurityConfigurationOpenIdConnect.PROFILE_ID))) {
             return false;
         }
 
         Config config = ConfigServiceFactory.getCurrentConfig();
-        if(config!=null && !LoginSilentMode.redirect.equals(config.values.loginSilentMode)){
+        if (config != null && !LoginSilentMode.redirect.equals(config.values.loginSilentMode)) {
             return false;
         }
-
 
 
         return true;
