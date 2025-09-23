1060896d7c03b1f06818df0657062ac003de6cae
Torsten Simon <simon@edu-sharing.net>
2025-09-23 11:54:16 +0200

    fix:storybook / move mds rendering parts to viewer service for reusability in ui lib

--- Frontend/projects/edu-sharing-ui/src/lib/mds-viewer/widget/mds-widget.component.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/mds-viewer/widget/mds-widget.component.ts
@@ -42,13 +42,11 @@ export enum MdsType {
 export interface MdsValueList {
     values: Suggestion[];
 }
-export interface MdsViewerWidget {
+export abstract class MdsViewerWidget {
     definition: MdsWidget;
     focusTrigger: Subject<void>;
-    getInitalValuesAsync(): Promise<InitialValues>;
-    getInitialDisplayValues(): BehaviorSubject<MdsValueList>;
-    getBasicType(flat?: boolean): string;
-    getFormattedValue(value: string[], basicType: string, translate: TranslateService): string[];
+    abstract getInitalValuesAsync(): Promise<InitialValues>;
+    abstract getInitialDisplayValues(): BehaviorSubject<MdsValueList>;
 }
 export enum MdsWidgetType {
     Text = 'text',
@@ -187,7 +185,12 @@ export class MdsWidgetComponent implements OnInit, OnDestroy, OnChanges {
             .subscribe(async (value: MdsValueList) => {
                 this.value.set(value.values.map((v) => v.displayString));
             });
-        this.basicType.set(this.widget.getBasicType(this.viewInstance.treeDisplay === 'flat'));
+        this.basicType.set(
+            MdsViewerService.getBasicType(
+                this.getDefinition(),
+                this.viewInstance.treeDisplay === 'flat',
+            ),
+        );
         this.rawValue = await this.getRawValue().toPromise();
     }
 
@@ -216,7 +219,12 @@ export class MdsWidgetComponent implements OnInit, OnDestroy, OnChanges {
         }
         const id = this.getDefinition().id;
         const values = this.getNodeValues();
-        if (this.widget.getBasicType(this.viewInstance.treeDisplay === 'flat') === 'license') {
+        if (
+            MdsViewerService.getBasicType(
+                this.definition,
+                this.viewInstance.treeDisplay === 'flat',
+            ) === 'license'
+        ) {
             this.license$.next({
                 icon: await this.nodeHelper.getLicenseIcon({
                     properties: this.getNodeValues(),
@@ -259,12 +267,14 @@ export class MdsWidgetComponent implements OnInit, OnDestroy, OnChanges {
 
     click() {
         if (this.getDefinition().link === '_BLANK') {
-            window.open(this.widget.getFormattedValue(this.value(), 'text', this.translate)[0]);
+            window.open(
+                this.mdsViewerService.getFormattedValue(this.value(), this.definition, 'text')[0],
+            );
         } else if (this.getDefinition().link === '_SELF') {
-            window.location.href = this.widget.getFormattedValue(
+            window.location.href = this.mdsViewerService.getFormattedValue(
                 this.value(),
+                this.definition,
                 'text',
-                this.translate,
             )[0];
         } else {
             console.warn('Unsupported link type ' + this.getDefinition().link);
@@ -272,10 +282,13 @@ export class MdsWidgetComponent implements OnInit, OnDestroy, OnChanges {
     }
 
     formatValue(): string[] {
-        return this.widget.getFormattedValue(
+        return this.mdsViewerService.getFormattedValue(
             this.value(),
-            this.widget.getBasicType(this.viewInstance.treeDisplay === 'flat'),
-            this.translate,
+            this.widget.definition,
+            MdsViewerService.getBasicType(
+                this.widget.definition,
+                this.viewInstance.treeDisplay === 'flat',
+            ),
         );
     }
 
