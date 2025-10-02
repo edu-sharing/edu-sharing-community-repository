2f35c12d5069cc8bed211a81ee43a06a2b38b8b6
Daniel Rudolph <rudolph@edu-sharing.net>
2025-10-02 10:13:15 +0200

    fix: openid: SilentLoginModeRedirect add ebug options

--- Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SilentLoginModeRedirect.java
+++ Backend/services/core/src/main/java/org/edu_sharing/spring/security/openid/SilentLoginModeRedirect.java
@@ -3,6 +3,7 @@ package org.edu_sharing.spring.security.openid;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.HttpMethod;
+import lombok.extern.slf4j.Slf4j;
 import org.edu_sharing.alfresco.service.config.model.Config;
 import org.edu_sharing.alfresco.service.config.model.LoginSilentMode;
 import org.edu_sharing.repository.server.authentication.AuthenticationFilter;
@@ -10,6 +11,7 @@ import org.edu_sharing.service.config.ConfigServiceFactory;
 import org.edu_sharing.spring.ApplicationContextFactory;
 import org.springframework.core.env.Profiles;
 
+@Slf4j
 public class SilentLoginModeRedirect {
     public static String SESS_ATT_SILENT_LOGIN_TARGET = "SILENT_LOGIN_TARGET";
     public static String SESS_ATT_SILENT_LOGIN_RESULT = "SILENT_LOGIN_RESULT";
@@ -17,15 +19,18 @@ public class SilentLoginModeRedirect {
     public static boolean process(HttpServletRequest request, HttpServletResponse response) throws Exception{
 
         if(!checkConditions(request,response)){
+            log.debug("conditions not given");
             return false;
         }
 
         if(request.getServletPath().equals("/rest")){
+            log.debug("path is rest");
             return false;
         }
 
         String result = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_RESULT);
         if(result != null) {
+            log.debug(SESS_ATT_SILENT_LOGIN_RESULT + ": "+result);
             request.getSession().removeAttribute(SESS_ATT_SILENT_LOGIN_RESULT);
             return false;
         }
@@ -35,33 +40,40 @@ public class SilentLoginModeRedirect {
                         + (request.getPathInfo() != null ? request.getPathInfo() : "")
                         + (request.getQueryString() != null ? ("?"+ request.getQueryString()) : ""))
         );
+        log.debug("SILENT_LOGIN_TARGET:"+request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET));
         response.sendRedirect(request.getContextPath() + SilentLoginAuthorizationRequestResolver.DEFAULT_SILENT_LOGIN_PATH);
         return true;
     }
 
     public static boolean processError(HttpServletRequest request, HttpServletResponse response) throws Exception{
         if(!checkConditions(request,response)){
+            log.debug("processError: conditions not given");
             return false;
         }
         String target = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
         if(target == null){
+            log.debug("processError: missing {} in session attributes",SESS_ATT_SILENT_LOGIN_TARGET);
             return false;
         }
         request.getSession().setAttribute(SESS_ATT_SILENT_LOGIN_RESULT,"login_required");
+        log.debug("processError: redirecting to "+target);
         response.sendRedirect(target);
         return true;
     }
 
     public static boolean processSuccess(HttpServletRequest request, HttpServletResponse response) throws Exception{
         if(!checkConditions(request,response)){
+            log.debug("processSuccess: conditions not given");
             return false;
         }
         String target = (String)request.getSession().getAttribute(SESS_ATT_SILENT_LOGIN_TARGET);
         if(target == null){
+            log.debug("processSuccess: missing {}", SESS_ATT_SILENT_LOGIN_TARGET);
             return false;
         }
 
         request.getSession().setAttribute(AuthenticationFilter.LOGIN_SUCCESS_REDIRECT_URL,target);
+        log.debug("processSuccess: set LOGIN_SUCCESS_REDIRECT_URL to {} and redirecting to /shibboleth", target);
         response.sendRedirect(request.getContextPath() + "/shibboleth");
         return true;
     }
@@ -78,11 +90,9 @@ public class SilentLoginModeRedirect {
 
         Config config = ConfigServiceFactory.getCurrentConfig();
         if(config!=null && !LoginSilentMode.redirect.equals(config.values.loginSilentMode)){
+            log.debug("checkConditions: LoginSilentMode.redirect not available for current config");
             return false;
         }
-
-
-
         return true;
     }
 }
