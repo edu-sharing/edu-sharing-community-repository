c9aef0b88ce4cd62d53f91e518fa9bb10ad5ae8b
Sebastian Wollner <wollner@edu-sharing.net>
2025-10-16 16:02:00 +0200

    feat: add organization assignment migration routine for Release 11.0
    Introduced `Release_11_0_OrganizationAssignment` update routine to migrate materials by assigning the organization of the current owner. Utilized `NodeRunner` for threaded processing, ensuring system-level execution and efficient node handling. Includes logging for processed nodes and error scenarios.

---
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/update/Release_11_0_OrganizationAssignment.java
@@ -0,0 +1,81 @@
+package org.edu_sharing.repository.update;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.alfresco.model.ContentModel;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.security.OwnableService;
+import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.namespace.QName;
+import org.edu_sharing.alfresco.service.ConnectionDBAlfresco;
+import org.edu_sharing.repository.client.rpc.Share;
+import org.edu_sharing.repository.client.tools.CCConstants;
+import org.edu_sharing.repository.server.jobs.helper.NodeRunner;
+import org.edu_sharing.repository.server.update.UpdateRoutine;
+import org.edu_sharing.repository.server.update.UpdateService;
+import org.edu_sharing.service.nodeservice.RecurseMode;
+import org.edu_sharing.service.organization.OrganizationService;
+import org.edu_sharing.service.share.ShareType;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+
+@Slf4j
+@UpdateService
+@RequiredArgsConstructor
+public class Release_11_0_OrganizationAssignment {
+
+
+    private final NodeService nodeService;
+    private final OwnableService ownableService;
+    private final OrganizationService organizationService;
+    private final PersonService personService;
+
+    @UpdateRoutine(
+            id = "Release_11_0_OrganizationAssignment",
+            description = "Migrate all materials by assigning the organization of the current owner",
+            order = 0
+    )
+    public void execute() {
+        NodeRunner runner = new NodeRunner();
+        runner.setRunAsSystem(true);
+        runner.setTypes(List.of(CCConstants.CCM_TYPE_IO));
+        runner.setThreaded(true);
+        runner.setTransaction(NodeRunner.TransactionMode.LocalRetrying);
+        runner.setKeepModifiedDate(true);
+        runner.setRecurseMode(RecurseMode.All);
+        runner.setFilter(nodeService::exists);
+        runner.setTask(nodeRef -> {
+            log.info("Processing {}", nodeRef);
+
+            String currentUser = ownableService.getOwner(nodeRef);
+            if(currentUser == null) {
+                currentUser = (String) nodeService.getProperty(nodeRef, ContentModel.PROP_CREATOR);
+            }
+
+            if(!personService.personExists(currentUser)) {
+                log.warn("Person {} does not exist", currentUser);
+                return;
+            }
+
+            Set<String> organisations = organizationService.getAssignedOrganisations(currentUser);
+            if (organisations.isEmpty()) {
+                log.warn("Person {} has no assigned organisations", currentUser);
+                return;
+            }
+
+            nodeService.addProperties(nodeRef, Map.of(QName.createQName(CCConstants.CCM_PROP_OWNING_ORGANIZATION), (Serializable) organisations.stream().toList()));
+            log.info("Assigned organization {} to node {}", organisations, nodeRef);
+        });
+
+        log.info("Starting organization assignment");
+        int processed = runner.run();
+        log.info("Organization assignment for all nodes updated. Processed: {}", processed);
+    }
+}
