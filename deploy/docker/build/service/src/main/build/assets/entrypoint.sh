d8e2aebbd8a9a7d62f78ba07252a7de65b84c7cb
Daniel Rudolph <rudolph@edu-sharing.net>
2025-10-16 19:20:43 +0200

    fix: session invalidates must be broadcasted

--- deploy/docker/build/service/src/main/build/assets/entrypoint.sh
+++ deploy/docker/build/service/src/main/build/assets/entrypoint.sh
@@ -303,7 +303,7 @@ xmlstarlet ed -L \
     -i '$redis' -t attr -n "configPath" -v "tomcat/conf/redisson.yaml" \
     -i '$redis' -t attr -n "readMode" -v "REDIS" \
     -i '$redis' -t attr -n "updateMode" -v "DEFAULT" \
-    -i '$redis' -t attr -n "broadcastSessionEvents" -v "false" \
+    -i '$redis' -t attr -n "broadcastSessionEvents" -v "true" \
     -i '$redis' -t attr -n "broadcastSessionUpdates" -v "false" \
     ${catCConf}
 
