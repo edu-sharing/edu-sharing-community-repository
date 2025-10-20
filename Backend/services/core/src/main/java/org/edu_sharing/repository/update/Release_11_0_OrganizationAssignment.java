3d2a97453299bca1900704ab8021d87d37b14e2f
Sebastian Wollner <wollner@edu-sharing.net>
2025-10-20 15:28:12 +0200

    refactor: simplify `Release_11_0_OrganizationAssignment` by removing unused imports and improving `NodeRunner` configuration
    Streamlined class by eliminating unnecessary imports and adjusting `NodeRunner` settings—disabled threading and transaction retrying for consistent behavior. Commented out the node existence check, added organization property validation, and refined logging for enhanced clarity.

--- Backend/services/core/src/main/java/org/edu_sharing/repository/update/Release_11_0_OrganizationAssignment.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/update/Release_11_0_OrganizationAssignment.java
@@ -4,26 +4,18 @@ package org.edu_sharing.repository.update;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.alfresco.model.ContentModel;
-import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.repository.StoreRef;
 import org.alfresco.service.cmr.security.OwnableService;
 import org.alfresco.service.cmr.security.PersonService;
 import org.alfresco.service.namespace.QName;
-import org.edu_sharing.alfresco.service.ConnectionDBAlfresco;
-import org.edu_sharing.repository.client.rpc.Share;
 import org.edu_sharing.repository.client.tools.CCConstants;
 import org.edu_sharing.repository.server.jobs.helper.NodeRunner;
 import org.edu_sharing.repository.server.update.UpdateRoutine;
 import org.edu_sharing.repository.server.update.UpdateService;
 import org.edu_sharing.service.nodeservice.RecurseMode;
 import org.edu_sharing.service.organization.OrganizationService;
-import org.edu_sharing.service.share.ShareType;
 
 import java.io.Serializable;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.*;
 
 @Slf4j
@@ -46,11 +38,11 @@ public class Release_11_0_OrganizationAssignment {
         NodeRunner runner = new NodeRunner();
         runner.setRunAsSystem(true);
         runner.setTypes(List.of(CCConstants.CCM_TYPE_IO));
-        runner.setThreaded(true);
-        runner.setTransaction(NodeRunner.TransactionMode.LocalRetrying);
+        runner.setThreaded(false);
+        runner.setTransaction(NodeRunner.TransactionMode.Local);
         runner.setKeepModifiedDate(true);
         runner.setRecurseMode(RecurseMode.All);
-        runner.setFilter(nodeService::exists);
+//        runner.setFilter(nodeService::exists);
         runner.setTask(nodeRef -> {
             log.info("Processing {}", nodeRef);
 
@@ -64,15 +56,23 @@ public class Release_11_0_OrganizationAssignment {
                 return;
             }
 
+            QName organizationQName = QName.createQName(CCConstants.CCM_PROP_OWNING_ORGANIZATION);
+            List<String> knownOrganizations = (List<String>)nodeService.getProperty(nodeRef, organizationQName);
+            if(knownOrganizations != null && !knownOrganizations.isEmpty()) {
+                log.info("Node {} already has assigned organizations {}", nodeRef, knownOrganizations);
+                return;
+            }
+
             Set<String> organisations = organizationService.getAssignedOrganisations(currentUser);
             if (organisations.isEmpty()) {
                 log.warn("Person {} has no assigned organisations", currentUser);
                 return;
             }
 
-            nodeService.addProperties(nodeRef, Map.of(QName.createQName(CCConstants.CCM_PROP_OWNING_ORGANIZATION), (Serializable) organisations.stream().toList()));
+           nodeService.addProperties(nodeRef, Map.of(organizationQName, (Serializable) organisations.stream().toList()));
             log.info("Assigned organization {} to node {}", organisations, nodeRef);
         });
+
 
         log.info("Starting organization assignment");
         int processed = runner.run();
