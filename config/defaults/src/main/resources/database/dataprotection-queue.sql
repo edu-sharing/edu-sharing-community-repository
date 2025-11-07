0f8541fd319a5355290866c4c533da050e8b67f7
Sebastian Wollner <wollner@edu-sharing.net>
2025-11-07 11:35:07 +0100

    chore: remove `edu_dataprotection_queue` table definition from SQL scripts
    Deleted the unused `edu_dataprotection_queue` table schema to clean up database configuration files.

--- config/defaults/src/main/resources/database/dataprotection-queue.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE edu_dataprotection_queue
-(
-    "user"  varchar(100) not null UNIQUE,
-    status  varchar(100) not null,
-    requested  timestamp not null,
-    node_id    varchar(36),
-    finished  timestamp
-);
