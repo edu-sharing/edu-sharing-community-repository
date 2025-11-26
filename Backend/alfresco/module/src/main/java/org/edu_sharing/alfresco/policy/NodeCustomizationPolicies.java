de3910ee25cf00ec9aa39559b06f7b0b5b722c57
Torsten Simon <simon@edu-sharing.net>
2025-11-26 08:31:42 +0100

    fix:nlp test

--- Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/policy/NodeCustomizationPolicies.java
+++ Backend/alfresco/module/src/main/java/org/edu_sharing/alfresco/policy/NodeCustomizationPolicies.java
@@ -416,7 +416,7 @@ public class NodeCustomizationPolicies implements OnContentUpdatePolicy, OnCreat
             InputStream inputStream = reader.getContentInputStream();
             MediaType mediaType = getMediaType(filename, inputStream);
             // we allow the text content for the repository config object
-            if(filename.equals(CCConstants.CCM_VALUE_IO_NAME_CONFIG_NODE_NAME) && mediaType.equals(MediaType.TEXT_PLAIN)) {
+            if(Objects.equals(filename, CCConstants.CCM_VALUE_IO_NAME_CONFIG_NODE_NAME) && mediaType.equals(MediaType.TEXT_PLAIN)) {
                 return;
             }
             if (mediaType.equals(MediaType.OCTET_STREAM) && !allowUnknownMimetypes) {
