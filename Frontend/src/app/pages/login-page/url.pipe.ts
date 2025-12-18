ce1bda769cb3a6e14d9e14cb762139682e155372
Torsten Simon <simon@edu-sharing.net>
2025-12-18 16:30:22 +0100

    fix:url pipe allow without http

--- Frontend/src/app/pages/login-page/url.pipe.ts
+++ Frontend/src/app/pages/login-page/url.pipe.ts
@@ -3,6 +3,9 @@ import { PipeTransform, Pipe } from '@angular/core';
 @Pipe({ name: 'esUrl' })
 export class UrlPipe implements PipeTransform {
     transform(value: any, args: any): any {
+        if (!value.startsWith('http://' && !value.endsWith('https://'))) {
+            value = 'https://' + value;
+        }
         let url = new URL(value);
         if (args['mode'] == 'domain') {
             return url.host;
