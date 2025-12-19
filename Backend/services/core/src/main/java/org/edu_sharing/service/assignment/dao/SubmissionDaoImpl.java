8a4715c6d5a8d5eec61ba73d65d07254431f9a2a
Torsten Simon <simon@edu-sharing.net>
2025-12-19 14:03:06 +0100

    fix:create submission run as system cause of missing permissions

--- Backend/services/core/src/main/java/org/edu_sharing/service/assignment/dao/SubmissionDaoImpl.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/assignment/dao/SubmissionDaoImpl.java
@@ -208,6 +208,7 @@ final class SubmissionDaoImpl extends BasicNodeDaoImpl implements SubmissionDao 
     @Override
     @RetryingTransaction // node does not exists after return, because rollback is performed for no reason
     @PreAuthorize("hasPermission(#root.this.getNodeId(), T(org.edu_sharing.repository.client.tools.CCConstants).PERMISSION_ASSIGNEE)")
+    @RunAsSystem
     public SubmissionFileDao createSubmissionFile(SubmissionFileRequest submissionFileRequest, InputStream fileInputStream, FormDataContentDisposition fileMetaData) {
         submissionFileRefs.invalidate();
         validateAssigneeCanChangeSubmission();
