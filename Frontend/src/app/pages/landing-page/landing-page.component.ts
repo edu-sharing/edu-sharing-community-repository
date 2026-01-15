55e1211800fc0ee93d76df327bfe40edfae46cce
tkubica-edu <kubica@edu-sharing.net>
2026-01-15 13:43:28 +0100

    feat: add search in media library to landing page

--- Frontend/src/app/pages/landing-page/landing-page.component.ts
+++ Frontend/src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { AuthenticationService, ConfigService } from 'ngx-edu-sharing-api';
-import { firstValueFrom } from 'rxjs';
-import { RestConstants } from '../../core-module/rest/rest-constants';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { UIService } from 'ngx-edu-sharing-ui';
+import { AuthenticationService, ConfigService } from 'ngx-edu-sharing-api';
+import { UIConstants, UIService } from 'ngx-edu-sharing-ui';
+import { firstValueFrom, Observable, Subject } from 'rxjs';
+import { distinctUntilChanged, filter, startWith, takeUntil } from 'rxjs/operators';
+import { RestConstants } from '../../core-module/rest/rest-constants';
 import { MainNavService } from '../../main/navigation/main-nav.service';
+import {
+    SearchEvent,
+    SearchFieldService,
+} from '../../main/navigation/search-field/search-field.service';
 
 export type SwimlaneTypes =
     | 'featured-media'
@@ -22,18 +27,24 @@ export type SwimlaneEntry = {
     styleUrls: ['landing-page.component.scss'],
     standalone: false,
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
+    private readonly destroyed$ = new Subject<void>();
+    readonly i18nPrefix: string = 'LANDING_PAGE.';
+    searchEvent$: Observable<SearchEvent>;
+
     /**
      * displayed swimlanes (in order)
      * are be retrieved from the backend client.config
      */
     swimlanes = signal<SwimlaneEntry[]>([]);
+
     constructor(
-        private router: Router,
-        private mainNav: MainNavService,
-        private configService: ConfigService,
-        private ui: UIService,
         private authenticationService: AuthenticationService,
+        private configService: ConfigService,
+        private mainNav: MainNavService,
+        private router: Router,
+        private searchFieldService: SearchFieldService,
+        private ui: UIService,
     ) {
         this.mainNav.setMainNavConfig({
             showUser: true,
@@ -47,6 +58,45 @@ export class LandingPageComponent implements OnInit {
             },
             showNavigation: true,
         });
+        // enable the search field and observe the search event
+        this.searchFieldService.enable(
+            {
+                enableFiltersAndSuggestions: false,
+                showFiltersButton: false,
+                placeholder: this.i18nPrefix + 'SEARCH_PLACEHOLDER',
+            },
+            this.destroyed$,
+        );
+        this.searchFieldService
+            .observeCurrentInstance()
+            .pipe(
+                takeUntil(this.destroyed$),
+                filter((i) => !!i),
+            )
+            .subscribe((instance) => {
+                this.searchEvent$ = instance.onSearchTriggered();
+                this.searchEvent$
+                    .pipe(
+                        startWith({
+                            searchString: this.searchFieldService
+                                .getCurrentInstance()
+                                ?.getSearchString(),
+                            cleared: false,
+                        }),
+                        distinctUntilChanged(),
+                    )
+                    .subscribe((event) => {
+                        if (!event.searchString) {
+                            return;
+                        }
+                        const params = {
+                            q: event.searchString,
+                        };
+                        void this.router.navigate([UIConstants.ROUTER_PREFIX + 'search'], {
+                            queryParams: params,
+                        });
+                    });
+            });
     }
 
     async ngOnInit(): Promise<void> {
@@ -79,5 +129,13 @@ export class LandingPageComponent implements OnInit {
                 },
             ]),
         );
+    }
+
+    /**
+     * On destruction, complete the subjects.
+     */
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+        this.destroyed$.complete();
     }
 }
