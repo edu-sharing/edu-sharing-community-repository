fbf6f75b796b95738fbc126c54788654fc88f0cd
Torsten Simon <simon@edu-sharing.net>
2026-04-22 10:23:15 +0200

    fix:assignment status

--- Frontend/projects/edu-sharing-ui/src/lib/list-items/list-node-assignment/list-node-assignment.component.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/list-items/list-node-assignment/list-node-assignment.component.ts
@@ -40,6 +40,13 @@ export class ListNodeAssignmentComponent extends ListWidget {
     }
 
     getValidationStatus(node: SubmissionWithAssignment) {
+        if (
+            true ||
+            node.assignment?.status === 'CORRECTED' ||
+            node.assignment?.status === 'FINISHED'
+        ) {
+            return 'SENDBACK';
+        }
         if (node.validationStatus === 'FINISHED') {
             return 'CORRECTED';
         }
