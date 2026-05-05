be2b97437d7cc2c3781495bddedb70cce1dc2c54
Sebastian Wollner <wollner@edu-sharing.net>
2026-05-05 10:12:27 +0200

    feat: add timestamp support to share creation logic
    - Updated `ShareInfoService` and `ShareInfoServiceImpl` to include `Date` parameter in `createShare` method.
    - Modified `Release_11_0_ShareInfos` to pass modification timestamps when creating shares.
    - Adjusted `ShareInfoMapper` to ensure consistent handling of share creation dates.

--- Backend/services/core/src/main/java/org/edu_sharing/service/share/ShareInfoService.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/share/ShareInfoService.java
@@ -2,14 +2,17 @@ package org.edu_sharing.service.share;
 
 import lombok.NonNull;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ShareInfoService {
 
-    @Transactional
     void createShare(@NonNull String nodeId, @NonNull String sharedBy, @NonNull String sharedWith, @NonNull ShareType shareType);
+
+    void createShare(@NotNull String nodeId, @NotNull String sharedBy, @NotNull String sharedWith, @NotNull ShareType shareType, @NotNull Date date);
 
     void rejectShare(@NonNull String nodeId);
 
