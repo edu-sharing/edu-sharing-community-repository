4bdec16865f58288895286afb07247d8dd4fe9d5
Daniel Rudolph <rudolph@edu-sharing.net>
2026-05-28 08:39:49 +0200

    fix: filter ccm:page_variant for subcollection count

--- Backend/alfresco/common/src/main/java/org/edu_sharing/repository/client/tools/CCConstants.java
+++ Backend/alfresco/common/src/main/java/org/edu_sharing/repository/client/tools/CCConstants.java
@@ -488,6 +488,7 @@ public class CCConstants {
 
 	public final static String CCM_ASPECT_WIDGET = "{http://www.campuscontent.de/model/1.0}widget";
 	public final static String CCM_ASPECT_PAGE = "{http://www.campuscontent.de/model/1.0}page";
+    public final static String CCM_ASPECT_PAGE_VARIANT = "{http://www.campuscontent.de/model/1.0}page_variant";
 
 	public final static String CCM_PROP_TOOL_OBJECT_TOOLINSTANCEREF = "{http://www.campuscontent.de/model/1.0}tool_instance_ref";
 
