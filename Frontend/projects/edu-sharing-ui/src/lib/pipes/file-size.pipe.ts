0d90d2e8330f70ec0bbb646c62e5efa968b161cd
Benedikt Reuter <reuter@edu-sharing.net>
2026-06-05 11:33:16 +0200

    fix: fix pipe not providedIn root for proper injection

--- Frontend/projects/edu-sharing-ui/src/lib/pipes/file-size.pipe.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/pipes/file-size.pipe.ts
@@ -1,5 +1,6 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 
+@Injectable({ providedIn: 'root' })
 @Pipe({
     name: 'formatSize',
     standalone: false,
