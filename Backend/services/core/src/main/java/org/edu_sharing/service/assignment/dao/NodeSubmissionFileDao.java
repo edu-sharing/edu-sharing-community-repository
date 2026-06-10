1685e1909d4245a9ba1a6548e0a0579fd0fd1075
Sebastian Wollner <wollner@edu-sharing.net>
2026-06-10 14:35:13 +0200

    fix: wrap `updateNodeNative` in `runAsSystem` and correct `getNodeId` usage in validation logic
    - Updated `updateNodeNative` to run in a `runAsSystem` context for elevated permissions.
    - Fixed incorrect use of `getCorrectionNodeId` by replacing it with `getNodeId` in validation status checks.

--- Backend/services/core/src/main/java/org/edu_sharing/service/assignment/dao/NodeSubmissionFileDao.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/assignment/dao/NodeSubmissionFileDao.java
@@ -268,8 +268,10 @@ final class NodeSubmissionFileDao extends BasicNodeDaoImpl implements Submission
         submissionDao.validateCanCoordinatorChangeSubmission();
 
         refresh();
-
-        nodeService.updateNodeNative(nodeId, Map.of(CCConstants.CCM_PROP_SUBMISSION_VALIDATION_STATUS, validationStatus.name()));
+        AuthenticationUtil.runAsSystem(() -> {
+            nodeService.updateNodeNative(nodeId, Map.of(CCConstants.CCM_PROP_SUBMISSION_VALIDATION_STATUS, validationStatus.name()));
+            return null;
+        });
         refresh();
     }
 
@@ -311,7 +313,7 @@ final class NodeSubmissionFileDao extends BasicNodeDaoImpl implements Submission
 
     @Override
     public Submission.Status getValidationStatus() {
-        if (AssignmentUtil.isAssignmentCoordinator(permissionService, getCorrectionNodeId())) {
+        if (AssignmentUtil.isAssignmentCoordinator(permissionService, getNodeId())) {
             return propertyMapper.get().getEnum(CCConstants.CCM_PROP_SUBMISSION_VALIDATION_STATUS, Submission.Status.class);
         } else {
             return null;
