22962809ab34e2522abb6836db88223eafe2af6d
Torsten Simon <simon@edu-sharing.net>
2026-06-15 12:50:00 +0200

    fix:allow pipe injection for duration

--- Frontend/projects/edu-sharing-ui/src/lib/list-items/format-duration.pipe.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/list-items/format-duration.pipe.ts
@@ -1,6 +1,7 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
 import { DurationFormat, DurationHelper } from '../util/duration-helper';
 
+@Injectable({ providedIn: 'root' })
 @Pipe({
     name: 'formatDuration',
     standalone: false,
