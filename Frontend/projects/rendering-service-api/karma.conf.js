cdee4b9aba3add19963fe3a87bff016bd2b2207b
Torsten Simon <simon@edu-sharing.net>
2026-06-16 14:44:16 +0200

    build:generate rendering-service-api client

--- Frontend/projects/rendering-service-api/karma.conf.js
+++ Frontend/projects/rendering-service-api/karma.conf.js
@@ -8,9 +8,9 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
+            require('karma-verbose-reporter'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage'),
-            require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             jasmine: {
