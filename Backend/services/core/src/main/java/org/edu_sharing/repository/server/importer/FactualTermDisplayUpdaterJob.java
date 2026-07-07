7a372b3cf11c94ff1857777707ceded44cbb2f01
Daniel Rudolph <rudolph@edu-sharing.net>
2026-07-07 12:04:46 +0200

    fix: FactualTermElasticUpdater log

--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermDisplayUpdaterJob.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermDisplayUpdaterJob.java
@@ -1,6 +1,7 @@
 package org.edu_sharing.repository.server.importer;
 
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.edu_sharing.repository.server.jobs.quartz.AbstractInterruptableJob;
 import org.edu_sharing.repository.server.jobs.quartz.AbstractJobMapAnnotationParams;
 import org.edu_sharing.repository.server.jobs.quartz.annotation.JobDescription;
 import org.quartz.JobExecutionContext;
@@ -11,13 +12,14 @@ import java.util.List;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @JobDescription(description = "resolves nodes with changed factualTerms and touch nodes so they get re-indexed by search index")
-public class FactualTermDisplayUpdaterJob extends AbstractJobMapAnnotationParams {
+public class FactualTermDisplayUpdaterJob extends AbstractInterruptableJob {
 
     @Autowired
     FactualTermElasticUpdater factualTermElasticUpdater;
 
+
     @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    protected void executeInterruptable(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
             AuthenticationUtil.runAsSystem(() ->{
                 PersistenHandlerKeywordsDNBMarc ph = new PersistenHandlerKeywordsDNBMarc();
--- Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermElasticUpdater.java
+++ Backend/services/core/src/main/java/org/edu_sharing/repository/server/importer/FactualTermElasticUpdater.java
@@ -1,6 +1,7 @@
 package org.edu_sharing.repository.server.importer;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.StoreRef;
 import org.edu_sharing.alfresco.service.search.CMISSearchHelper;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class FactualTermElasticUpdater {
@@ -22,6 +24,7 @@ public class FactualTermElasticUpdater {
         Map<String,Object> filter = new HashMap<>();
         filter.put(CCConstants.CCM_PROP_IO_REPL_CLASSIFICATION_KEYWORD, factualTermIdent);
         List<NodeRef> nodeRefs = CMISSearchHelper.fetchNodesByTypeAndFilters(CCConstants.CCM_TYPE_IO,filter, StoreRef.STORE_REF_WORKSPACE_SPACESSTORE);
+        log.info("Found {} nodes to touch: {}", nodeRefs.size(),String.join(", ", nodeRefs.stream().map(NodeRef::getId).toList()));
         nodeRefs.forEach(nodeRef -> nodeService.touch(nodeRef.getId(), true));
     }
 }
