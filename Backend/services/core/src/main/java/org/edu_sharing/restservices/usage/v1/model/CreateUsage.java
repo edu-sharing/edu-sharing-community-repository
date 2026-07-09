cfa24e7b2e2dd36803464057f55be816e9d1f0e6
Sebastian Wollner <wollner@edu-sharing.net>
2026-07-09 15:20:10 +0200

    fix: Ignore unknown fields because newer Moodle versions send courseTitle, which is not supported in 9.1 and causes requests to fail.

--- Backend/services/core/src/main/java/org/edu_sharing/restservices/usage/v1/model/CreateUsage.java
+++ Backend/services/core/src/main/java/org/edu_sharing/restservices/usage/v1/model/CreateUsage.java
@@ -1,7 +1,11 @@
 package org.edu_sharing.restservices.usage.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateUsage {
     @JsonProperty
     public String appId;
