c9728fd31d1af5deb71cfbe0b6d2b0fc7e71c467
Torsten Simon <simon@edu-sharing.net>
2026-07-22 13:32:27 +0200

    fix:tp check for mz based collections, reset to null (cherrypick of 11b089aa5a0ced107fc3a1853e5d3bbf4d290674)

--- Backend/services/core/src/main/java/org/edu_sharing/service/collection/CollectionServiceImpl.java
+++ Backend/services/core/src/main/java/org/edu_sharing/service/collection/CollectionServiceImpl.java
@@ -74,6 +74,7 @@ import java.util.stream.Collectors;
 public class CollectionServiceImpl implements CollectionService {
 
     private NotificationService notificationService;
+    private AuthorityService localAuthorityService;
 
     public static CollectionService build(String appId) {
         CollectionServiceConfig config = (CollectionServiceConfig) ApplicationContextFactory.getApplicationContext().getBean("collectionServiceConfig");
@@ -146,6 +147,7 @@ public class CollectionServiceImpl implements CollectionService {
             this.toolPermissionService = ToolPermissionServiceFactory.getInstance();
             this.permissionService = PermissionServiceFactory.getPermissionService(appId);
             this.notificationService = NotificationServiceFactoryUtility.getNotificationService(appId);
+            this.localAuthorityService = AuthorityServiceFactory.getAuthorityService(appId);
             ApplicationContext appContext = AlfAppContextGate.getApplicationContext();
             policyBehaviourFilter = (BehaviourFilter) appContext.getBean("policyBehaviourFilter");
 
@@ -436,6 +438,7 @@ public class CollectionServiceImpl implements CollectionService {
         final String fcurrentUsername = currentUsername;
 
         if (fcurrentUsername != null) {
+            Map<String, Object> props = asProps(collection);
             return AuthenticationUtil.runAsSystem(new RunAsWork<Collection>() {
 
                 @Override
@@ -448,7 +451,6 @@ public class CollectionServiceImpl implements CollectionService {
                         parentIdLocal = getHomePath();
                     }
 
-                    Map<String, Object> props = asProps(collection);
                     try {
                         new DuplicateFinder().transformToSafeName(client.getChildren(parentIdLocal), props);
                     } catch (Throwable e) {
@@ -639,8 +641,28 @@ public class CollectionServiceImpl implements CollectionService {
         props.put(CCConstants.CCM_PROP_MAP_COLLECTIONVIEWTYPE, collection.getViewtype());
         props.put(CCConstants.CCM_PROP_MAP_COLLECTIONLEVEL0, collection.isLevel0());
         if (collection.getAuthorFreetext() != null && !collection.getAuthorFreetext().isEmpty()) {
-            ToolPermissionHelper.throwIfToolpermissionMissing(CCConstants.CCM_VALUE_TOOLPERMISSION_COLLECTION_CHANGE_OWNER);
-            props.put(CCConstants.CCM_PROP_MAP_COLLECTION_AUTHOR_FREETEXT, collection.getAuthorFreetext());
+            if (toolPermissionService.hasToolPermission(CCConstants.CCM_VALUE_TOOLPERMISSION_COLLECTION_CHANGE_OWNER)) {
+                props.put(CCConstants.CCM_PROP_MAP_COLLECTION_AUTHOR_FREETEXT, collection.getAuthorFreetext());
+            } else {
+                if (Objects.equals(collection.getType(), CCConstants.COLLECTIONTYPE_MEDIA_CENTER)) {
+                    // force use the displayname of the first mediacenter the user is admin of
+                    try {
+                        String mediacenter = searchService.getAllMediacenters().stream()
+                                .filter(localAuthorityService::hasAdminAccessToMediacenter)
+                                .findFirst()
+                                .orElseThrow(() -> new IllegalArgumentException("Current user is not admin of any mediacenter"));
+                        String displayName = (String) localAuthorityService.getAuthorityProperty(mediacenter,
+                                CCConstants.CM_PROP_AUTHORITY_AUTHORITYDISPLAYNAME);
+                        props.put(CCConstants.CCM_PROP_MAP_COLLECTION_AUTHOR_FREETEXT, displayName);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                } else {
+                    ToolPermissionHelper.throwIfToolpermissionMissing(CCConstants.CCM_VALUE_TOOLPERMISSION_COLLECTION_CHANGE_OWNER);
+                }
+            }
+        } else {
+            props.put(CCConstants.CCM_PROP_MAP_COLLECTION_AUTHOR_FREETEXT, null);
         }
         return props;
     }
