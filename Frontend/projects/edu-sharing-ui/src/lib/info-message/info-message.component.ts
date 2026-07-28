9266e90fa6ba4ad0dc0bee2be7fe93cde59f3497
tkubica-edu <kubica@edu-sharing.net>
2026-07-28 09:53:39 +0200

    feat: make es-info-message optionally closable so the metadata hint can be dismissed on small screens

--- Frontend/projects/edu-sharing-ui/src/lib/info-message/info-message.component.ts
+++ Frontend/projects/edu-sharing-ui/src/lib/info-message/info-message.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { TranslateModule } from '@ngx-translate/core';
 import { IconDirective } from '../directives/icon.directive';
 
@@ -6,7 +7,7 @@ import { IconDirective } from '../directives/icon.directive';
     selector: 'es-info-message',
     templateUrl: 'info-message.component.html',
     styleUrls: ['info-message.component.scss'],
-    imports: [TranslateModule, IconDirective],
+    imports: [TranslateModule, IconDirective, MatButtonModule],
 })
 export class InfoMessageComponent {
     /**
@@ -14,6 +15,10 @@ export class InfoMessageComponent {
      */
     @Input() message: string;
     @Input() mode: 'info' | 'warning' | 'error' = 'info';
+    /** renders a close button that lets the user hide the message */
+    @Input() closable: boolean = false;
+    /** set by the close button, hides the whole message */
+    protected dismissed = signal(false);
 
     ICONS: any = {
         info: 'info',
