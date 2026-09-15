9fee5a65f3732a0ce13160318c5ad1b034a91f98
Benedikt Reuter <reuter@edu-sharing.net>
2026-09-15 08:11:00 +0200

    fix: add aria-label to QR code images in share and node dialogs

--- Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.module.ts
+++ Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { QRCodeComponent } from 'angularx-qrcode';
 import { QrDialogComponent } from './qr-dialog.component';
 import { QrContentComponent } from './qr-content/qr-content.component';
@@ -8,7 +9,7 @@ export { QrDialogComponent };
 
 @NgModule({
     declarations: [QrDialogComponent, QrContentComponent],
-    imports: [CommonModule, QRCodeComponent],
+    imports: [CommonModule, QRCodeComponent, TranslateModule],
     exports: [
         // Export for embed app
         QrContentComponent,
