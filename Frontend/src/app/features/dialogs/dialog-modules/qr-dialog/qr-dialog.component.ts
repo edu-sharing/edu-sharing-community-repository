cb5c6bccdfe802d458e6e81f6378f2cf1d641801
Benedikt Reuter <reuter@edu-sharing.net>
2026-09-21 12:28:14 +0200

    feat:show shortlink with copy button below QR code dialog

--- Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.component.ts
+++ Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.component.ts
@@ -1,10 +1,13 @@
+import { Clipboard } from '@angular/cdk/clipboard';
 import { Component, OnInit, inject } from '@angular/core';
 import { Node } from 'ngx-edu-sharing-api';
 import { NodeHelperService } from '../../../../services/node-helper.service';
+import { Toast, ToastType } from '../../../../services/toast';
 import { CARD_DIALOG_DATA } from '../../card-dialog/card-dialog-config';
 
 export interface QrDialogData {
     node: Node;
+    showShortlink?: boolean;
 }
 
 @Component({
@@ -16,10 +19,28 @@ export interface QrDialogData {
 export class QrDialogComponent implements OnInit {
     data = inject<QrDialogData>(CARD_DIALOG_DATA);
     private nodeHelper = inject(NodeHelperService);
+    private clipboard = inject(Clipboard);
+    private toast = inject(Toast);
 
     url: string;
+    shortUrl: string;
 
     ngOnInit(): void {
         this.url = this.nodeHelper.getNodeUrl(this.data.node);
+        if (this.data.showShortlink ?? true) {
+            this.shortUrl = this.nodeHelper.getNodeUrl(this.data.node, null, true);
+        }
+    }
+
+    copyShortlink(): void {
+        if (this.clipboard.copy(this.shortUrl)) {
+            this.toast.show({
+                message: 'WORKSPACE.SHARE_LINK.COPIED_CLIPBOARD',
+                type: 'info',
+                subtype: ToastType.InfoSimple,
+            });
+        } else {
+            this.toast.error(null, 'WORKSPACE.SHARE_LINK.COPIED_CLIPBOARD_ERROR');
+        }
     }
 }
--- Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.module.ts
+++ Frontend/src/app/features/dialogs/dialog-modules/qr-dialog/qr-dialog.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { QRCodeComponent } from 'angularx-qrcode';
+import { SharedModule } from '../../../../shared/shared.module';
 import { QrDialogComponent } from './qr-dialog.component';
 import { QrContentComponent } from './qr-content/qr-content.component';
 
@@ -9,7 +10,7 @@ export { QrDialogComponent };
 
 @NgModule({
     declarations: [QrDialogComponent, QrContentComponent],
-    imports: [CommonModule, QRCodeComponent, TranslateModule],
+    imports: [CommonModule, QRCodeComponent, TranslateModule, SharedModule],
     exports: [
         // Export for embed app
         QrContentComponent,
